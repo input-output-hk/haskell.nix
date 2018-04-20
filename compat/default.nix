dd47498850efd36cc89f3132e4cc656693b974dc
Moritz Angermann <moritz.angermann@gmail.com>
2018-04-20 18:08:36 +0200

    Add default.nix

---
+++ compat/default.nix
@@ -0,0 +1,1 @@
+{ license-map = import ./cabal-licenses.nix; platform-map = import ./cabal-os-arch-comp.nix; host-map = import ./host-map.nix; driver = import ./driver.nix; }
---
+++ default.nix
@@ -0,0 +1,1 @@
+{ compat = import ./compat; }
