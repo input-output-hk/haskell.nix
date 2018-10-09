a3236262bfcd916d10aa43d7d0396f3bb5720f88
Moritz Angermann <moritz.angermann@gmail.com>
2018-10-09 23:58:36 +0800

    builds

--- default.nix
+++ default.nix
@@ -74,6 +74,8 @@ let
                                           Glob = dontCheck super.Glob;
                                           infer-license = dontCheck super.infer-license;
                                           text-metrics = dontCheck super.text-metrics;
+
+                                          vector-algorithms = dontCheck super.vector-algorithms;
                                           # doctest
                                           comonad = dontCheck super.comonad;
                                           semigroupoids = dontCheck super.semigroupoids;
