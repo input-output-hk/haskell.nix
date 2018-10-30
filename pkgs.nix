b7f11579f12635420f04231a6907bcbf93efdf11
Moritz Angermann <moritz.angermann@gmail.com>
2018-10-30 16:07:35 +0800

    Bump haskell.nix/hackage.nix

--- pkgs.nix
+++ pkgs.nix
@@ -14,14 +14,14 @@ let
   hackage = import (overrideWith "hackage"
                    (pkgs.fetchFromGitHub { owner  = "angerman";
                                            repo   = "hackage.nix";
-                                           rev    = "5910ad8039568239082e71eae18117fdcac94d89";
+                                           rev    = "66c28064da46525711722b75b4adb2ac878897d3";
                                            sha256 = "12ffzzjgirwzha3ngxbniccgn19406iryxspq19kgi4kz9lz6bpr"; }))
                    ;
   # a different haskell infrastructure
   haskell = import (overrideWith "haskell"
                     (pkgs.fetchFromGitHub { owner  = "angerman";
                                             repo   = "haskell.nix";
-                                            rev    = "866042b9bfe7cd44ff794efee5eb0fae762c5fbe";
+                                            rev    = "7d2b2b8e364b5ec76c5eeb4a411595fd4382ddfa";
                                             sha256 = "05vzgx3frr7jrzhkpdrrknd2c29dynsxsdaalh39hzq95j175pmm"; }))
                    hackage;
 
