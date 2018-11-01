051369e9fed6144578f99bb89e7c340942374b38
Will Fancher <elvishjerricco@gmail.com>
2018-10-31 22:36:53 -0400

    Added a configuration-nix.nix file for plan-agnostic configuration

---
+++ modules/configuration-nix.nix
@@ -0,0 +1,11 @@
+# This file is for configuration that should be required by every
+# package set. Hopefully we can keep configuration for particular
+# pacakge sets out of this repo. Ideally, this file is only used for
+# fixing things that are broken due to the Nix infrastructure.
+
+{ pkgs, ... }: {
+  # terminfo doesn't list libtinfo in its cabal file. We could ignore
+  # this if we used the terminfo shipped with GHC, but this package is
+  # reinstallable so we'd rather have it defined in the plan.
+  packages.terminfo.components.library.libs = [pkgs.ncurses];
+}
