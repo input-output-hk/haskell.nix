fc60fe80fded31126c4e7c42857230c1a8eae0fa
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-11-05 16:11:48 +1000

    default.nix: Add check that hackage-db submodule is initialized

--- default.nix
+++ default.nix
@@ -1,7 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
 let
- hsPkgs = import ./pkgs.nix { inherit pkgs; };
+  hsPkgs = import ./pkgs.nix { inherit pkgs; };
 in
- hsPkgs // { nix-tools-all-execs = pkgs.symlinkJoin
-           { name = "nix-tools";
-             paths = builtins.attrValues hsPkgs.nix-tools.components.exes; }; }
+  if ! builtins.pathExists ./hackage-db/hackage-db.cabal
+  then abort ''
+
+    The `hackage-db' submodule is not initialized.
+    Run `git submodule update --init`.
+  ''
+  else
+    hsPkgs // {
+      nix-tools-all-execs = pkgs.symlinkJoin
+        { name = "nix-tools";
+          paths = builtins.attrValues hsPkgs.nix-tools.components.exes; };
+    }
