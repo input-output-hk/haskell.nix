28a9c6f3528daa57d506d134e137fb617a947985
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-09 16:02:18 +0800

    Drop def-pkg-overlay

--- default.nix
+++ default.nix
@@ -4,5 +4,5 @@ hackage: let haskell = {
   # The *new* pkg set is one that build components.
   # This also uses the module system for much greater extensibility.
   # To make extend and override things, pass a modules argument to new-package-set.nix
-  mkNewPkgSet = { pkgs, pkg-def, pkg-def-overlays ? [], modules ? [] }@args: import ./new-package-set.nix (args // { inherit hackage; });
+  mkNewPkgSet = { pkgs, pkg-def, modules ? [] }@args: import ./new-package-set.nix (args // { inherit hackage; });
 }; in haskell
