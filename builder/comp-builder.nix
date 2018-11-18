5838e54278915b2d94669e8cce8585783668882f
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-18 16:18:56 +0800

    Make sure we don't flood libraries with RPATHs on macOS

--- builder/comp-builder.nix
+++ builder/comp-builder.nix
@@ -73,6 +73,35 @@ let
     ${lib.concatMapStringsSep "\n" (p: exactDep "--package-db ${p.components.library}/package.conf.d" p.identifier.name) component.depends}
     ${lib.concatMapStringsSep "\n" (exactDep "") nonReinstallablePkgs}
 
+  ''
+  # This code originates in the `generic-builder.nix` from nixpkgs.  However GHC has been fixed
+  # to drop unused libraries referneced from libraries; and this patch is usually included in the
+  # nixpkgs's GHC builds.  This doesn't sadly make this stupid hack unnecessary.  It resurfes in
+  # the form of Cabal trying to be smart. Cabal when linking a library figures out that you likely
+  # need those `rpath` entries, and passes `-optl-Wl,-rpath,...` for each dynamic library path to
+  # GHC, thus subverting the linker and forcing it to insert all those RPATHs weather or not they
+  # are needed.  We therfore reuse the linker hack here to move all al dynamic lirbaries into a
+  # common folder (as links) and thus prevent Cabal from going nuts.
+  #
+  # TODO: Fix Cabal.
+  # TODO: this is only needed if we do dynamic libraries.
+  + lib.optionalString stdenv.isDarwin ''
+    # Work around a limit in the macOS Sierra linker on the number of paths
+    # referenced by any one dynamic library:
+    #
+    # Create a local directory with symlinks of the *.dylib (macOS shared
+    # libraries) from all the dependencies.
+    local dynamicLinksDir="$out/lib/links"
+    mkdir -p $dynamicLinksDir
+    for d in $(grep dynamic-library-dirs "$out/package.conf.d/"*|awk '{print $2}'|sort -u); do
+      ln -s "$d/"*.dylib $dynamicLinksDir
+    done
+    # Edit the local package DB to reference the links directory.
+    for f in "$out/package.conf.d/"*.conf; do
+      sed -i "s,dynamic-library-dirs: .*,dynamic-library-dirs: $dynamicLinksDir," $f
+    done
+  '' + ''
+    ${ghc.targetPrefix}ghc-pkg --package-db $out/package.conf.d recache
   '' + ''
     ${ghc.targetPrefix}ghc-pkg --package-db $out/package.conf.d check
   '');
@@ -138,7 +167,7 @@ in stdenv.mkDerivation ({
   '';
 
   buildPhase = ''
-    $SETUP_HS build -j$NIX_BUILD_CORES
+    $SETUP_HS build -j$NIX_BUILD_CORES ${lib.concatStringsSep " " component.setupBuildFlags}
   '';
 
   checkPhase = ''
--- modules/package.nix
+++ modules/package.nix
@@ -123,6 +123,10 @@ in {
             type = listOfFilteringNulls str;
             default = [];
           };
+          setupBuildFlags = mkOption {
+            type = listOfFilteringNulls str;
+            default = [];
+          };
           doExactConfig = mkOption {
             type = bool;
             default = false;
