5f56d93147ab0ff0c9798ca760569038fa632b18
Moritz Angermann <moritz.angermann@gmail.com>
2018-11-29 15:35:10 +0800

    Revert "Drop def-pkg-overlay"
    We still needthis, as the module system is not very well suited
    for package overrides.
    
    This reverts commit 28a9c6f3528daa57d506d134e137fb617a947985.

--- default.nix
+++ default.nix
@@ -18,5 +18,7 @@ hackage: let haskell = {
   # The *new* pkg set is one that build components.
   # This also uses the module system for much greater extensibility.
   # To make extend and override things, pass a modules argument to new-package-set.nix
-  mkNewPkgSet = { pkgs, pkg-def, modules ? [] }@args: import ./new-package-set.nix (args // { inherit hackage; pkg-def = strip-pkg-def pkgs pkg-def; });
+  mkNewPkgSet
+    = { pkgs, pkg-def, pkg-def-overlays ? [], modules ? [] }@args:
+      import ./new-package-set.nix (args // { inherit hackage; pkg-def = strip-pkg-def pkgs pkg-def; });
 }; in haskell
