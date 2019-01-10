9ce06e821ef6d3e901fad1b7724a755d15abf33f
Moritz Angermann <moritz.angermann@gmail.com>
2019-01-10 21:52:24 +0800

    Make it build
    - bump haskell.nix
    - bump hackage.nix
    - add sources for haskell-src-meta and hnix.
    
    Note: We can not upgrade haskell-src-ext 0.20.3 -> 0.21.0, or
    pretty-show, those will result in broken happy lookups. (Not
    sure where those come from)
    
    Also we seem to have an issue with source repository packages in the
    plan.json parser.  They are ignored and lead to incorrect builds
    with plan-to-json.

--- pkgs.nix
+++ pkgs.nix
@@ -14,28 +14,35 @@ let
   hackage = import (overrideWith "hackage"
                    (pkgs.fetchFromGitHub { owner  = "angerman";
                                            repo   = "hackage.nix";
-                                           rev    = "66c28064da46525711722b75b4adb2ac878897d3";
-                                           sha256 = "12ffzzjgirwzha3ngxbniccgn19406iryxspq19kgi4kz9lz6bpr";
+                                           rev    = "15155a37d3a40173b57e93b09453984fac614523";
+                                           sha256 = "016538bfnlb0dxdga4n5p0m75zq0zymbdh5vrvr0gwsa7pywi7zy";
                                            name   = "hackage-exprs-source"; }))
                    ;
   # a different haskell infrastructure
   haskell = import (overrideWith "haskell"
                     (pkgs.fetchFromGitHub { owner  = "angerman";
                                             repo   = "haskell.nix";
-                                            rev    = "2a3b2612a15fd7f14d32c3519aba2b64bd7b1e43";
-                                            sha256 = "181dv1zlf381kkb82snjmpibhgmkyw1n5qsvpqjrv8dxmcjqjl2k";
+                                            rev    = "ed1dbc01f98411894f6613c62818f14b02fb6679";
+                                            sha256 = "0kbj4kb9rlvjb4afpcisz9zlk5z3h7frkwggfwri1q5683fapkgv";
                                             name   = "haskell-lib-source"; }))
                    hackage;
 
   # our packages
+  # this is generated with plan-to-nix
+  # $ cabal new-configure
+  # $ plan-to-nix ./dist-newstyle/cache/plan.json > plan.nix
   plan = import ./plan.nix;
 
   pkgSet = haskell.mkNewPkgSet {
     inherit pkgs;
     pkg-def = plan;
     pkg-def-overlays = [
-      { nix-tools = ./nix-tools.nix;
-        hackage-db = ./hackage-db.nix;
+      { nix-tools  = ./nix-tools.nix;
+        # these are generated with cabal-to-nix
+        # $ cabal-to-nix https://github.com/galenhuntington/haskell-src-meta.git 109ee29d5fd0f4e23fdd2f80eb122d66341b64a9 > haskell-src-meta.nix
+        hackage-db       = ./hackage-db.nix;
+        haskell-src-meta = ./haskell-src-meta.nix;
+        hnix             = ./hnix.nix;
       }
     ];
     modules = [
