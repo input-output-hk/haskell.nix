3a6c2f823d0db6ac6730dcf78076fd5d904fba85
Moritz Angermann <moritz.angermann@gmail.com>
2019-01-18 16:03:54 +0800

    no patch versions

--- patches/ghc862/default.nix
+++ patches/ghc862/default.nix
@@ -1,10 +1,10 @@
 {
-  packages.hpc.patches = [ ({ version, revision }: if version == "0.6.0.3.patch" && revision == 0 then ./hpc-0.6.0.3.patch else null) ];
-  packages.containers.patches = [ ({ version, revision }: if version == "0.6.0.1.patch" && revision == 0 then ./containers-0.6.0.1.patch else null) ];
-  packages.binary.patches = [ ({ version, revision }: if version == "0.8.6.0.patch" && revision == 0 then ./binary-0.8.6.0.patch else null) ];
-  packages.parsec.patches = [ ({ version, revision }: if version == "3.1.13.0.patch" && revision == 0 then ./parsec-3.1.13.0.patch else null) ];
-  packages.time.patches = [ ({ version, revision }: if version == "1.8.0.2.patch" && revision == 0 then ./time-1.8.0.2.patch else null) ];
-  packages.unix.patches = [ ({ version, revision }: if version == "2.7.2.2.patch" && revision == 0 then ./unix-2.7.2.2.patch else null) ];
-  packages.process.patches = [ ({ version, revision }: if version == "1.6.3.0.patch" && revision == 0 then ./process-1.6.3.0.patch else null) ];
-  packages.transformers.patches = [ ({ version, revision }: if version == "0.5.5.0.patch" && revision == 0 then ./transformers-0.5.5.0.patch else null) ];
+  packages.hpc.patches = [ ({ version, revision }: if version == "0.6.0.3" && revision == 0 then ./hpc-0.6.0.3.patch else null) ];
+  packages.containers.patches = [ ({ version, revision }: if version == "0.6.0.1" && revision == 0 then ./containers-0.6.0.1.patch else null) ];
+  packages.binary.patches = [ ({ version, revision }: if version == "0.8.6.0" && revision == 0 then ./binary-0.8.6.0.patch else null) ];
+  packages.parsec.patches = [ ({ version, revision }: if version == "3.1.13.0" && revision == 0 then ./parsec-3.1.13.0.patch else null) ];
+  packages.time.patches = [ ({ version, revision }: if version == "1.8.0.2" && revision == 0 then ./time-1.8.0.2.patch else null) ];
+  packages.unix.patches = [ ({ version, revision }: if version == "2.7.2.2" && revision == 0 then ./unix-2.7.2.2.patch else null) ];
+  packages.process.patches = [ ({ version, revision }: if version == "1.6.3.0" && revision == 0 then ./process-1.6.3.0.patch else null) ];
+  packages.transformers.patches = [ ({ version, revision }: if version == "0.5.5.0" && revision == 0 then ./transformers-0.5.5.0.patch else null) ];
 }
--- patches/update-patches.sh
+++ patches/update-patches.sh
@@ -4,7 +4,7 @@ for ghc in $(find . -name "ghc*" -type d); do
    echo "{" > default.nix
    (for a in $(find . -name "*.patch" -type f); do
      b=${a%%.patch};
-     b=${a##./};
+     b=${b##./};
      echo "  packages.${b%%-*}.patches = [ ({ version, revision }: if version == \"${b##*-}\" && revision == 0 then $a else null) ];" >> default.nix
    done) || true
    echo "}" >> default.nix)
