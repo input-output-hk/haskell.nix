5acc8b97fd55e7bbe65994e499b14e535a28026c
Moritz Angermann <moritz.angermann@gmail.com>
2019-01-18 15:50:24 +0800

    missing default.nix

--- patches/default.nix
+++ patches/default.nix
@@ -1,2 +1,9 @@
 {
+  ghc841 = import ./ghc841;
+  ghc842 = import ./ghc842;
+  ghc843 = import ./ghc843;
+  ghc844 = import ./ghc844;
+  ghc861 = import ./ghc861;
+  ghc862 = import ./ghc862;
+  ghc863 = import ./ghc863;
 }
