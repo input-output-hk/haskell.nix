70f01c2029b3020c68ae22eef6542c3e7610807d
Will Fancher <elvishjerricco@gmail.com>
2019-02-02 00:23:01 -0500

    Use mkForce in with-package test, and add a comment about the quirk.

--- test/with-packages/default.nix
+++ test/with-packages/default.nix
@@ -25,7 +25,7 @@ let
       # vary component config for tests
       {
         packages.test-with-packages.components = {
-          all.doExactConfig = false;    # the default
+          all.doExactConfig = mkForce false; # the default; use force because of quirk in all. See package.nix
           library.doExactConfig = true; # not the default
         };
       }
@@ -68,14 +68,15 @@ in
     '';
 
     meta.platforms = platforms.all;
-} // {
-  # Used for debugging with nix repl
-  inherit packages pkgSet;
+    passthru = {
+      # Used for debugging with nix repl
+      inherit packages pkgSet;
 
-  # Used for testing externally with nix-shell (../tests.sh).
-  # This just adds cabal-install to the existing shells.
-  test-shell = addCabalInstall packages.test-with-packages.components.all;
+      # Used for testing externally with nix-shell (../tests.sh).
+      # This just adds cabal-install to the existing shells.
+      test-shell = addCabalInstall packages.test-with-packages.components.all;
 
-  # A variant of test-shell with the component option doExactConfig enabled
-  test-shell-dec = addCabalInstall packages.test-with-packages.components.library;
+      # A variant of test-shell with the component option doExactConfig enabled
+      test-shell-dec = addCabalInstall packages.test-with-packages.components.library;
+    };
 }
