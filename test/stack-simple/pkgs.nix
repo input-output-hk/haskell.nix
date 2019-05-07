0e02f1fb6809e6c223b670bad7e3b09144e816f2
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-05-07 12:51:29 +1000

    Regenerate nix for test/stack-simple

---
+++ test/stack-simple/pkgs.nix
@@ -0,0 +1,10 @@
+{
+  extras = hackage:
+    {
+      packages = {
+        "transformers" = (((hackage.transformers)."0.5.6.2").revisions).default;
+        "process" = (((hackage.process)."1.6.5.0").revisions).default;
+        } // { stack-simple = ./stack-simple.nix; };
+      };
+  resolver = "lts-13.18";
+  }
