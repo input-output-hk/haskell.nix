7c45574aedbb86192c7a48cb95fab6c3ed3d7113
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2019-05-17 15:19:34 +1200

    Use `cabal update` as it works with older versions

--- lib/hackageIndex.nix
+++ lib/hackageIndex.nix
@@ -5,12 +5,12 @@ let
   # To avoid downloading more data than necessary this will provide a base.
   cachedState = runCommand "hackage-${builtins.substring 0 4 indexState}" {} ''
     mkdir -p $out
-    HOME=$out ${cabal-install}/bin/cabal new-update 'hackage.haskell.org,${builtins.substring 0 4 indexState}-01-01T00:00:00Z'
+    HOME=$out ${cabal-install}/bin/cabal update --index-state='${builtins.substring 0 4 indexState}-01-01T00:00:00Z'
   '';
 in runCommand "hackage-${builtins.replaceStrings [":"] [""] indexState}" {} ''
     mkdir -p $out
     cp -r ${cachedState}/.cabal $out
     chmod +w -R $out/.cabal
     sed -i.back -e "s|${cachedState}|$out|g" $out/.cabal/config
-    HOME=$out ${cabal-install}/bin/cabal new-update 'hackage.haskell.org,${indexState}'
+    HOME=$out ${cabal-install}/bin/cabal update --index-state='${indexState}'
   ''
