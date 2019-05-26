f6b17d13dbfb426f68d6741796bfae0ccf165b78
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-05-26 18:18:31 +1000

    tests: Add ghcWithHoogle test

--- test/snapshots/default.nix
+++ test/snapshots/default.nix
@@ -1,19 +1,33 @@
-{ stdenv, haskellPackages }:
+{ stdenv, haskellPackages, snapshots }:
 
 with stdenv.lib;
 
+let
+  env = snapshots."lts-12.21".ghcWithHoogle
+    (ps: with ps; [ conduit conduit-extra resourcet ]);
+
+in
   stdenv.mkDerivation {
     name = "shell-for-test";
 
     buildCommand = ''
       ########################################################################
-      # test snapshot ghcWithHoogle
+      # test single component from haskellPackages
 
       printf "checking that the latest LTS snapshot has the lens package...\n" >& 2
       test -d ${haskellPackages.lens.components.library}
+
+      ########################################################################
+      # test snapshot ghcWithHoogle
+
+      printf "checking that the ghcWithPackages env has the package...\n" >& 2
+      ${env}/bin/ghc-pkg list | grep conduit
 
       touch $out
     '';
 
     meta.platforms = platforms.all;
-  }
+    passthru = {
+      inherit env;
+    };
+}
