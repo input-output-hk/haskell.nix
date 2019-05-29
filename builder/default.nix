fa40a7f35cf72a32c3cfa3596859469eea341db7
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2019-05-29 17:46:39 +1200

    Fix missing args resulting from rebase error

--- builder/default.nix
+++ builder/default.nix
@@ -31,7 +31,7 @@ in {
   # Build a Haskell package from its config.
   # TODO: this pkgs is the adjusted pkgs, but pkgs.pkgs is unadjusted
   build-package = haskellLib.weakCallPackage pkgs ./hspkg-builder.nix {
-    inherit haskellLib ghc buildGHC comp-builder;
+    inherit haskellLib ghc buildGHC comp-builder nonReinstallablePkgs hsPkgs;
   };
 
   # Same as haskellPackages.shellFor in nixpkgs.
