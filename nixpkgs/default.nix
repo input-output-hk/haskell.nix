8512832c32025564c49c59b7bff3e8f49f0afdf4
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-06-02 07:21:21 +1000

    Fix evaluation on Hydra (#153)
    * Fix eval on Hydra
    
    * Bump nixpkgs to latest 19.03
    
    * Change unit tests into a derivation
    
    Prevents Hydra from complaining about them.
    
    * Add unpacked source pins to Hydra jobset
    
    So that they are cached.

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -2,5 +2,9 @@ let
   fetch = jsonFile:
     with builtins;
     let spec = fromJSON (readFile jsonFile);
-    in fetchTarball { inherit (spec) sha256; url = "${spec.url}/archive/${spec.rev}.tar.gz"; };
+    in fetchTarball {
+      name = "nixpkgs";
+      inherit (spec) sha256;
+      url = "${spec.url}/archive/${spec.rev}.tar.gz";
+    };
 in import (fetch ./github.json)
