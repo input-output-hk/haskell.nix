ff978b485f9372a8a653e7d4898eb0a61a3b6268
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2019-06-03 01:29:06 +1200

    Add ghc865 patch files

---
+++ patches/ghc865/default.nix
@@ -0,0 +1,7 @@
+{
+  packages.binary.patches = [ ({ version, revision }: if version == "0.8.6.0" && revision == 0 then ./binary-0.8.6.0.patch else null) ];
+  packages.containers.patches = [ ({ version, revision }: if version == "0.6.0.1" && revision == 0 then ./containers-0.6.0.1.patch else null) ];
+  packages.hpc.patches = [ ({ version, revision }: if version == "0.6.0.3" && revision == 0 then ./hpc-0.6.0.3.patch else null) ];
+  packages.parsec.patches = [ ({ version, revision }: if version == "3.1.13.0" && revision == 0 then ./parsec-3.1.13.0.patch else null) ];
+  packages.unix.patches = [ ({ version, revision }: if version == "2.7.2.2" && revision == 0 then ./unix-2.7.2.2.patch else null) ];
+}
