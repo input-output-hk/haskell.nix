caf1f61267f3a409a3200130f394b75ceb7fb9b8
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-06-03 13:26:28 +1000

    Use release-lib.nix for Hydra (#161)
    * Use release-lib.nix for Hydra jobset
    
    * Buildkite: Add a script to check Hydra evaluation

--- release.nix
+++ release.nix
@@ -1,7 +1,35 @@
-let
-  haskell = import ./default.nix {};
-in {
-  inherit (haskell) nix-tools source-pins;
+{ supportedSystems ? [ "x86_64-linux" ] # spare our "x86_64-darwin" builders for now
+, scrubJobs ? true
+, haskell-nix ? { outPath = ./.; rev = "abcdef"; }
+, nixpkgsArgs ? {}
+}:
 
-  tests = import ./test { inherit haskell; };
-}
+let fixedNixpkgs = import ./nixpkgs {}; in
+with fixedNixpkgs.lib;
+with (import (fixedNixpkgs.path + "/pkgs/top-level/release-lib.nix") {
+  inherit supportedSystems scrubJobs nixpkgsArgs;
+  packageSet = import (haskell-nix + /build.nix);
+});
+
+let
+  # Remove tests which have meta.disabled = true
+  filterTests = let
+    nonEmpty = attrs: length (attrValues attrs) != 0;
+    removeDisabled = filterAttrs (system: test: !(test.meta.disabled or false));
+  in jobs: jobs // {
+    tests = filterAttrs (_: nonEmpty) (mapAttrs (name: removeDisabled) jobs.tests);
+  };
+
+  jobs = {
+    native = filterTests (mapTestOn (packagePlatforms pkgs));
+  } // {
+    # On IOHK Hydra, "required" is a special job that updates the
+    # GitHub CI status.
+    required = fixedNixpkgs.releaseTools.aggregate {
+      name = "haskell.nix-required";
+      meta.description = "All jobs required to pass CI";
+      constituents = collect isDerivation jobs.native;
+    };
+  };
+
+in jobs
