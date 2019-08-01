ecd0eb0d6c2e3704524716f62f8c1ed80cbfc15a
Moritz Angermann <moritz.angermann@gmail.com>
2019-08-01 20:58:26 +0800

    fix ci

--- ci.nix
+++ ci.nix
@@ -3,15 +3,15 @@ let nixpkgs1903 = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archiv
     "release-19.03" = {
         x86_64-linux = {
             hello = with (import ./. { nixpkgs = nixpkgs1903; system = "x86_64-linux"; });
-                hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
+                (hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
             x86_64-pc-mingw32-hello = with (import ./. { nixpkgs = nixpkgs1903; system = "x86_64-linux"; crossSystem = "x86_64-pc-mingw32"; });
-                hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
+                (hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
         };
         x86_64-darwin = {
             hello = with (import ./. { nixpkgs = nixpkgs1903; system = "x86_64-darwin"; });
-                hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
+                (hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
             x86_64-pc-mingw32-hello = with (import ./. { nixpkgs = nixpkgs1903; system = "x86_64-darwin"; crossSystem = "x86_64-pc-mingw32"; });
-                hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
+                (hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
         };
     };
 }
