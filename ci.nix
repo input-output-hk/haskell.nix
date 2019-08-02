cbfee273e3824f9688441872d973668462091ca3
Moritz Angermann <moritz.angermann@gmail.com>
2019-08-02 20:58:32 +0800

    disable darwin

--- ci.nix
+++ ci.nix
@@ -10,12 +10,12 @@ in recRecurseIntoAttrs (x: lib.isAttrs x && !lib.isDerivation x) {
             x86_64-pc-mingw32-hello = with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-linux"; crossSystem = { config = "x86_64-pc-mingw32"; }; }; });
                 (haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
         };
-        x86_64-darwin = {
-            hello = with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; }; });
-                (haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
-            x86_64-pc-mingw32-hello = with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; crossSystem = { config = "x86_64-pc-mingw32"; }; }; });
-                (haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
-        };
+        # x86_64-darwin = {
+        #     hello = with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; }; });
+        #         (haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
+        #     x86_64-pc-mingw32-hello = with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; crossSystem = { config = "x86_64-pc-mingw32"; }; }; });
+        #         (haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
+        # };
     };
     tests = {
         x86_64-linux = (import ./test { nixpkgsArgs = { system = "x86_64-linux"; }; });
@@ -24,7 +24,7 @@ in recRecurseIntoAttrs (x: lib.isAttrs x && !lib.isDerivation x) {
     stackage = {
         x86_64-linux = (with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-linux"; }; });
             haskell-nix.snapshots."lts-13.29");
-        x86_64-darwin = (with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; }; });
-            haskell-nix.snapshots."lts-13.29");
+        # x86_64-darwin = (with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; }; });
+        #     haskell-nix.snapshots."lts-13.29");
     };
 }
