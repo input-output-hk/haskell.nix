9eee472a1ec25032265e8e14bda1d3aee11f23ad
Moritz Angermann <moritz.angermann@gmail.com>
2019-08-02 20:24:59 +0800

    cursed recurse

--- ci.nix
+++ ci.nix
@@ -1,28 +1,31 @@
 let nixpkgs1903 = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/b978a94c8f9167fb86372ce1044a23f8df2edea0.tar.gz"; in
 with (import nixpkgs1903 {});
-recurseIntoAttrs {
-    "release-19.03" = recurseIntoAttrs {
-       x86_64-linux = recurseIntoAttrs {
+let
+    isDerivation = x: x ? "type" && x.type == "derivation";
+    recRecurseIntoAttrs = pred: x: if pred x then recurseIntoAttrs (lib.mapAttrs (_: v: recRecurseIntoAttrs v) x) else x;
+in recRecurseIntoAttrs (x: !isDerivation x) {
+    "release-19.03" = {
+       x86_64-linux = {
             hello = with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-linux"; }; });
                 (haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
             x86_64-pc-mingw32-hello = with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-linux"; crossSystem = { config = "x86_64-pc-mingw32"; }; }; });
                 (haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
         };
-        x86_64-darwin = recurseIntoAttrs {
+        x86_64-darwin = {
             hello = with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; }; });
                 (haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
             x86_64-pc-mingw32-hello = with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; crossSystem = { config = "x86_64-pc-mingw32"; }; }; });
                 (haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
         };
     };
-    tests = recurseIntoAttrs {
-        x86_64-linux = recurseIntoAttrs (import ./test { nixpkgsArgs = { system = "x86_64-linux"; }; });
-        x86_64-darwin = recurseIntoAttrs (import ./test { nixpkgsArgs = { system = "x86_64-darwin"; }; });
+    tests = {
+        x86_64-linux = (import ./test { nixpkgsArgs = { system = "x86_64-linux"; }; });
+        x86_64-darwin = (import ./test { nixpkgsArgs = { system = "x86_64-darwin"; }; });
     };
-    stackage = recurseIntoAttrs {
-        x86_64-linux = recurseIntoAttrs (with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-linux"; }; });
+    stackage = {
+        x86_64-linux = (with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-linux"; }; });
             haskell-nix.snapshots."lts-13.29");
-        x86_64-darwin = recurseIntoAttrs (with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; }; });
+        x86_64-darwin = (with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; }; });
             haskell-nix.snapshots."lts-13.29");
     };
 }
