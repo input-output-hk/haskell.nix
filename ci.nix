795b2d6c8c0183858c3edb609469d757eabd88ea
Moritz Angermann <moritz.angermann@gmail.com>
2019-08-03 16:37:59 +0800

    disable stacke builds; add compilers

--- ci.nix
+++ ci.nix
@@ -17,14 +17,23 @@ in recRecurseIntoAttrs (x: lib.isAttrs x && !lib.isDerivation x) {
         #         (haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
         # };
     };
+    haskell.compiler = {
+        x86_64-linux = with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-linux"; }; });
+            haskell.compiler
+        x86_64-darwin = with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; }; });
+            haskell.compiler
+    };
     tests = {
         x86_64-linux = (import ./test { nixpkgsArgs = { system = "x86_64-linux"; }; });
         x86_64-darwin = (import ./test { nixpkgsArgs = { system = "x86_64-darwin"; }; });
     };
-    stackage = {
-        x86_64-linux = (with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-linux"; }; });
-            haskell-nix.snapshots."lts-13.29");
-        # x86_64-darwin = (with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; }; });
-        #     haskell-nix.snapshots."lts-13.29");
-    };
+    # Disable stackage until Monday Aug 4th.
+    # We need a new agent re-deployed and the limit fo 10k evals lifted.
+    #
+    # stackage = {
+    #     x86_64-linux = (with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-linux"; }; });
+    #         haskell-nix.snapshots."lts-13.29");
+    #     # x86_64-darwin = (with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; }; });
+    #     #     haskell-nix.snapshots."lts-13.29");
+    # };
 }
