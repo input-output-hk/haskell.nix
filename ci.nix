590ef9e46cdc3b77b590f24a0834e86bb2c8ade2
Moritz Angermann <moritz.angermann@gmail.com>
2019-08-05 20:44:01 +0800

    Disable stackage until we have the agent re-deployed.

--- ci.nix
+++ ci.nix
@@ -27,10 +27,12 @@ in recRecurseIntoAttrs (x: lib.isAttrs x && !lib.isDerivation x) {
         x86_64-linux = (import ./test { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-linux"; }; });
         # x86_64-darwin = (import ./test { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; }; });
     };
-    stackage = {
-        x86_64-linux = (with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-linux"; }; });
-            haskell-nix.snapshots."lts-13.29");
+    # Needs agent redeploy
+    #
+    # stackage = {
+    #     x86_64-linux = (with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-linux"; }; });
+    #         haskell-nix.snapshots."lts-13.29");
         # x86_64-darwin = (with (import ./. { nixpkgs = nixpkgs1903; nixpkgsArgs = { system = "x86_64-darwin"; }; });
         #     haskell-nix.snapshots."lts-13.29");
-    };
+    # };
 }
