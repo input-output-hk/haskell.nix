b7384a8d0d0a00004baff86a2374a83dcdc9cb16
Moritz Angermann <moritz.angermann@gmail.com>
2019-10-14 10:27:48 +0800

    Update config.nix

--- config.nix
+++ config.nix
@@ -3,7 +3,10 @@
   allowUnsupportedSystem = true;
   # we want the 64bit wine version
   wine.build = "wine64";
-  wine.release = "series3";
+  # once https://github.com/NixOS/nixpkgs/pull/68397 is
+  # merged, we can use `series3`; wine4 seems to suffer
+  # from some non-obvious regression :(
+  wine.release = "stable"; #"series3";
 
   # sadly we need to patch GHC a bit.
   packageOverrides = ps: with ps; let
