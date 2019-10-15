d571d26d1bb8184c97f3c7208d5b157b96aeae67
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2019-10-15 15:33:50 +1300

    Moritz's super cool default.nix idea

--- default.nix
+++ default.nix
@@ -1,13 +1,5 @@
-{ pkgs ? import nixpkgs ({ config = config; overlays = overlays; } // nixpkgsArgs)
-# Use a pinned nixpkgs rather than the one on NIX_PATH
-, nixpkgs ? <nixpkgs> #./nixpkgs
-# default config we provide to nixpkgs.
-, config ? import ./config.nix
-# default overlays
-, overlays ? import ./overlays
-# Provide additional args to the nixpkgs instantiation. This can also override
-# config and overlays.
-, nixpkgsArgs ? { }
-}:
-
-pkgs
+# This default.nix is designed to be passed directly nixpkgs with something like:
+#   import <nixpkgs> (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz)
+{ config   = import ./config.nix;
+  overlays = import ./overlays;
+}
