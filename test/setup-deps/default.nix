55e6a0a233851fec274d3bbc660d743ad1f79317
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2019-10-29 21:10:21 +1300

    Use reinstallableLibGhc to fix setup-depends test

--- test/setup-deps/default.nix
+++ test/setup-deps/default.nix
@@ -12,7 +12,8 @@ let
       # Package has no exposed modules which causes
       #   haddock: No input file(s)
       packages.bytestring-builder.doHaddock = false;
-    }];
+    }
+    { reinstallableLibGhc = true; } ];
   };
 
   packages = pkgSet.config.hsPkgs;
