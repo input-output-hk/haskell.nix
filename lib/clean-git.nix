5393c501e2401489e1f64914decb02739d90ebf8
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2019-11-06 22:44:09 +1300

    Fix cleanGit when using a worktree (#282)
    Joachim's original version had logic to handle git worktree's but
    it was not wired into the updated version that uses cleanSourceWith.

--- lib/clean-git.nix
+++ lib/clean-git.nix
@@ -54,34 +54,61 @@ in
 if builtins.pathExists (toString src + "/.git")
 then
   let
-    git_dir =
-      if builtins.pathExists (toString src + "/.git/index")
-      then (toString src + "/.git")
-      else # likely a git worktree, so follow the indirection
-        let
-          git_content = lines (readFile (toString src + "/.git"));
-          first_line = head git_content;
-          prefix = "gitdir: ";
-          ok = length git_content == 1 && has_prefix prefix first_line;
-        in
-          if ok
-          then /. + remove_prefix prefix first_line
-          else abort "gitSource.nix: Cannot parse ${toString src + "/.git"}";
-
-    gitFiles = cleanSourceWith {
-      inherit src;
-      filter = path: type:
-        type == "directory" ||
-        lib.any (i: (lib.hasSuffix i path)) [
-          ".gitmodules" ".git/config" ".git/index" ".git/HEAD" ".git/objects" ".git/refs" ] ||
-        (lib.strings.hasInfix ".git/modules/" path &&
+    # Identify the .git directory and filter just the files that we need.
+    gitDir = cleanSourceWith ({
+        filter = path: type:
+          type == "directory" ||
           lib.any (i: (lib.hasSuffix i path)) [
-            "config" "index" "HEAD" "objects" "refs" ]);
-    };
+            "/config" "/index" "/HEAD" ] ||
+          (lib.strings.hasInfix "modules/" path &&
+            lib.any (i: (lib.hasSuffix i path)) [
+              "/config" "/index" "/HEAD" "/objects" "/refs" ]);
+      } // (
+      if builtins.pathExists (toString src + "/.git/index")
+        then { inherit src; subDir = ".git"; }
+        else {
+          # likely a git worktree, so follow the indirection
+          src =
+            let
+              git_content = lines (readFile (toString src + "/.git"));
+              first_line = head git_content;
+              prefix = "gitdir: ";
+              ok = length git_content == 1 && has_prefix prefix first_line;
+            in
+              if ok
+              then /. + remove_prefix prefix first_line
+              else abort "gitSource.nix: Cannot parse ${toString src + "/.git"}";
+    }));
 
+    # Worktrees have a commondir pointing to the common `.git` dir.  We need the
+    # config file from there to get the list of active submodules right.
+    commonConfig = if builtins.pathExists (toString gitDir.origSrc + "/commondir")
+        then
+           # The commondir file contains just the relative path of the
+           # common `.git` dir.
+           let
+             git_content = lines (readFile (toString gitDir.origSrc + "/commondir"));
+             first_line = head git_content;
+           in toString gitDir.origSrc + "/" + first_line + "/config"
+        else toString gitDir + "/config";
+
+    # We need the .gitmodules file for submoules to work.
+    gitModules = builtins.path { name = "gitmodules"; path = toString src + "/.gitmodules"; };
+
+    # Make a temporary dir that looks enough like the real thing for
+    # `git ls-files --recurse-submodules` to give us an accurate list
+    # of all the files in the index.
     whitelist_file =
       runCommand "git-ls-files" {envVariable = true;} ''
-        cd ${gitFiles}
+        tmp=$(mktemp -d)
+        cd $tmp
+        cp -r ${gitDir} .git
+        chmod +w -R .git
+        mkdir -p .git/objects .git/refs
+        mkdir -p .git-common/objects .git-common/refs
+        cp ${commonConfig} .git-common/config
+        echo ../.git-common > .git/commondir
+        cp ${gitModules} ./.gitmodules
         ${git}/bin/git ls-files --recurse-submodules > $out
       '';
 
