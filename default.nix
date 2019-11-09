ad6e5af0ef3310b9926393a9fbc325681bda43bf
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2019-11-09 15:11:27 +1300

    Improve error when old code breaks

--- default.nix
+++ default.nix
@@ -1,4 +1,7 @@
 rec {
+  # Make error given when using haskell.nix the old way clear
+  _Update_Haskell_Nix_See_PR_307_ = "https://github.com/input-output-hk/haskell.nix/pull/307";
+  
   # nixpkgsArgs is designed to be passed directly nixpkgs with something like:
   #   import <nixpkgs> (import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz)).nixpkgsArgs
   nixpkgsArgs = {
