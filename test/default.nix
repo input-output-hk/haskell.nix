19248b5a15cae162eb1b4e1224a1b659716a99c3
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2019-11-15 14:20:16 +1300

    Revert "Disable all the tests except the haskellNixRoots"
    This reverts commit 2000983bb0f813ad8199acfb4316f5a17c31ad0e.

--- test/default.nix
+++ test/default.nix
@@ -9,35 +9,35 @@ let
   util = callPackage ./util.nix {};
 in pkgs.recurseIntoAttrs {
   inherit (haskell-nix) haskellNixRoots;
-#  cabal-simple = haskell-nix.callPackage ./cabal-simple { inherit util; };
-#  cabal-simple-prof = haskell-nix.callPackage ./cabal-simple-prof { inherit util; };
-#  cabal-sublib = haskell-nix.callPackage ./cabal-sublib { inherit util; };
-#  cabal-22 = haskell-nix.callPackage ./cabal-22 { inherit util; };
-#  with-packages = haskell-nix.callPackage ./with-packages { inherit util; };
-#  builder-haddock = haskell-nix.callPackage ./builder-haddock {};
-#  stack-simple = haskell-nix.callPackage ./stack-simple {};
-#  snapshots = haskell-nix.callPackage ./snapshots {};
-#  shell-for = haskell-nix.callPackage ./shell-for {};
-#  shell-for-setup-deps = haskell-nix.callPackage ./shell-for-setup-deps {};
-#  setup-deps = import ./setup-deps { inherit pkgs; };
-#  callStackToNix = haskell-nix.callPackage ./call-stack-to-nix {};
-#  callCabalProjectToNix = haskell-nix.callPackage ./call-cabal-project-to-nix {};
-#  cabal-source-repo = haskell-nix.callPackage ./cabal-source-repo {};
-#  buildable = haskell-nix.callPackage ./buildable {};
-#  project-flags-cabal = haskell-nix.callPackage ./project-flags/cabal.nix {};
-#  project-flags-stack = haskell-nix.callPackage ./project-flags/stack.nix {};
-#  fully-static = haskell-nix.callPackage ./fully-static { inherit (pkgs) buildPackages; };
-#  ghc-options-cabal = haskell-nix.callPackage ./ghc-options/cabal.nix {};
-#  ghc-options-stack = haskell-nix.callPackage ./ghc-options/stack.nix {};
-#  exe-only = haskell-nix.callPackage ./exe-only { inherit util; };
+  cabal-simple = haskell-nix.callPackage ./cabal-simple { inherit util; };
+  cabal-simple-prof = haskell-nix.callPackage ./cabal-simple-prof { inherit util; };
+  cabal-sublib = haskell-nix.callPackage ./cabal-sublib { inherit util; };
+  cabal-22 = haskell-nix.callPackage ./cabal-22 { inherit util; };
+  with-packages = haskell-nix.callPackage ./with-packages { inherit util; };
+  builder-haddock = haskell-nix.callPackage ./builder-haddock {};
+  stack-simple = haskell-nix.callPackage ./stack-simple {};
+  snapshots = haskell-nix.callPackage ./snapshots {};
+  shell-for = haskell-nix.callPackage ./shell-for {};
+  shell-for-setup-deps = haskell-nix.callPackage ./shell-for-setup-deps {};
+  setup-deps = import ./setup-deps { inherit pkgs; };
+  callStackToNix = haskell-nix.callPackage ./call-stack-to-nix {};
+  callCabalProjectToNix = haskell-nix.callPackage ./call-cabal-project-to-nix {};
+  cabal-source-repo = haskell-nix.callPackage ./cabal-source-repo {};
+  buildable = haskell-nix.callPackage ./buildable {};
+  project-flags-cabal = haskell-nix.callPackage ./project-flags/cabal.nix {};
+  project-flags-stack = haskell-nix.callPackage ./project-flags/stack.nix {};
+  fully-static = haskell-nix.callPackage ./fully-static { inherit (pkgs) buildPackages; };
+  ghc-options-cabal = haskell-nix.callPackage ./ghc-options/cabal.nix {};
+  ghc-options-stack = haskell-nix.callPackage ./ghc-options/stack.nix {};
+  exe-only = haskell-nix.callPackage ./exe-only { inherit util; };
 
-#  # Run unit tests with: nix-instantiate --eval --strict -A unit.tests
-#  # An empty list means success.
-#  unit = let
-#    tests = haskell-nix.callPackage ./unit.nix {};
-#  in runCommand "unit-tests" { passthru = { inherit tests; }; }
-#     (lib.concatMapStringsSep "\n" (t: "echo ${t.name} failed") tests +
-#      (if builtins.length tests == 0 then "\ntouch $out" else "\nexit 1"));
+  # Run unit tests with: nix-instantiate --eval --strict -A unit.tests
+  # An empty list means success.
+  unit = let
+    tests = haskell-nix.callPackage ./unit.nix {};
+  in runCommand "unit-tests" { passthru = { inherit tests; }; }
+     (lib.concatMapStringsSep "\n" (t: "echo ${t.name} failed") tests +
+      (if builtins.length tests == 0 then "\ntouch $out" else "\nexit 1"));
 }
 
 ## more possible test cases
