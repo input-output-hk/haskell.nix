45849f1ef17a7e244554a60699976f2cfcb7a14a
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2019-11-18 22:44:02 +1300

    Test both nixpkgs release-19.03 and 19.03

--- release.nix
+++ release.nix
@@ -6,11 +6,6 @@
 
 let fixedNixpkgs = import ./nixpkgs {}; in
 with fixedNixpkgs.lib;
-with (import (fixedNixpkgs.path + "/pkgs/top-level/release-lib.nix") {
-  inherit supportedSystems scrubJobs nixpkgsArgs;
-  packageSet = import (haskell-nix + /build.nix);
-});
-
 let
   # Remove tests which have meta.disabled = true
   filterTests = let
@@ -22,19 +17,38 @@ let
 
   inherit (systems.examples) musl64 mingwW64;
 
-  jobs = {
-    native = filterTests (mapTestOn (packagePlatforms pkgs));
-    "${mingwW64.config}" = filterTests (mapTestOnCross mingwW64 (packagePlatforms pkgs));
-    # Disabled for now. Something is wrong and this would require `allowBroken`
-    # "${musl64.config}" = filterTests (mapTestOnCross musl64 (packagePlatforms pkgs));
-  } // {
-    # On IOHK Hydra, "required" is a special job that updates the
-    # GitHub CI status.
-    required = fixedNixpkgs.releaseTools.aggregate {
-      name = "haskell.nix-required";
-      meta.description = "All jobs required to pass CI";
-      constituents = collect isDerivation jobs.native;
-    };
-  };
+  jobs = nixpkgs-pin:
+    with (import (fixedNixpkgs.path + "/pkgs/top-level/release-lib.nix") {
+      inherit supportedSystems scrubJobs nixpkgsArgs;
+      packageSet = {
+            system ? builtins.currentSystem
+          , crossSystem ? null
+          , nixpkgsArgs ? { inherit system crossSystem; }
+          , ...}@args:
+        import (haskell-nix + /build.nix) (args // {
+          nixpkgsArgs = nixpkgsArgs // { inherit nixpkgs-pin; };
+      });
+    });
 
-in jobs
+    {
+      native = filterTests (mapTestOn (packagePlatforms pkgs));
+      # Disabled for now. Something is wrong and this would require `allowBroken`
+      # "${musl64.config}" = filterTests (mapTestOnCross musl64 (packagePlatforms pkgs));
+    } // (optionalAttrs (nixpkgs-pin == "release-19.03") {
+      "${mingwW64.config}" = filterTests (mapTestOnCross mingwW64 (packagePlatforms pkgs));
+    }) // {
+      # On IOHK Hydra, "required" is a special job that updates the
+      # GitHub CI status.
+      required = fixedNixpkgs.releaseTools.aggregate {
+        name = "haskell.nix-required";
+        meta.description = "All jobs required to pass CI";
+        constituents = collect isDerivation jobs.native;
+      };
+    };
+
+in builtins.mapAttrs (_: nixpkgs-pin: jobs nixpkgs-pin) {
+  "R1903" = "release-19.03";
+  "R1909" = "release-19.09";
+}
+
+
