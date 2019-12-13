input-output-hk/haskell.nix

commit 24669a42085aa18867ad0623e445e44f84bbb230

improve speed of make-config-files (#318) (#323)
caches `configure-flags`, `cabal.config`, and `ghc-environment` files
per library in the nix store, so the `make-config-files` script just has
to append them into the same named files for the package being built

instead of using `ghc-pkg dump` and `ghc-pkg register` for each
dependency, copies the `.conf` files from the dependencies
`package.conf.d` directory and then uses a single `ghc-pkg recache`

eliminate the call of `ghc-pkg check`

suppress some output of `ghc-pkg` (not related to speed improvements,
I'm flexible on rolling this bit back)

in my tests, the make-config-files scripts now only take seconds, even
for packages that have many many dependencies - prior to this change,
this script took more than a minute for such packages

builder/make-config-files.nix
@@ -1,10 +1,12 @@
-{ stdenv, lib, haskellLib, ghc, nonReinstallablePkgs, runCommand }:
+{ stdenv, lib, haskellLib, ghc, nonReinstallablePkgs, runCommand, writeText, writeScript }:
 
 let
   flagsAndConfig = field: xs: lib.optionalString (xs != []) ''
     echo ${lib.concatStringsSep " " (map (x: "--${field}=${x}") xs)} >> $out/configure-flags
     echo "${field}: ${lib.concatStringsSep " " xs}" >> $out/cabal.config
   '';
+
+  target-pkg = "${ghc.targetPrefix}ghc-pkg";
 
   # This is a bit of a hack.  So we'll have a slightly longer explaination here:
   # exactDep will pass --exact-configuration to the `SETUP_HS confiugre` command.
@@ -20,38 +22,57 @@ let
   #            sublib name to exactDep, as we don't have access to it at the call-site,
   #            we resort to a bit of globbing, which (as pkg db's should contain only
   #            a single package) work.
-  exactDep = pdbArg: p: ''
-    if id=$(target-pkg ${pdbArg} field ${p} id --simple-output); then
+  exactDep = pdbArg: p: nativeBuildInputs:  runCommand "${p}-exactdep" { inherit nativeBuildInputs; } ''
+    mkdir -p $out
+    touch $out/configure-flags
+    touch $out/cabal.config
+
+    if id=$(${target-pkg} ${pdbArg} field ${p} id --simple-output); then
       echo "--dependency=${p}=$id" >> $out/configure-flags
-    elif id=$(target-pkg ${pdbArg} field "z-${p}-z-*" id --simple-output); then
-      name=$(target-pkg ${pdbArg} field "z-${p}-z-*" name --simple-output)
+    elif id=$(${target-pkg} ${pdbArg} field "z-${p}-z-*" id --simple-output); then
+      name=$(${target-pkg} ${pdbArg} field "z-${p}-z-*" name --simple-output)
       # so we are dealing with a sublib. As we build sublibs separately, the above
       # query should be safe.
       echo "--dependency=''${name#z-${p}-z-}=$id" >> $out/configure-flags
     fi
-    if ver=$(target-pkg ${pdbArg} field ${p} version --simple-output); then
+    if ver=$(${target-pkg} ${pdbArg} field ${p} version --simple-output); then
       echo "constraint: ${p} == $ver" >> $out/cabal.config
       echo "constraint: ${p} installed" >> $out/cabal.config
     fi
   '';
 
-  envDep = pdbArg: p: ''
-    if id=$(target-pkg ${pdbArg} field ${p} id --simple-output); then
-      echo "package-id $id" >> $out/ghc-environment
+  catExactDep = dep: ''
+    cat ${dep}/configure-flags >> $out/configure-flags
+    cat ${dep}/cabal.config >> $out/cabal.config
+  '';
+
+  catPkgExactDep = p:
+    catExactDep (exactDep (packageDb p) p.identifier.name [ghc (p.components.library or p)]);
+
+  catGhcPkgExactDep = p: catExactDep (exactDep "" p [ghc]);
+
+  envDep = pdbArg: p: nativeBuildInputs: runCommand "${p}-envdep" { inherit nativeBuildInputs; } ''
+    touch $out
+    if id=$(${target-pkg} ${pdbArg} field ${p} id --simple-output); then
+      echo "package-id $id" >> $out
     fi
   '';
 
-in { identifier, component, fullName, flags ? {} }:
+  catEnvDep = ghcEnv: ''
+    cat ${ghcEnv} >> $out/ghc-environment
+  '';
 
+  catPkgEnvDep = p:
+    catEnvDep (envDep (packageDb p) p.identifier.name [ghc (p.components.library or p)]);
+
+  catGhcPkgEnvDep = p: catEnvDep (envDep "" p [ghc]);
+
+  packageDb = p: "--package-db ${p.components.library or p}/package.conf.d";
+in { identifier, component, fullName, flags ? {} }:
   runCommand "${fullName}-config" { nativeBuildInputs = [ghc]; } (''
     mkdir -p $out
 
-    # Calls ghc-pkg for the target platform
-    target-pkg() {
-      ${ghc.targetPrefix}ghc-pkg "$@"
-    }
-
-    target-pkg init $out/package.conf.d
+    ${target-pkg} init $out/package.conf.d
 
     ${lib.concatStringsSep "\n" (lib.mapAttrsToList flagsAndConfig {
       "extra-lib-dirs" = map (p: "${lib.getLib p}/lib") component.libs;
@@ -60,14 +81,12 @@ in { identifier, component, fullName, flags ? {} }:
     })}
 
     # Copy over the nonReinstallablePkgs from the global package db.
-    # Note: we need to use --global-package-db with ghc-pkg to prevent it
-    #       from looking into the implicit global package db when registering the package.
     ${lib.concatMapStringsSep "\n" (p: ''
-      target-pkg describe ${p} | target-pkg --force --global-package-db $out/package.conf.d register - || true
+      find ${ghc}/lib/${ghc.name}/package.conf.d -name '${p}*.conf' -exec cp -f {} $out/package.conf.d \;
     '') nonReinstallablePkgs}
 
     ${lib.concatMapStringsSep "\n" (p: ''
-      target-pkg --package-db ${p}/package.conf.d dump | target-pkg --force --package-db $out/package.conf.d register -
+      cp -f "${p}/package.conf.d/"*.conf $out/package.conf.d
     '') (haskellLib.flatLibDepends component)}
 
     # Note: we pass `clear` first to ensure that we never consult the implicit global package db.
@@ -82,16 +101,16 @@ in { identifier, component, fullName, flags ? {} }:
     cat > $out/ghc-environment <<EOF
     package-db $out/package.conf.d
     EOF
-    ${lib.concatMapStringsSep "\n" (p: envDep "--package-db ${p.components.library or p}/package.conf.d" p.identifier.name) component.depends}
-    ${lib.concatMapStringsSep "\n" (envDep "") (lib.remove "ghc" nonReinstallablePkgs)}
 
+    ${lib.concatMapStringsSep "\n" catPkgEnvDep component.depends}
+    ${lib.concatMapStringsSep "\n" catGhcPkgEnvDep (lib.remove "ghc" nonReinstallablePkgs)}
   '' + lib.optionalString component.doExactConfig ''
     echo "--exact-configuration" >> $out/configure-flags
     echo "allow-newer: ${identifier.name}:*" >> $out/cabal.config
     echo "allow-older: ${identifier.name}:*" >> $out/cabal.config
 
-    ${lib.concatMapStringsSep "\n" (p: exactDep "--package-db ${p.components.library or p}/package.conf.d" p.identifier.name) component.depends}
-    ${lib.concatMapStringsSep "\n" (exactDep "") nonReinstallablePkgs}
+    ${lib.concatMapStringsSep "\n" catPkgExactDep component.depends}
+    ${lib.concatMapStringsSep "\n" catGhcPkgExactDep nonReinstallablePkgs}
 
   ''
   # This code originates in the `generic-builder.nix` from nixpkgs.  However GHC has been fixed
@@ -124,7 +143,5 @@ in { identifier, component, fullName, flags ? {} }:
       sed -i "s,dynamic-library-dirs: .*,dynamic-library-dirs: $dynamicLinksDir," $f
     done
   '' + ''
-    target-pkg --package-db $out/package.conf.d recache
-  '' + ''
-    target-pkg --package-db $out/package.conf.d check
+    ${target-pkg} -v0 --package-db $out/package.conf.d recache
   '')

commit 300fff437e08bbd67be6200d841a2ada7f347715

Move envDeps and exactDeps into lib and ghc (#360)
This change moves the work done to in `envDeps` and `exactDeps`
from `make-config-files.nix` into the library components and
the `ghc` compiler component (well `passthru` derivations on the
`ghc` component).

This greatly reduces the number of times `runCommand` is
used while nix is working out what to build.

This fixes a performance regression in #323 for builds where
little or no work needs to be done.  While maintaining the
performance improvement in #323 that resulted from reducing the
number of calls to `ghc-pkg` when significant work does need
to be done.

builder/comp-builder.nix
@@ -259,7 +259,9 @@ stdenv.mkDerivation ({
   #
   # Note 3: if a package has no libs SETUP will not generate anything.  This can
   #         happen when building the `all` component of a package.
-  installPhase = ''
+  installPhase = let
+      target-pkg-and-db = "${ghc.targetPrefix}ghc-pkg -v0 --package-db $out/package.conf.d";
+    in ''
     runHook preInstall
     $SETUP_HS copy ${lib.concatStringsSep " " component.setupInstallFlags}
     ${lib.optionalString (haskellLib.isLibrary componentId || haskellLib.isAll componentId) ''
@@ -271,6 +273,28 @@ stdenv.mkDerivation ({
         done
       elif [ -e "${name}.conf" ]; then
         ${ghc.targetPrefix}ghc-pkg -v0 --package-db ${configFiles}/package.conf.d -f $out/package.conf.d register ${name}.conf
+      fi
+
+      mkdir -p $out/exactDep
+      touch $out/exactDep/configure-flags
+      touch $out/exactDep/cabal.config
+
+      if id=$(${target-pkg-and-db} field ${package.identifier.name} id --simple-output); then
+        echo "--dependency=${package.identifier.name}=$id" >> $out/exactDep/configure-flags
+      elif id=$(${target-pkg-and-db} field "z-${package.identifier.name}-z-*" id --simple-output); then
+        name=$(${target-pkg-and-db} field "z-${package.identifier.name}-z-*" name --simple-output)
+        # so we are dealing with a sublib. As we build sublibs separately, the above
+        # query should be safe.
+        echo "--dependency=''${name#z-${package.identifier.name}-z-}=$id" >> $out/exactDep/configure-flags
+      fi
+      if ver=$(${target-pkg-and-db} field ${package.identifier.name} version --simple-output); then
+        echo "constraint: ${package.identifier.name} == $ver" >> $out/exactDep/cabal.config
+        echo "constraint: ${package.identifier.name} installed" >> $out/exactDep/cabal.config
+      fi
+
+      touch $out/envDep
+      if id=$(${target-pkg-and-db} field ${package.identifier.name} id --simple-output); then
+        echo "package-id $id" >> $out/envDep
       fi
     ''}
     ${(lib.optionalString (haskellLib.isTest componentId || haskellLib.isBenchmark componentId || haskellLib.isAll componentId) ''

builder/make-config-files.nix
@@ -9,12 +9,17 @@ let
   target-pkg = "${ghc.targetPrefix}ghc-pkg";
 
   # This is a bit of a hack.  So we'll have a slightly longer explaination here:
+
+  # Every library component built with `comp-builder.nix` includes an `exactDep`
+  # and `envDep` directory with precomputed values used here.
+  # GHC derivations include `exactDep` and `envDep` derivations that have
+  # the same information for each of the built in packages.
+
   # exactDep will pass --exact-configuration to the `SETUP_HS confiugre` command.
   # This requires us to pass --dependency={dep name}={pkg id}.  The dependency
-  # name will usually be the name of the package `p`, which we can locate in the
-  # package-db, passed in via `pdbArg`.  Thus querying the package-db for the
-  # id field for package `p`, will unsually provide is with the right value.  Sublibs
-  # need a bit of special handling:
+  # name will usually be the name of the package `p`, that will have been located
+  # in the suitable package db when the dependency (along with `exactDep` and `envDep`)
+  # was built.  Sublibs need a bit of special handling:
   #
   # - Sublibs: if the dependency is a sublibrary of a package, we need to use
   #            the sublibrary's name for the dep name, and lookup the sublibraries
@@ -22,52 +27,33 @@ let
   #            sublib name to exactDep, as we don't have access to it at the call-site,
   #            we resort to a bit of globbing, which (as pkg db's should contain only
   #            a single package) work.
-  exactDep = pdbArg: p: nativeBuildInputs:  runCommand "${p}-exactdep" { inherit nativeBuildInputs; } ''
-    mkdir -p $out
-    touch $out/configure-flags
-    touch $out/cabal.config
 
-    if id=$(${target-pkg} ${pdbArg} field ${p} id --simple-output); then
-      echo "--dependency=${p}=$id" >> $out/configure-flags
-    elif id=$(${target-pkg} ${pdbArg} field "z-${p}-z-*" id --simple-output); then
-      name=$(${target-pkg} ${pdbArg} field "z-${p}-z-*" name --simple-output)
-      # so we are dealing with a sublib. As we build sublibs separately, the above
-      # query should be safe.
-      echo "--dependency=''${name#z-${p}-z-}=$id" >> $out/configure-flags
-    fi
-    if ver=$(${target-pkg} ${pdbArg} field ${p} version --simple-output); then
-      echo "constraint: ${p} == $ver" >> $out/cabal.config
-      echo "constraint: ${p} installed" >> $out/cabal.config
+  getLibComponent = dep:
+       dep.components.library # Regular package dependency
+    or dep;                   # or a sublib 
+  
+  catPkgExactDep = p: ''
+    cat ${getLibComponent p}/exactDep/configure-flags >> $out/configure-flags
+    cat ${getLibComponent p}/exactDep/cabal.config >> $out/cabal.config
+  '';
+
+  catGhcPkgExactDep = p: ''
+    if [ -e ${ghc.exactDeps}/${p} ]; then
+      cat ${ghc.exactDeps}/${p}/configure-flags >> $out/configure-flags
+      cat ${ghc.exactDeps}/${p}/cabal.config >> $out/cabal.config
     fi
   '';
 
-  catExactDep = dep: ''
-    cat ${dep}/configure-flags >> $out/configure-flags
-    cat ${dep}/cabal.config >> $out/cabal.config
+  catPkgEnvDep = p: ''
+    cat ${getLibComponent p}/envDep >> $out/ghc-environment
   '';
 
-  catPkgExactDep = p:
-    catExactDep (exactDep (packageDb p) p.identifier.name [ghc (p.components.library or p)]);
-
-  catGhcPkgExactDep = p: catExactDep (exactDep "" p [ghc]);
-
-  envDep = pdbArg: p: nativeBuildInputs: runCommand "${p}-envdep" { inherit nativeBuildInputs; } ''
-    touch $out
-    if id=$(${target-pkg} ${pdbArg} field ${p} id --simple-output); then
-      echo "package-id $id" >> $out
+  catGhcPkgEnvDep = p: ''
+    if [ -e ${ghc.envDeps}/${p} ]; then
+      cat ${ghc.envDeps}/${p} >> $out/ghc-environment
     fi
   '';
 
-  catEnvDep = ghcEnv: ''
-    cat ${ghcEnv} >> $out/ghc-environment
-  '';
-
-  catPkgEnvDep = p:
-    catEnvDep (envDep (packageDb p) p.identifier.name [ghc (p.components.library or p)]);
-
-  catGhcPkgEnvDep = p: catEnvDep (envDep "" p [ghc]);
-
-  packageDb = p: "--package-db ${p.components.library or p}/package.conf.d";
 in { identifier, component, fullName, flags ? {} }:
   runCommand "${fullName}-config" { nativeBuildInputs = [ghc]; } (''
     mkdir -p $out
@@ -86,8 +72,9 @@ in { identifier, component, fullName, flags ? {} }:
     '') nonReinstallablePkgs}
 
     ${lib.concatMapStringsSep "\n" (p: ''
-      cp -f "${p}/package.conf.d/"*.conf $out/package.conf.d
-    '') (haskellLib.flatLibDepends component)}
+      cp -f "${(getLibComponent p).configFiles}/package.conf.d/"*.conf $out/package.conf.d
+      cp -f "${getLibComponent p}/package.conf.d/"*.conf $out/package.conf.d
+    '') component.depends}
 
     # Note: we pass `clear` first to ensure that we never consult the implicit global package db.
     ${flagsAndConfig "package-db" ["clear" "$out/package.conf.d"]}

compiler/ghc/default.nix
@@ -13,6 +13,8 @@
 
 , libiconv ? null, ncurses
 
+, exactDeps, envDeps
+
 , # GHC can be built with system libffi or a bundled one.
   libffi ? null
 
@@ -204,7 +206,7 @@ in let configured-src = stdenv.mkDerivation (rec {
         installPhase = "cp -r . $out";
     });
 
-in stdenv.mkDerivation (rec {
+  drv =stdenv.mkDerivation (rec {
   version = ghc-version;
   name = "${targetPrefix}ghc-${version}";
 
@@ -313,6 +315,10 @@ in stdenv.mkDerivation (rec {
 
     # Used to detect non haskell-nix compilers (accedental use of nixpkgs compilers can lead to unexpected errors)
     isHaskellNixCompiler = true;
+
+    # These are used in make-config-files.nix
+    exactDeps = exactDeps drv;
+    envDeps = envDeps drv;
   } // extra-passthru;
 
   meta = {
@@ -326,4 +332,6 @@ in stdenv.mkDerivation (rec {
   dontStrip = true;
   dontPatchELF = true;
   noAuditTmpdir = true;
-})
+});
+
+in drv

overlays/bootstrap.nix
@@ -1,4 +1,36 @@
-self: super: {
+self: super:
+let
+    exactDeps = ghc: self.runCommand "ghc-exactdep-${ghc.version}" { nativeBuildInputs = [ ghc ]; } ''
+      for P in $(${ghc.targetPrefix}ghc-pkg list --simple-output | sed 's/-[0-9][0-9.]*//g'); do
+        mkdir -p $out/$P
+        touch $out/$P/configure-flags
+        touch $out/$P/cabal.config
+
+        if id=$(${ghc.targetPrefix}ghc-pkg field $P id --simple-output); then
+          echo "--dependency=$P=$id" >> $out/$P/configure-flags
+        elif id=$(${ghc.targetPrefix}ghc-pkg field "z-$P-z-*" id --simple-output); then
+          name=$(${ghc.targetPrefix}ghc-pkg field "z-$P-z-*" name --simple-output)
+          # so we are dealing with a sublib. As we build sublibs separately, the above
+          # query should be safe.
+          echo "--dependency=''${name#z-$P-z-}=$id" >> $out/$P/configure-flags
+        fi
+        if ver=$(${ghc.targetPrefix}ghc-pkg field $P version --simple-output); then
+          echo "constraint: $P == $ver" >> $out/$P/cabal.config
+          echo "constraint: $P installed" >> $out/$P/cabal.config
+        fi
+      done
+    '';
+
+    envDeps = ghc: self.runCommand "ghc-envdep-${ghc.version}" { nativeBuildInputs = [ ghc ]; } ''
+      mkdir -p $out
+      for P in $(${ghc.targetPrefix}ghc-pkg list --simple-output | sed 's/-[0-9.]*//g'); do
+        touch $out/$P
+        if id=$(${ghc.targetPrefix}ghc-pkg field $P id --simple-output); then
+          echo "package-id $id" >> $out/$P
+        fi
+      done
+    '';
+in {
   haskell-nix = super.haskell-nix // {
     # Use this to disable the existing haskell infra structure for testing purposes
     compiler =
@@ -73,7 +105,7 @@ self: super: {
             ghc844 = self.callPackage ../compiler/ghc {
                 extra-passthru = { buildGHC = self.buildPackages.haskell-nix.compiler.ghc844; };
 
-                inherit bootPkgs sphinx;
+                inherit bootPkgs sphinx exactDeps envDeps;
 
                 buildLlvmPackages = self.buildPackages.llvmPackages_5;
                 llvmPackages = self.llvmPackages_5;
@@ -91,7 +123,7 @@ self: super: {
             ghc861 = self.callPackage ../compiler/ghc {
                 extra-passthru = { buildGHC = self.buildPackages.haskell-nix.compiler.ghc861; };
 
-                inherit bootPkgs sphinx;
+                inherit bootPkgs sphinx exactDeps envDeps;
 
                 buildLlvmPackages = self.buildPackages.llvmPackages_5;
                 llvmPackages = self.llvmPackages_5;
@@ -107,7 +139,7 @@ self: super: {
             ghc862 = self.callPackage ../compiler/ghc {
                 extra-passthru = { buildGHC = self.buildPackages.haskell-nix.compiler.ghc862; };
 
-                inherit bootPkgs sphinx;
+                inherit bootPkgs sphinx exactDeps envDeps;
 
                 buildLlvmPackages = self.buildPackages.llvmPackages_5;
                 llvmPackages = self.llvmPackages_5;
@@ -124,7 +156,7 @@ self: super: {
             ghc863 = self.callPackage ../compiler/ghc {
                 extra-passthru = { buildGHC = self.buildPackages.haskell-nix.compiler.ghc863; };
 
-                inherit bootPkgs sphinx;
+                inherit bootPkgs sphinx exactDeps envDeps;
 
                 buildLlvmPackages = self.buildPackages.llvmPackages_5;
                 llvmPackages = self.llvmPackages_5;
@@ -141,7 +173,7 @@ self: super: {
             ghc864 = self.callPackage ../compiler/ghc {
                 extra-passthru = { buildGHC = self.buildPackages.haskell-nix.compiler.ghc864; };
 
-                inherit bootPkgs sphinx;
+                inherit bootPkgs sphinx exactDeps envDeps;
 
                 buildLlvmPackages = self.buildPackages.llvmPackages_5;
                 llvmPackages = self.llvmPackages_5;
@@ -158,7 +190,7 @@ self: super: {
             ghc865 = self.callPackage ../compiler/ghc {
                 extra-passthru = { buildGHC = self.buildPackages.haskell-nix.compiler.ghc865; };
 
-                inherit bootPkgs sphinx;
+                inherit bootPkgs sphinx exactDeps envDeps;
 
                 buildLlvmPackages = self.buildPackages.llvmPackages_5;
                 llvmPackages = self.llvmPackages_5;
@@ -231,7 +263,13 @@ self: super: {
         # compilers into the same place where nix expects them.
         # We mark these compilers as boot compilers to make sure they are only used
         # where a boot compiler is expected.
-        compiler = builtins.mapAttrs (_: v: v // { isHaskellNixBootCompiler = true; })
+        compiler = builtins.mapAttrs (_: v:
+            v // {
+              isHaskellNixBootCompiler = true;
+              exactDeps = exactDeps v;
+              envDeps = envDeps v;
+            }
+          )
           (import ../compiler/old-ghc-nix { pkgs = self; });
 
         packages = {