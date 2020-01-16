06117e95e2260883e4b8ecef964ebceffc947322
Moritz Angermann <moritz.angermann@gmail.com>
2020-01-16 22:33:12 +0800

    Update default.nix

--- default.nix
+++ default.nix
@@ -2,4 +2,8 @@
 #   import <nixpkgs> (import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz))
 { config   = import ./config.nix;
   overlays = import ./overlays;
+  # convenience function. To make it possible to call
+  #   import <nixpkgs> (import (...).withArgs { system = builtins.currentSystem; crossSystem = null; config = { ... }; overlays = [...]; }
+  # instead of having this convoluted args reshuffling in the client code.
+  withArgs = args: args // { config = config // args.config; overlays = overlays ++ args.overlays; };
 }
