b8d4aeada4e9d67772472e4a3bef070348968faf
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-02-25 21:43:48 +1300

    Add ghc 8.8.3 and set idfLevel 0

--- overlays/haskell.nix
+++ overlays/haskell.nix
@@ -477,7 +477,7 @@ self: super: {
         };
 
         haskellNixRoots' = ifdLevel:
-            let filterSupportedGhc = self.lib.filterAttrs (n: _: n == "ghc865" || n == "ghc882");
+            let filterSupportedGhc = self.lib.filterAttrs (n: _: n == "ghc865" || n == "ghc883");
           in self.recurseIntoAttrs ({
             # Things that require no IFD to build
             inherit (self.buildPackages.haskell-nix) nix-tools source-pins;
@@ -491,7 +491,7 @@ self: super: {
             happy = self.buildPackages.haskell-nix.bootstrap.packages.happy;
             hscolour = self.buildPackages.haskell-nix.bootstrap.packages.hscolour;
             ghc865 = self.buildPackages.haskell-nix.compiler.ghc865;
-            ghc882 = self.buildPackages.haskell-nix.compiler.ghc882;
+            ghc883 = self.buildPackages.haskell-nix.compiler.ghc883;
             ghc-extra-projects = self.recurseIntoAttrs (builtins.mapAttrs (_: proj: withInputs proj.plan-nix)
               (filterSupportedGhc self.ghc-extra-projects));
           } // self.lib.optionalAttrs (ifdLevel > 1) {
