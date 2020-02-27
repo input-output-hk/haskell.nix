6b16cad760a29b80575329add5b63c7f488b4bdc
Michael Bishop <cleverca22@gmail.com>
2020-02-27 04:49:24 -0400

    ensure that hydra caches the old-ghc-nix used at eval time

--- release.nix
+++ release.nix
@@ -16,6 +16,12 @@ let
     tests = filterAttrs (_: nonEmpty) (mapAttrs (name: removeDisabled) jobs.tests);
   };
 
+  mkPins = inputs: defaultNixpkgs.runCommand "ifd-pins" {} ''
+    mkdir $out
+    cd $out
+    ${concatMapStringsSep "\n" (input: "ln -sv ${input.value} ${input.key}") (attrValues (mapAttrs (key: value: { inherit key value; }) inputs))}
+  '';
+
   jobs = nixpkgs-pin:
     # Now we know what nixpkgs-pin we are testing use that one.
     # This fixes and issue where the vendor has changed from `x86_64-pc-mingw32`
@@ -50,6 +56,15 @@ let
     };
 
 in allJobs // {
+    ifd-pins = mkPins {
+      # hydra is not aware of anything depended on at eval time (via IFD), so it wont try to cache those things
+      # this job will expose all of those derivations as deps of the `ifd-pins` derivation, which tricks hydra into caching them
+      # that will then speed up evals for end-users that are using hydra as a cache
+      old-ghc-nix = let spec = __fromJSON (__readFile ./compiler/old-ghc-nix/old-ghc-nix.json); in defaultNixpkgs.fetchgit {
+        name = "old-ghc-nix";
+        inherit (spec) sha256 url rev;
+      };
+    };
     # On IOHK Hydra, "required" is a special job that updates the
     # GitHub CI status.
     required = defaultNixpkgs.releaseTools.aggregate {
