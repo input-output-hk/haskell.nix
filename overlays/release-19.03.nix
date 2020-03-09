b0832425a158281254d993a949348ec64b7ff39e
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-03-09 15:47:39 +1300

    Use upstream branch for 19.03 and override openssl

--- overlays/release-19.03.nix
+++ overlays/release-19.03.nix
@@ -12,4 +12,30 @@ self: super: super.lib.optionalAttrs (super.lib.versions.majorMinor super.lib.ve
              export TMPDIR=/tmp
            '';
     });
+    openssl = if self.stdenv.hostPlatform.isWindows
+      then let static = false;  # TODO check that it is ok to assume this (or find a way to detect without instanciating perl)
+        in super.openssl.overrideAttrs (attrs: {
+          postInstall = self.stdenv.lib.optionalString (!static) ''
+            # If we're building dynamic libraries, then don't install static
+            # libraries.
+            if [ -n "$(echo $out/lib/*.so $out/lib/*.dylib $out/lib/*.dll)" ]; then
+                rm "$out/lib/"*.a
+            fi
+
+          '' +
+          ''
+            mkdir -p $bin
+            mv $out/bin $bin/
+
+            mkdir $dev
+            mv $out/include $dev/
+
+            # remove dependency on Perl at runtime
+            rm -r $out/etc/ssl/misc
+
+            rmdir $out/etc/ssl/{certs,private}
+          '';
+        postFixup = "";
+      })
+      else super.openssl;
 }
