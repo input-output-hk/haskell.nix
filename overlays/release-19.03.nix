642636b47b9bce32d68554e9a935eff120fd95a1
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-03-10 14:53:40 +1300

    Update nixpkgs pins (#477)
    This change updates the pins for nixpkgs to the latest upstream nixpkgs-19.*-darwin branches. It also moves the remaining fixes from the haskell-nix/channels/nixpkgs-19.*-darwin branches into overlays in haskell.nix.

--- overlays/release-19.03.nix
+++ overlays/release-19.03.nix
@@ -12,4 +12,53 @@ self: super: super.lib.optionalAttrs (super.lib.versions.majorMinor super.lib.ve
              export TMPDIR=/tmp
            '';
     });
+    # Avoid dependency on perl for windows as it fails to build.
+    openssl = if self.stdenv.hostPlatform.isWindows
+      then let static = false;  # TODO check that it is ok to assume this (or find a way to detect without instanciating perl)
+        in super.openssl.overrideAttrs (attrs: {
+          # This is the `postInstall` from 19.03 nixpkgs but with the line
+          #   substituteInPlace $out/bin/c_rehash --replace ${buildPackages.perl} ${perl}
+          # removed (as including it requires perl to build).
+          # (code is copied here because modifying `attrs.postInstall` would still
+          # introducing the same dependency)
+          postInstall = self.stdenv.lib.optionalString (!static) ''
+            # If we're building dynamic libraries, then don't install static
+            # libraries.
+            if [ -n "$(echo $out/lib/*.so $out/lib/*.dylib $out/lib/*.dll)" ]; then
+                rm "$out/lib/"*.a
+            fi
+
+          '' +
+          ''
+            mkdir -p $bin
+            mv $out/bin $bin/
+
+            mkdir $dev
+            mv $out/include $dev/
+
+            # remove dependency on Perl at runtime
+            rm -r $out/etc/ssl/misc
+
+            rmdir $out/etc/ssl/{certs,private}
+          '';
+        # Skip this step on windows as it checks for references to buildPackages.perl
+        # and we are keeping them.
+        postFixup = "";
+      })
+      else super.openssl;
+    winePackages = super.winePackages // {
+      minimal = super.winePackages.minimal.overrideAttrs (oldAttrs: {
+        # Needed for CFNotificationCenterAddObserver symbols.
+        buildInputs = oldAttrs.buildInputs ++ self.lib.optional self.stdenv.isDarwin
+	      self.buildPackages.darwin.cf-private;
+        # the relevant check in configure.ac will fail under nix as we are not
+        # using the system supplied compiler tools. We will subsequently *not*
+        # pass `-mmacosx-version-min` in the LDFLAGS, and end up with a linked
+        # binary against 10.14 which *does* contain a __DATA,__dyld section and
+        # then horribly fail to run on Mojave.
+        postConfigure = self.lib.optional (self.stdenv.hostPlatform.isDarwin) ''
+          sed -i 's|-nostartfiles -nodefaultlibs|-nostartfiles -nodefaultlibs -mmacosx-version-min=10.7|g' loader/Makefile
+        '';
+      });
+    };
 }
