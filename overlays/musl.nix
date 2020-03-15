4524d857c3f532737188606c2d86800fbd3f0442
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-03-15 20:00:44 +1300

    Just fix linux-x86_64 vs linux-generic64 issue

--- overlays/musl.nix
+++ overlays/musl.nix
@@ -1,9 +1,10 @@
 self: super: super.lib.optionalAttrs super.stdenv.hostPlatform.isMusl {
-  # musl appears to need a static openssl to build
-  openssl = (super.openssl.override { static = true; })
-    .overrideAttrs (attrs: {
+  # On nixpkgs 19.09 openssl is configured as `linux-generic64` instead
+  # of `linux-x86_64` and as a result the `asm` parts of of openssl
+  # are not built.  Because the `no_asm` configure flag is also not passed
+  # the c versions of the functions are also not included.
+  openssl = super.openssl.overrideAttrs (attrs:
+    super.lib.optionalAttrs super.stdenv.hostPlatform.isx86_64 {
       configureScript = "./Configure linux-x86_64";
-      configureFlags = attrs.configureFlags
-        ++ super.lib.optional (super.lib.versionAtLeast attrs.version "1.1.0") "no-shared";
     });
 }
