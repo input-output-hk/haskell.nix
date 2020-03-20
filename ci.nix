33dc7bce3444f8957dec9dccb484dd4b40ee463d
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2020-03-20 07:52:34 +0000

    Tidy ci.nix using dimension (#500)
    I recently came across this rather nice way of structuring build
    matrices for ci.nix.
    
    I think it makes the haskell.nix ci.nix a lot clearer!

--- ci.nix
+++ ci.nix
@@ -3,66 +3,42 @@
 # - https://github.com/NixOS/nixpkgs/pull/68398
 
 let
+  inherit (import ./dimension.nix) dimension;
   nixpkgsVersions = {
 #   "release-18.09" = builtins.fetchTarball "https://github.com/input-output-hk/nixpkgs/archive/7e4dcacbf066a8e2d12693a9de1fb30c77081c5d.tar.gz";
     "release-19.03" = builtins.fetchTarball "https://github.com/input-output-hk/nixpkgs/archive/a8f81dc037a5977414a356dd068f2621b3c89b60.tar.gz";
     # "release-19.09" = builtins.fetchTarball "https://github.com/input-output-hk/nixpkgs/archive/3d623a406cec9052ae0a16a79ce3ce9de11236bb.tar.gz";
   };
+  systems = {
+    "x86_64-linux" = {};
+  };
+  crossSystems = {
+    "x86_64-pc-mingw32" = {};
+  };
   haskellNixArgs = import ./.;
-  defaultNixpkgs = import (nixpkgsVersions."release-19.03") {};
-  recRecurseIntoAttrs = with defaultNixpkgs; pred: x: if pred x then recurseIntoAttrs (lib.mapAttrs (n: v: if n == "buildPackages" then v else recRecurseIntoAttrs pred v) x) else x;
 in
-  recRecurseIntoAttrs (x: with defaultNixpkgs; lib.isAttrs x && !lib.isDerivation x) (
-    builtins.mapAttrs (nixpkgsName: nixpkgsSrc: with (import nixpkgsSrc {}); {
-      x86_64-linux = {
-            hello = with (import nixpkgsSrc (haskellNixArgs // { system = "x86_64-linux"; }));
-                (haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
-            x86_64-pc-mingw32-hello = with (import nixpkgsSrc (haskellNixArgs // { system = "x86_64-linux"; crossSystem.config = "x86_64-pc-mingw32"; }));
-                (haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
+dimension "Nixpkgs version" nixpkgsVersions (nixpkgsName: nixpkgsSrc:
+  dimension "System" systems (system: _:
+    # Native builds
+    # TODO: can we merge this into the general case by picking an appropriate "cross system" to mean native?
+    let pkgs = import nixpkgsSrc (haskellNixArgs // { inherit system; });
+    in pkgs.recurseIntoAttrs {
+      hello = (pkgs.haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
+      iserv-proxy = pkgs.ghc-extra-packages.ghc865.iserv-proxy.components.exes.iserv-proxy;
+      ghc = pkgs.recurseIntoAttrs pkgs.haskell-nix.compiler;
+      tests = pkgs.lib.optionalAttrs (system == "x86_64-linux") (import ./test { inherit pkgs; });
+    }
+    //
+    dimension "Cross system" crossSystems (crossSystem: _:
+      # Cross builds
+      let pkgs = import nixpkgsSrc (haskellNixArgs // { inherit system; crossSystem.config = crossSystem; });
+      in pkgs.recurseIntoAttrs {
+        hello = (pkgs.haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
 
-            iserv-proxy = with (import nixpkgsSrc (haskellNixArgs // { system = "x86_64-linux"; }));
-                (ghc-extra-packages.ghc865.iserv-proxy.components.exes).iserv-proxy;
+        iserv-proxy = pkgs.ghc-extra-packages.ghc865.iserv-proxy.components.exes.iserv-proxy;
 
-            x86_64-pc-mingw32-iserv-proxy = with (import nixpkgsSrc (haskellNixArgs // { system = "x86_64-linux"; crossSystem.config = "x86_64-pc-mingw32"; }));
-                (buildPackages.ghc-extra-packages.ghc865.iserv-proxy.components.exes).iserv-proxy;
-
-            x86_64-pc-mingw32-remote-iserv = with (import nixpkgsSrc (haskellNixArgs // { system = "x86_64-linux"; crossSystem.config = "x86_64-pc-mingw32"; }));
-                (ghc-extra-packages.ghc865.remote-iserv.components.exes).remote-iserv;
-
-      };
-      x86_64-darwin = {
-            hello = with (import nixpkgsSrc (haskellNixArgs // { system = "x86_64-darwin"; }));
-                (haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
-            x86_64-pc-mingw32-hello = with (import nixpkgsSrc (haskellNixArgs // { system = "x86_64-darwin"; crossSystem.config = "x86_64-pc-mingw32"; }));
-                (haskell-nix.hackage-package { name = "hello"; version = "1.0.0.2";}).components.exes.hello;
-
-            iserv-proxy = with (import nixpkgsSrc (haskellNixArgs // { system = "x86_64-darwin"; }));
-                (ghc-extra-packages.ghc865.iserv-proxy.components.exes).iserv-proxy;
-
-            x86_64-pc-mingw32-iserv-proxy = with (import nixpkgsSrc (haskellNixArgs // { system = "x86_64-darwin"; crossSystem.config = "x86_64-pc-mingw32"; }));
-                (buildPackages.ghc-extra-packages.ghc865.iserv-proxy.components.exes).iserv-proxy;
-
-            x86_64-pc-mingw32-remote-iserv = with (import nixpkgsSrc (haskellNixArgs // { system = "x86_64-darwin"; crossSystem.config = "x86_64-pc-mingw32"; }));
-                (ghc-extra-packages.ghc865.remote-iserv.components.exes).remote-iserv;
-
-      };
-      haskell-nix.compiler = {
-        x86_64-linux = with (import nixpkgsSrc (haskellNixArgs // { system = "x86_64-linux"; }));
-            haskell-nix.compiler;
-        x86_64-darwin = with (import nixpkgsSrc (haskellNixArgs // { system = "x86_64-darwin";}));
-            haskell-nix.compiler;
-      };
-      tests = {
-        x86_64-linux = (import ./test { nixpkgs = nixpkgsSrc; nixpkgsArgs = { system = "x86_64-linux"; }; });
-        # x86_64-darwin = (import ./test { nixpkgs = nixpkgsSrc; nixpkgsArgs = { system = "x86_64-darwin"; }; });
-      };
-
-# Don't build (all of) stackage on linux for now.
-#    stackage = {
-#        x86_64-linux = (with (import nixpkgsSrc (haskellNixArgs // { system = "x86_64-linux"; }));
-#            haskell-nix.snapshots."lts-13.29");
-#        # x86_64-darwin = (with (import nixpkgsSrc (haskellNixArgs // { system = "x86_64-darwin"; }));
-#        #     haskell-nix.snapshots."lts-13.29");
-#    };
-
-}) nixpkgsVersions)
+        remote-iserv = pkgs.ghc-extra-packages.ghc865.remote-iserv.components.exes.remote-iserv;
+      }
+    )
+  )
+)
