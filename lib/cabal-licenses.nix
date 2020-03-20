cb3acc6bd598edc6bc28896c78cc0a9a4ee1d679
Zhenya Vinogradov <zhenyavinogradov@gmail.com>
2020-03-20 10:15:03 +0300

    Add AGPL-3.0-only and AGPL-3.0-or-later to cabal-licenses.nix (#503)

--- lib/cabal-licenses.nix
+++ lib/cabal-licenses.nix
@@ -4,11 +4,13 @@ lib: with lib.licenses;
   MIT = mit;
   "MPL-2.0" = mpl20;
   ISC = isc;
-  "LGPL-2.1-only" = lgpl21;
-  "LGPL-3.0-only" = lgpl3;
-  "GPL-2.0-only"  = gpl2;
-  "GPL-3.0-only"  = gpl3;
-  "Apache-2.0"    = asl20;
+  "LGPL-2.1-only"     = lgpl21;
+  "LGPL-3.0-only"     = lgpl3;
+  "GPL-2.0-only"      = gpl2;
+  "GPL-3.0-only"      = gpl3;
+  "AGPL-3.0-only"     = agpl3;
+  "AGPL-3.0-or-later" = agpl3Plus;
+  "Apache-2.0"        = asl20;
   # Generic
   LicenseRef-Apache = "Apache";
   LicenseRef-GPL = "GPL";
