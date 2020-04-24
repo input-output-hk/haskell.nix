829cba55bc1c09e2a11aa582ebe1ee81f5e760e4
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-04-24 13:57:36 +1200

    Fix lookup-sha256 test for musl and windows cross (#558)

--- test/lookup-sha256/default.nix
+++ test/lookup-sha256/default.nix
@@ -1,4 +1,4 @@
-{ haskell-nix, testSrc } :
+{ pkgs, lib, stdenv, haskell-nix, testSrc, zlib } :
 let
   pandoc = haskell-nix.hackage-package {
     name         = "pandoc";
@@ -10,6 +10,23 @@ let
       { "https://github.com/jgm/pandoc-citeproc"."0.17"
           = "0dxx8cp2xndpw3jwiawch2dkrkp15mil7pyx7dvd810pwc22pm2q"; }
         ."${location}"."${tag}";
+    modules = [
+      # Windows characters confuse cross compilation
+      # See https://github.com/snoyberg/file-embed/pull/33
+      {
+        packages.file-embed.src = pkgs.fetchgit {
+          url = "https://github.com/hamishmack/file-embed.git";
+          rev = "12b33b8b710517308954c1facff3dc679c2dc5e3";
+          sha256 = "0jcpja4s4cylmg9rddyakb1p1fb4l41ffwmy0njpb1dxc5z3v618";
+        };
+      }
+      # Musl needs static zlib
+      (lib.optionalAttrs stdenv.hostPlatform.isMusl {
+        packages.pandoc.components.exes.pandoc.configureFlags = [
+          "--ghc-option=-optl=-L${zlib.static}/lib"
+        ];
+      })
+    ];
   };
 in
   pandoc.components.exes.pandoc
