06857c38ac21bcff29425fb27a071ed8a9990aa9
Alexander Bantyev <balsoft@balsoft.ru>
2020-04-28 17:46:13 +0300

    Remove checks for now

--- flake.nix
+++ flake.nix
@@ -29,34 +29,5 @@
 
     config = import ./config.nix;
     sources = import ./nixpkgs;
-    checks = let
-      nixpkgs = system:
-        import self.sources.nixpkgs-default {
-          localSystem = { inherit system; };
-        };
-      systems = [ "x86_64-linux" "x86_64-darwin" ];
-      forAllSystems = f:
-        builtins.listToAttrs (map (system: {
-          name = system;
-          value = builtins.listToAttrs (map (test: {
-            name = (builtins.parseDrvName test.name).name;
-            value = test;
-          }) (f system));
-        }) systems);
-      removeShells =
-        builtins.filter (x: isNull (builtins.match ".*shell-for.*" x.name));
-      tests = system:
-        import ./test {
-          nixpkgsArgs = {
-            inherit (self) config;
-            overlays = [ self.overlay ];
-            localSystem = { inherit system; };
-          };
-        };
-      testDrvs = system:
-        with nixpkgs system;
-        with lib;
-        removeShells (collect isDerivation (tests system));
-    in forAllSystems testDrvs;
   };
 }
