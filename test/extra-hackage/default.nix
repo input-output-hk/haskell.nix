5b6f3a798fdada22f3cc007386af1e231b790a0d
Moritz Angermann <moritz.angermann@gmail.com>
2020-05-02 11:24:02 +0800

    Update default.nix
    Add test execution logic

--- test/extra-hackage/default.nix
+++ test/extra-hackage/default.nix
@@ -1,4 +1,4 @@
-{ cabalProject', testSrc }:
+{ stdenv, cabalProject', haskellLib, util, recurseIntoAttrs, testSrc }:
 
 let  
   
@@ -23,6 +23,26 @@ let
       { packages.external-package-demo.src = demo-src; }
     ];
   };
+  packages = project.hsPkgs;
 
-in
-  project
+in recurseIntoAttrs {
+  ifdInputs = {
+    inherit (project) plan-nix;
+  };
+  run = stdenv.mkDerivation {
+    name = "external-hackage-test";
+
+    buildCommand = ''
+      exe="${packages.external-package-user.components.exes.external-package-user}/bin/external-package-user${stdenv.hostPlatform.extensions.executable}"
+      size=$(command stat --format '%s' "$exe")
+      printf "size of executable $exe is $size. \n" >& 2
+      # fixme: run on target platform when cross-compiled
+      printf "checking whether executable runs... " >& 2
+      cat ${haskellLib.check packages.external-package-user.components.exes.external-package-user}
+    '';
+    meta.platforms = platforms.all;
+    passthru = {
+      inherit project;
+    };  
+  };
+}
