117ada89aa1e10c7a6344ff902e3520be0a4bda3
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-05-06 20:39:45 +1200

    Fix haddock in ghcjs 8.8

--- compiler/ghcjs/ghcjs.nix
+++ compiler/ghcjs/ghcjs.nix
@@ -68,9 +68,11 @@ let
           chmod -R +w $out/bin
           rm $out/bin/ghcjs-boot
           cp ${ghcjs.components.exes.ghcjs-boot}/bin/ghcjs-boot $out/bin
+          rm $out/bin/haddock
+          cp ${ghcjs.components.exes.haddock}/bin/haddock $out/bin
 
           wrapProgram $out/bin/ghcjs --add-flags "-B$out/lib"
-          # wrapProgram $out/bin/haddock-ghcjs --add-flags "-B$out/lib"
+          wrapProgram $out/bin/haddock --add-flags "-B$out/lib"
           wrapProgram $out/bin/ghcjs-pkg --add-flags "--global-package-db=$out/lib/package.conf.d"
 
           env PATH=$out/bin:$PATH PYTHON=${pkgs.buildPackages.python3}/bin/python3 $out/bin/ghcjs-boot -j1 --with-emsdk=${project.emsdk}
