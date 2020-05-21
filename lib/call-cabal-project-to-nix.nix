c770af595760a8c5c28ce4adc2be32b47b2c37fc
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-05-22 00:37:59 +1200

    Fix LANG settings to use evalPackages

--- lib/call-cabal-project-to-nix.nix
+++ lib/call-cabal-project-to-nix.nix
@@ -317,7 +317,7 @@ let
   }) (pkgs.evalPackages.runCommand (if name == null then "plan-to-nix-pkgs" else name + "-plan-to-nix-pkgs") {
     nativeBuildInputs = [ nix-tools dummy-ghc dummy-ghc-pkg hpack cabal-install pkgs.evalPackages.rsync ];
     # Needed or stack-to-nix will die on unicode inputs
-    LOCALE_ARCHIVE = pkgs.lib.optionalString (pkgs.stdenv.hostPlatform.libc == "glibc") "${pkgs.glibcLocales}/lib/locale/locale-archive";
+    LOCALE_ARCHIVE = pkgs.lib.optionalString (pkgs.evalPackages.stdenv.hostPlatform.libc == "glibc") "${pkgs.evalPackages.glibcLocales}/lib/locale/locale-archive";
     LANG = "en_US.UTF-8";
     meta.platforms = pkgs.lib.platforms.all;
     preferLocalBuild = false;
