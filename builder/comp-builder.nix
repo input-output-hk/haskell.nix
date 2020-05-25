ef4b8cc511a6cfaa481308eecbd925d0f06cfaf3
Moritz Angermann <moritz.angermann@gmail.com>
2020-05-25 04:21:45 +0000

    [musl] fix libs

--- builder/comp-builder.nix
+++ builder/comp-builder.nix
@@ -1,13 +1,6 @@
 { stdenv, buildPackages, ghc, lib, gobject-introspection ? null, haskellLib, makeConfigFiles, ghcForComponent, hsPkgs, runCommand, libffi, gmp, zlib, ncurses, numactl, nodejs }:
-let
-   # These are here to avoid multiple calls to override
-   gmpStatic = gmp.override { withStatic = true; };
-   zlibStatic = zlib.static;
-   ncursesStatic = ncurses.override { enableStatic = true; };
-   numactlStatic = numactl.overrideAttrs (_: { configureFlags = "--enable-static"; });
-in
 lib.makeOverridable (
-let self = 
+let self =
 { componentId
 , component
 , package
@@ -126,10 +119,6 @@ let
       "--disable-executable-dynamic"
       "--ghc-option=-optl=-pthread"
       "--ghc-option=-optl=-static"
-      "--ghc-option=-optl=-L${gmpStatic}/lib"
-      "--ghc-option=-optl=-L${zlibStatic}/lib"
-      "--ghc-option=-optl=-L${ncursesStatic}/lib"
-      "--ghc-option=-optl=-L${numactlStatic}/lib"
     ] ++ lib.optional enableSeparateDataOutput "--datadir=$data/share/${ghc.name}"
       ++ lib.optional doHaddock' "--docdir=${docdir "$doc"}"
       ++ lib.optional (enableLibraryProfiling || enableExecutableProfiling) "--profiling-detail=${profilingDetail}"
