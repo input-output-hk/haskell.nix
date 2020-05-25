53c549e0a6b8a0f1f1b84a4e34d9ea438e06938f
Moritz Angermann <moritz.angermann@gmail.com>
2020-05-25 04:21:45 +0000

    [musl] fix libs

--- overlays/musl.nix
+++ overlays/musl.nix
@@ -11,6 +11,16 @@ final: prev: prev.lib.optionalAttrs prev.stdenv.hostPlatform.isMusl ({
   # Prevent pkgsMusl.pkgsStatic chain
   busybox-sandbox-shell = prev.busybox-sandbox-shell.override { inherit (final) busybox; };
 
+  # we don't want the static output to be split. That just
+  # messes with the z -> libz mapping. We can't have a conditonal
+  # z -> libz / z -> libz.static mapping without threading the
+  # package configuration in.  That seems a bit overkill.
+  zlib = prev.zlib.override { splitStaticOutput = false; };
+
+  # and a few more packages that need their static libs explicitly enabled
+  gmp = prev.gmp.override { withStatic = true; };
+  ncurses = prev.ncurses.override { enableStatic = true; };
+
   # Fails on cross compile
   nix = prev.nix.overrideAttrs (_: { doInstallCheck = false; });
 } // prev.lib.optionalAttrs (prev.lib.versionAtLeast prev.lib.trivial.release "20.03") {
