6a0e4707affb2df1c9b46b5920084908cbe207a3
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-06-15 21:32:24 +1200

    Turn on ghc8101

--- ci.nix
+++ ci.nix
@@ -17,7 +17,7 @@
     ghc865 = {};
   } // nixpkgs.lib.optionalAttrs (nixpkgsName == "R2003") {
     ghc883 = {};
-#    ghc8101 = {};
+    ghc8101 = {};
   });
   systems = nixpkgs: nixpkgs.lib.filterAttrs (_: v: builtins.elem v supportedSystems) {
     # I wanted to take these from 'lib.systems.examples', but apparently there isn't one for linux!
