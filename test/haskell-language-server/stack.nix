fc20fb7868a831791015402404dbfe9ecd45629e
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-06-16 00:06:38 +1200

    Add `sha256map` to `cabalProject` & `stackProject`
    This is a simple way to specify the sha256 values for references
    to git repositories that do not have a `--sha256:` comment in the
    `cabal.project` file or a `# nix-sha256:` comment in the `stack.yaml`
    file.
    
    The tests demonstrate how to use `sha256map` with both
    `cabalProject` and `stackProject` to build the
    haskell-language-server from github with the sha256 hashes
    needed to work with `--option restrict-eval true`.

--- test/haskell-language-server/stack.nix
+++ test/haskell-language-server/stack.nix
@@ -8,8 +8,8 @@
     sha256 = "0s0k2i0imkcn9zykhrlqq0r4ssv25mwbpdyc675xkzgl1vj1l8kd";
   };
   stackYaml = "stack-${haskell-nix.ghc.version}.yaml";
-  lookupSha256 = { location, tag, ... }: {
-      "https://github.com/wz1000/shake.git"."fb3859dca2e54d1bbb2c873e68ed225fa179fbef" = "0sa0jiwgyvjsmjwpfcpvzg2p7277aa0dgra1mm6afh2rfnjphz8z";
-      "https://github.com/peti/cabal-plan.git"."894b76c0b6bf8f7d2f881431df1f13959a8fce87" = "06iklj51d9kh9bhc42lrayypcpgkjrjvna59w920ln41rskhjr4y";
-    }."${location}"."${tag}";
+  sha256map = {
+    "https://github.com/wz1000/shake.git"."fb3859dca2e54d1bbb2c873e68ed225fa179fbef" = "0sa0jiwgyvjsmjwpfcpvzg2p7277aa0dgra1mm6afh2rfnjphz8z";
+    "https://github.com/peti/cabal-plan.git"."894b76c0b6bf8f7d2f881431df1f13959a8fce87" = "06iklj51d9kh9bhc42lrayypcpgkjrjvna59w920ln41rskhjr4y";
+  };
 }).haskell-language-server.components.exes.haskell-language-server
