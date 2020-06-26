7beeb88bd9c3de31452797455bc434e4d8e5ef7b
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-06-27 01:45:15 +1200

    Replace lib.fix with recursive let in builders

--- builder/haddock-builder.nix
+++ builder/haddock-builder.nix
@@ -56,89 +56,88 @@ let
     configFiles = docsConfigFiles;
   };
 
-in stdenv.lib.fix (drv: stdenv.mkDerivation (commonAttrs // {
-  name = fullName;
+  drv = stdenv.mkDerivation (commonAttrs // {
+    name = fullName;
 
-  passthru = {
-    configFiles = docsConfigFiles;
+    passthru = {
+      configFiles = docsConfigFiles;
 
-    # The directory containing the haddock documentation.
-    haddockDir = if doHaddock' then "${docdir drv.doc}/html" else null;
-  };
+      # The directory containing the haddock documentation.
+      haddockDir = if doHaddock' then "${docdir drv.doc}/html" else null;
+    };
 
-  # `out` contains the `package.conf.d` files used for building the
-  # haddock files.
-  # `doc` contains just the haddock output files.
-  outputs = ["out"]
+    # `out` contains the `package.conf.d` files used for building the
+    # haddock files.
+    # `doc` contains just the haddock output files.
+    outputs = ["out"]
     ++ lib.optional doHaddock' "doc";
 
-  nativeBuildInputs =
-    [ shellWrappers buildPackages.removeReferencesTo ]
-    ++ componentDrv.executableToolDepends;
+    nativeBuildInputs =
+      [ shellWrappers buildPackages.removeReferencesTo ]
+      ++ componentDrv.executableToolDepends;
 
-  configurePhase = ''
-    echo Configure flags:
-    printf "%q " ${finalConfigureFlags}
-    echo
-    $SETUP_HS configure ${finalConfigureFlags}
-  '';
-
-  buildPhase = ''
-    mkdir -p $out
-  '' + lib.optionalString doHaddock' ''
-    runHook preHaddock
-    # If we don't have any source files, no need to run haddock
-    [[ -n $(find . -name "*.hs" -o -name "*.lhs") ]] && {
-
-    $SETUP_HS haddock \
-      "--html" \
-      ${lib.optionalString doHoogle "--hoogle"} \
-      ${lib.optionalString hyperlinkSource "--hyperlink-source"} \
-      ${lib.optionalString quickjump "--quickjump"} \
-      ${lib.concatStringsSep " " (setupHaddockFlags ++ setupGhcOptions)}
-    }
-    runHook postHaddock
-  '';
-
-  installPhase =
-    let
-      target-pkg-and-db = "${ghc.targetPrefix}ghc-pkg -v0 --package-db $out/package.conf.d";
-    in ''
-      html="dist/doc/html/${package.identifier.name}"
-
-      if [ -d "$html" ]; then
-         # Ensure that libraries are not pulled into the docs closure.
-         # As an example, the prettified source code of a
-         # Paths_package module will contain store paths of the library package.
-         for x in "$html/src/"*.html; do
-           remove-references-to -t $out $x
-           remove-references-to -t ${componentDrv} $x
-         done
-
-         docdir="${docdir "$doc"}"
-         mkdir -p "$docdir"
-
-         cp -R "$html" "$docdir"/html
-      fi
-
-      ${ghc.targetPrefix}ghc-pkg -v0 init $out/package.conf.d
-
-      for i in "${componentDrv}/package.conf.d"/*.conf; do
-        # Copy the .conf files from the build derivation, but replace the `haddock-intefaces:`
-        # field with correct location for haddocks (now it is known).  This will insure that
-        # the other haddock derivations build to reference this one will have the correct
-        # working hyper links.
-        pkg=$(basename "$i")
-        sed -e "s|haddock-interfaces:.*|haddock-interfaces: $docdir/html/${componentId.cname}.haddock|" -e "s|haddock-html:.*|haddock-html: $docdir/html/|" "$i" > "$pkg"
-        ${ghc.targetPrefix}ghc-pkg -v0 --package-db ${docsConfigFiles}/${configFiles.packageCfgDir} -f $out/package.conf.d register "$pkg"
-      done
-
-      ln -s ${componentDrv}/exactDep $out/exactDep
-      ln -s ${componentDrv}/envDep $out/envDep
+    configurePhase = ''
+      echo Configure flags:
+      printf "%q " ${finalConfigureFlags}
+      echo
+      $SETUP_HS configure ${finalConfigureFlags}
     '';
-}
-// haskellLib.optionalHooks {
-  inherit preHaddock postHaddock;
-}
-))
 
+    buildPhase = ''
+      mkdir -p $out
+    '' + lib.optionalString doHaddock' ''
+      runHook preHaddock
+      # If we don't have any source files, no need to run haddock
+      [[ -n $(find . -name "*.hs" -o -name "*.lhs") ]] && {
+
+      $SETUP_HS haddock \
+        "--html" \
+        ${lib.optionalString doHoogle "--hoogle"} \
+        ${lib.optionalString hyperlinkSource "--hyperlink-source"} \
+        ${lib.optionalString quickjump "--quickjump"} \
+        ${lib.concatStringsSep " " (setupHaddockFlags ++ setupGhcOptions)}
+      }
+      runHook postHaddock
+    '';
+
+    installPhase =
+      let
+        target-pkg-and-db = "${ghc.targetPrefix}ghc-pkg -v0 --package-db $out/package.conf.d";
+      in ''
+        html="dist/doc/html/${package.identifier.name}"
+
+        if [ -d "$html" ]; then
+           # Ensure that libraries are not pulled into the docs closure.
+           # As an example, the prettified source code of a
+           # Paths_package module will contain store paths of the library package.
+           for x in "$html/src/"*.html; do
+             remove-references-to -t $out $x
+             remove-references-to -t ${componentDrv} $x
+           done
+
+           docdir="${docdir "$doc"}"
+           mkdir -p "$docdir"
+
+           cp -R "$html" "$docdir"/html
+        fi
+
+        ${ghc.targetPrefix}ghc-pkg -v0 init $out/package.conf.d
+
+        for i in "${componentDrv}/package.conf.d"/*.conf; do
+          # Copy the .conf files from the build derivation, but replace the `haddock-intefaces:`
+          # field with correct location for haddocks (now it is known).  This will insure that
+          # the other haddock derivations build to reference this one will have the correct
+          # working hyper links.
+          pkg=$(basename "$i")
+          sed -e "s|haddock-interfaces:.*|haddock-interfaces: $docdir/html/${componentId.cname}.haddock|" -e "s|haddock-html:.*|haddock-html: $docdir/html/|" "$i" > "$pkg"
+          ${ghc.targetPrefix}ghc-pkg -v0 --package-db ${docsConfigFiles}/${configFiles.packageCfgDir} -f $out/package.conf.d register "$pkg"
+        done
+
+        ln -s ${componentDrv}/exactDep $out/exactDep
+        ln -s ${componentDrv}/envDep $out/envDep
+      '';
+  }
+  // haskellLib.optionalHooks {
+    inherit preHaddock postHaddock;
+  });
+in drv
