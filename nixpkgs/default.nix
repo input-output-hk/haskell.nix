eeff15e8f7e5cd0afb33b7367f89e58511f43395
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-07-02 12:46:09 +1200

    Use nixpkgs instead of nixpkgs-default for niv

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,4 +1,5 @@
 # This file is for backwards compatibility only
-{
-  inherit (import ../nix/sources.nix) nixpkgs-2003 nixpkgs-1909 nixpkgs-default;
+rec {
+  inherit (import ../nix/sources.nix) nixpkgs-2003 nixpkgs-1909 nixpkgs;
+  nixpkgs-default = nixpkgs;
 }
