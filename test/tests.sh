b30a9a107e2c316ad083756bfc1516e111b8f663
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-07-06 19:19:26 +1200

    Fix extra-hackage buildkite test

--- test/tests.sh
+++ test/tests.sh
@@ -107,13 +107,13 @@ echo >& 2
 printf "*** Checking that plan construction works with extra Hackages...\n" >& 2
 nix build $NIX_BUILD_ARGS --no-link \
     -f ./default.nix \
-    extra-hackage.plan-nix
+    extra-hackage.run.project.plan-nix
 echo >& 2
 
 printf "*** Checking that package with extra Hackages can be build...\n" >& 2
 nix build $NIX_BUILD_ARGS --no-link \
     -f ./default.nix \
-    extra-hackage.hsPkgs.external-package-user.components.all
+    extra-hackage.run.project.hsPkgs.external-package-user.components.all
 echo >& 2
 
 printf "\n*** Finished successfully\n" >& 2
