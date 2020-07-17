19f0a340605fb9b9216d26d46f844d2f5dca109c
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-07-17 18:02:26 +1200

    Fix haskell-language-server for cabal

--- test/haskell-language-server/cabal.nix
+++ test/haskell-language-server/cabal.nix
@@ -9,4 +9,10 @@
     sha256 = "0w2v133yk0bhnbsj7hnsh5w2kzyd86r6xrkx4q167jyrfq8sa1qg";
   };
   projectFileName = "cabal.project";
+  cabalProjectLocal = ''
+    allow-newer: diagrams-svg:base, monoid-extras:base, svg-builder:base,
+      diagrams-lib:base, dual-tree:base, active:base, diagrams-core:base,
+      diagrams-contrib:base, force-layout:base, diagrams-postscript:base,
+      statestack:base
+  '';
 }).haskell-language-server.components.exes.haskell-language-server
