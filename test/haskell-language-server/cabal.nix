f92ddfe629cfe56d972151117cbb039b9fd32874
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-08-10 16:00:08 +1200

    Update hls test and fix for ghc 8.10.2

--- test/haskell-language-server/cabal.nix
+++ test/haskell-language-server/cabal.nix
@@ -5,10 +5,13 @@
   src = evalPackages.fetchgit {
     url = "https://github.com/haskell/haskell-language-server.git";
     fetchSubmodules = true;
-    rev = "8d37a64f359950f3b340ec50ed419742758ebf0c";
-    sha256 = "0w2v133yk0bhnbsj7hnsh5w2kzyd86r6xrkx4q167jyrfq8sa1qg";
+    rev = "c966e6f8b7be1ec7ca8dc5084fe7f2e6432c50f0";
+    sha256 = "1msjprk4g5v7aqpaa8zg34q999yxz0hg7zavc8a89p7yczss9h28";
   };
   projectFileName = "cabal.project";
+  sha256map = {
+    "https://github.com/bubba/brittany.git"."c59655f10d5ad295c2481537fc8abf0a297d9d1c" = "1rkk09f8750qykrmkqfqbh44dbx1p8aq1caznxxlw8zqfvx39cxl";
+  };
   cabalProjectLocal = ''
     allow-newer: diagrams-svg:base, monoid-extras:base, svg-builder:base,
       diagrams-lib:base, dual-tree:base, active:base, diagrams-core:base,
