969d63027b412e6ad7744f579e354c7463bc8a4a
Moritz Angermann <moritz.angermann@gmail.com>
2020-09-24 07:42:00 +0000

    Adds experimental TSan logic. This will require a custom compiler

--- builder/comp-builder.nix
+++ builder/comp-builder.nix
@@ -76,6 +76,8 @@ let self =
 # Debug
 , enableDebugRTS ? false
 , enableDWARF ? false
+# This will only work with a custom TSan way enabled custom compiler
+, enableTSanRTS ? false
 
 # This will only work with a custom TSan way enabled custom compiler
 , enableTSanRTS ? false
