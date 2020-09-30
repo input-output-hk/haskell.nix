09c4beb22cab30db778130ce3a03029da783f77e
toonn <toonn@toonn.io>
2020-09-30 20:35:37 +0200

    Add tests for the SPDX parser combinators
    I simply added a file with expressions that are expected to fail to
    parse or parse successfully.

---
+++ lib/spdx/test.nix
@@ -0,0 +1,32 @@
+let spdx = import ./spdx.nix;
+in {
+  idstringF = [ (spdx.idstring "$@#!$") ];
+  idstringP = [ (spdx.idstring "blah") ];
+
+  license-refF = [ (spdx.license-ref "LicenseRef-$@#!$") ];
+  license-refP = [ (spdx.license-ref "LicenseRef-blah")
+                   (spdx.license-ref "DocumentRef-beep:LicenseRef-boop")
+                 ];
+
+  simple-expressionF = [ (spdx.simple-expression "$@#!$") ];
+  simple-expressionP = [
+    (spdx.simple-expression "blah")
+    (spdx.simple-expression "blah+")
+    (spdx.simple-expression "LicenseRef-blah")
+    (spdx.simple-expression "DocumentRef-beep:LicenseRef-boop")
+  ];
+
+  compound-expressionF = [ (spdx.compound-expression "$@#!$") ];
+  compound-expressionP = [
+    (spdx.compound-expression "blah")
+    (spdx.compound-expression "blah+")
+    (spdx.compound-expression "LicenseRef-blah")
+    (spdx.compound-expression "DocumentRef-beep:LicenseRef-boop")
+    (spdx.compound-expression "(blah)")
+    (spdx.compound-expression "beep OR boop")
+    (spdx.compound-expression "beep AND boop")
+    (spdx.compound-expression "beep WITH boop")
+    (spdx.compound-expression "beep AND (boop OR blap)")
+    (spdx.compound-expression "(beep AND ((boop OR ((blap)))))")
+  ];
+}
