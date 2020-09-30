0fd7dbf30fae96458db2cdce51cdbceb9cb6177e
toonn <toonn@toonn.io>
2020-09-30 20:37:22 +0200

    Add the SPDX license list
    The SPDX license list as attrsets following the nixpkgs lib.licenses
    convention.
    
    This uses fetchurl which is not ideal because it's not available during
    pure evaluation.
    
    It does not yet include the SPDX exceptions list.

---
+++ lib/spdx/licenses.nix
@@ -0,0 +1,19 @@
+with builtins; let
+  licensesJSON = fromJSON (replaceStrings
+      [ "\\u0026" "\\u0027" "\\u003d" ]
+      [ "&" "'" "=" ]
+      (readFile (fetchurl https://spdx.org/licenses/licenses.json))
+    );
+  dropFour = s: substring 0 (stringLength s - 4) s;
+  toSpdx = lic: with lic;
+            { spdxId = licenseId
+            ; shortName = licenseId
+            ; fullName = name
+            ; url = dropFour detailsUrl + "html"
+            ; free = isOsiApproved
+            ;
+            };
+  toNamedValue = lic: { name = lic.spdxId; value = lic; };
+in
+
+listToAttrs (map (l: toNamedValue (toSpdx l)) licensesJSON.licenses)
