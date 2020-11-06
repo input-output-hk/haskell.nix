b57307f0df5c133b6d9b82be1bf47a857d66d472
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-11-06 21:14:28 +1300

    Fix shim

--- overlays/shims.nix
+++ overlays/shims.nix
@@ -1,9 +1,9 @@
 # Shims to make older versions of nixpkgs work
-final: prev: final.optionalAttrs (!(prev ? runCommandLocal)) {
+final: prev: {
   # nixpkgs 19.09 and older does not have runCommandLocal
-  runCommandLocal = name: env:
+  runCommandLocal = prev.runCommandLocal or (name: env:
     final.runCommand name (env // {
       preferLocalBuild = true;
       allowSubstitutes = false;
-    });
+    }));
 }
