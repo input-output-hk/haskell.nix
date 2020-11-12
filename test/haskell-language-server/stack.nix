ea8f98a401c4cc152eebbcd06baa8e75cce31e7d
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-11-12 13:51:11 +1300

    Fix eval time failure building HLS

--- test/haskell-language-server/stack.nix
+++ test/haskell-language-server/stack.nix
@@ -8,8 +8,7 @@ let
       rev = "e390da54a90f2c6c02ce836a27f2d4c33a71f607";
       sha256 = "0s77r79zn6q42k3biylni81vhn1yr8r1mqn8pj9kgggh16xw229b";
     };
-    projectFileName = "stack-${haskell-nix.compiler.${compiler-nix-name}.version}.yaml";
-    modules = [{ config.compiler.nix-name = lib.mkForce compiler-nix-name; }];
+    projectFileName = "stack-${buildPackages.haskell-nix.compiler.${compiler-nix-name}.version}.yaml";
     sha256map = {
       "https://github.com/bubba/brittany.git"."c59655f10d5ad295c2481537fc8abf0a297d9d1c" = "1rkk09f8750qykrmkqfqbh44dbx1p8aq1caznxxlw8zqfvx39cxl";
     };
