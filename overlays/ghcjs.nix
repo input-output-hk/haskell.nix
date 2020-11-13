7ce51e4d07bd893bef95ed77f5fae5430e866a20
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-11-14 00:22:12 +1300

    Fix issue where Cabal patch is not applied

--- overlays/ghcjs.nix
+++ overlays/ghcjs.nix
@@ -27,6 +27,21 @@ final: prev:
         in {
           # Override Cabal used for default setup
           setup-depends = [ cabal-project.hsPkgs.Cabal ];
+          # Allow Cabal to be reinstalled so that custom setups will use a Cabal
+          # built with packages.Cabal.patches
+          nonReinstallablePkgs =
+            [ "rts" "ghc-heap" "ghc-prim" "integer-gmp" "integer-simple" "base"
+              "deepseq" "array" "ghc-boot-th" "pretty" "template-haskell"
+              "ghcjs-prim" "ghcjs-th"
+            ]
+            ++ lib.optionals (!config.reinstallableLibGhc) [
+              "ghc-boot"
+              "ghc" "Win32" "array" "binary" "bytestring" "containers"
+              "directory" "filepath" "ghc-boot" "ghc-compact" "ghc-prim"
+              "hpc"
+              "mtl" "parsec" "process" "text" "time" "transformers"
+              "unix" "xhtml" "terminfo"
+            ];
           # Include patches for custom setups
           packages.Cabal.patches = [
             ./patches/Cabal/Cabal-3.0.0.0-drop-pkg-db-check.diff
