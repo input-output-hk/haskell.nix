c6f83ceeeb7f0f104a0b2051f0b2cb94d9fab727
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-11-23 17:48:54 +1300

    Add support for "latest" as a version (#934)
    When using `shellFor { tools = { x = "1.2.3"; }; }` it can be a pain
    to have to check if you have the latest version of `x` or if a new
    one has been introduced.  This change adds support for passing
    `"latest"` as the version number to anything built on top of the
    `haskell-nix.hackage-project` function.  When `"latest"` is used
    the newest version in the hackage.nix snapshot will be selected.

--- overlays/haskell.nix
+++ overlays/haskell.nix
@@ -459,29 +459,35 @@ final: prev: {
         hackage-project =
             { name
             , compiler-nix-name
-            , version
+            , version ? "latest"
             , revision ? "default"
             , ... }@args':
-            let args = { caller = "hackage-project"; } // args';
+            let version' = if version == "latest"
+                  then builtins.head (
+                    builtins.sort
+                      (a: b: builtins.compareVersions a b > 0)
+                      (builtins.attrNames hackage.${name}))
+                  else version;
+                args = { caller = "hackage-project"; } // args';
                 tarball = final.pkgs.fetchurl {
-                  url = "mirror://hackage/${name}-${version}.tar.gz";
-                  inherit (hackage.${name}.${version}) sha256; };
-                rev = hackage.${name}.${version}.revisions.${revision};
+                  url = "mirror://hackage/${name}-${version'}.tar.gz";
+                  inherit (hackage.${name}.${version'}) sha256; };
+                rev = hackage.${name}.${version'}.revisions.${revision};
                 cabalFile = final.pkgs.fetchurl {
-                  url = "https://hackage.haskell.org/package/${name}-${version}/revision/${toString rev.revNum}.cabal";
+                  url = "https://hackage.haskell.org/package/${name}-${version'}/revision/${toString rev.revNum}.cabal";
                   inherit (rev) sha256;
                 };
                 revSuffix = final.lib.optionalString (rev.revNum > 0) "-r${toString rev.revNum}";
-            in let src = final.buildPackages.pkgs.runCommand "${name}-${version}${revSuffix}-src" { } (''
+            in let src = final.buildPackages.pkgs.runCommand "${name}-${version'}${revSuffix}-src" { } (''
                   tmp=$(mktemp -d)
                   cd $tmp
                   tar xzf ${tarball}
-                  mv "${name}-${version}" $out
+                  mv "${name}-${version'}" $out
                 '' + final.lib.optionalString (rev.revNum > 0) ''
                   cp ${cabalFile} $out/${name}.cabal
                 '');
           in cabalProject' (
-            (final.haskell-nix.hackageQuirks { inherit name version; }) //
+            (final.haskell-nix.hackageQuirks { inherit name; version = version'; }) //
               builtins.removeAttrs args [ "version" "revision" ] // { inherit src; });
 
         # This function is like `cabalProject` but it makes the plan-nix available
