719cb997a3a392890ea61d1aed39fc0ceacd7156
Zhenya Vinogradov <zhenyavinogradov@gmail.com>
2020-11-25 10:18:44 +0300

    Fix stack.yaml filtering when resolver file is in a subdirectory
    `maybeCleanedSource` tries to include the resolver file into the cleaned
    source, if a resolver is specified, but it was only working when the
    file was in the root of the project, because all directories would be
    filtered out.

--- lib/stack-cache-generator.nix
+++ lib/stack-cache-generator.nix
@@ -32,8 +32,17 @@ let
         then haskellLib.cleanSourceWith {
           inherit src;
           filter = path: type:
-               pkgs.lib.hasSuffix ("/" + stackYaml) path
-            || (resolver != null && pkgs.lib.hasSuffix ("/" + resolver) path);
+            let
+              origSrc = if src ? _isLibCleanSourceWith then src.origSrc else src;
+              relPath = pkgs.lib.removePrefix (toString origSrc + "/") path;
+
+              # checks if path1 is a parent directory for path2
+              isParent = path1: path2: pkgs.lib.hasPrefix "${path1}/" path2;
+
+            in
+              (relPath == stackYaml)
+              || (resolver != null && (relPath == resolver || isParent relPath resolver))
+            ;
         }
         else src;
 
