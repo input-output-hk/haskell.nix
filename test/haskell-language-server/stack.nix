8955b582647e7740d6a2057cd732b58c0e664183
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-12-02 23:11:47 +1300

    Fix hls test

--- test/haskell-language-server/stack.nix
+++ test/haskell-language-server/stack.nix
@@ -8,7 +8,11 @@ let
       rev = "e390da54a90f2c6c02ce836a27f2d4c33a71f607";
       sha256 = "0s77r79zn6q42k3biylni81vhn1yr8r1mqn8pj9kgggh16xw229b";
     };
-    projectFileName = "stack-${buildPackages.haskell-nix.compiler.${compiler-nix-name}.version}.yaml";
+    projectFileName = "stack-${
+      if compiler-nix-name == "ghc81020201118"
+        then "8.10.2"
+        else buildPackages.haskell-nix.compiler.${compiler-nix-name}.version
+    }.yaml";
     sha256map = {
       "https://github.com/bubba/brittany.git"."c59655f10d5ad295c2481537fc8abf0a297d9d1c" = "1rkk09f8750qykrmkqfqbh44dbx1p8aq1caznxxlw8zqfvx39cxl";
     };
