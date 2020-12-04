99dbba0e8403b4f4ab54209954d257afb246941c
Edward Amsden <edward@blackriversoft.com>
2020-12-04 10:24:32 -0500

    Fix missing attribute error in gobject introspection overlay

--- overlays/gobject-introspection.nix
+++ overlays/gobject-introspection.nix
@@ -3,7 +3,7 @@ final: prev: {
   # As of nixpkgs 20.09 harfbuzz is built with mesonFlags and hopefully includes
   # gobject introspection correctly.  Older nixpkgs will have configureFlags to update.
   harfbuzz = prev.harfbuzz.overrideAttrs (attr: final.lib.optionalAttrs (attr ? configureFlags) {
-    configureFlags = attr.configureFlags ++ [ "--enable-introspection=yes" "--with-gobject=yes" ];
-    buildInputs = attr.buildInputs ++ [ final.pkgs.gobject-introspection ];
+    configureFlags = attr.configureFlags or [] ++ [ "--enable-introspection=yes" "--with-gobject=yes" ];
+    buildInputs = attr.buildInputs or [] ++ [ final.pkgs.gobject-introspection ];
   });
 }
