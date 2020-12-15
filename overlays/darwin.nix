e248a581694e6045c83a11d3a08a8c159d4ce62a
Drew Hess <src@drewhess.com>
2020-12-15 14:57:49 +0000

    x509-system: use /usr/bin/security on macOS.
    This mirrors the workaround in nixpkgs for haskellPackages.
    
    ref: https://github.com/NixOS/nixpkgs/pull/47676

--- overlays/darwin.nix
+++ overlays/darwin.nix
@@ -1,3 +1,21 @@
-final: prev: {
-
+final: prev:
+{
+  haskell-nix = prev.haskell-nix // ({
+    defaultModules = prev.haskell-nix.defaultModules ++ [
+      ({ pkgs, buildModules, config, lib, ... }:
+        {
+          packages = { } // pkgs.lib.optionalAttrs (pkgs.stdenv.hostPlatform.isDarwin && !pkgs.stdenv.cc.nativeLibc)
+            {
+              # Workaround for broken nixpkgs darwin.security_tool in
+              # Mojave. This mirrors the workaround in nixpkgs
+              # haskellPackages.
+              #
+              # ref:
+              # https://github.com/NixOS/nixpkgs/pull/47676
+              # https://github.com/NixOS/nixpkgs/issues/45042
+              x509-system.components.library.preBuild = "substituteInPlace System/X509/MacOS.hs --replace security /usr/bin/security";
+            };
+        })
+    ];
+  });
 }
