bd2b99b281de40e3c4c634a4f10477d83583f600
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2020-12-17 10:19:53 +0000

    Revert "Fix hls stack test eval for ghc810220201118"
    This reverts commit b405cd7ee8bd62214bd2d0a61e0317673b5bf1b5.

--- test/haskell-language-server/stack.nix
+++ test/haskell-language-server/stack.nix
@@ -8,10 +8,7 @@ let
       rev = "e390da54a90f2c6c02ce836a27f2d4c33a71f607";
       sha256 = "0s77r79zn6q42k3biylni81vhn1yr8r1mqn8pj9kgggh16xw229b";
     };
-    projectFileName = "stack-${buildPackages.haskell-nix.compiler.${
-      if compiler-nix-name == "ghc810220201118"
-        then "ghc8102"
-        else compiler-nix-name}.version}.yaml";
+    projectFileName = "stack-${buildPackages.haskell-nix.compiler.${compiler-nix-name}.version}.yaml";
     sha256map = {
       "https://github.com/bubba/brittany.git"."c59655f10d5ad295c2481537fc8abf0a297d9d1c" = "1rkk09f8750qykrmkqfqbh44dbx1p8aq1caznxxlw8zqfvx39cxl";
     };
