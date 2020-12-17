738af22ce95d9e3f1c9677045db441bca2acc472
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2020-12-17 15:57:09 +0000

    Move more CI to 20.09 (attempt 2) (#975)
    * Update default nixpkgs to 20.09
    
    * Move more CI to work on 20.09 by default
    
    * IFD level 1
    
    * Use nixpkgs 20.03 emscripten (needed for ghcjs)
    
    * Add missing materialized files
    
    * Add missing materialized files
    
    * Fix for ghc 8.8 build on darwin
    
    * Fix for ghcjs 8.8 build on darwin
    
    * Fix hls stack test eval for ghc810220201118
    
    * ifdLevel 2
    
    * Oops: accidentally turned on tests for experimental GHC
    
    * Revert "Fix hls stack test eval for ghc810220201118"
    
    This reverts commit b405cd7ee8bd62214bd2d0a61e0317673b5bf1b5.
    
    * ifdLevel 3
    
    * Disable GHC 8.6.5 windows cross on nixpkgs 20.09
    
    * Update supported GHC doc with a table
    
    * Add emscripten comment
    
    Co-authored-by: Hamish Mackenzie <Hamish.Mackenzie@iohk.io>

--- release.nix
+++ release.nix
@@ -16,9 +16,9 @@ in allJobs // {
     required = genericPkgs.releaseTools.aggregate {
       name = "haskell.nix-required";
       meta.description = "All jobs required to pass CI";
-      # Hercules will require all of these, we just require the 20.03 jobs
+      # Hercules will require all of these, we just require the 20.09 jobs
       # to avoid stressing Hydra too much
-      constituents = lib.collect lib.isDerivation allJobs.R2003.ghc865.linux.native;
+      constituents = lib.collect lib.isDerivation allJobs.R2009.ghc865.linux.native;
     };
   }
 
