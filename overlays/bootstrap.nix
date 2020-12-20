ef78c386431f601b567aae772f24d57e5cab31ce
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-12-20 14:20:04 +1300

    Build ghc 8.10.3

--- overlays/bootstrap.nix
+++ overlays/bootstrap.nix
@@ -372,6 +372,26 @@ in {
 
                 ghc-patches = ghc-patches "8.10.2";
             };
+            ghc8103 = final.callPackage ../compiler/ghc {
+                extra-passthru = { buildGHC = final.buildPackages.haskell-nix.compiler.ghc8103; };
+
+                bootPkgs = bootPkgs // {
+                  # Not using 8.8 due to https://gitlab.haskell.org/ghc/ghc/-/issues/18143
+                  ghc = final.buildPackages.buildPackages.haskell-nix.compiler.ghc865;
+                };
+                inherit sphinx installDeps;
+
+                buildLlvmPackages = final.buildPackages.llvmPackages_9;
+                llvmPackages = final.llvmPackages_9;
+
+                src-spec = rec {
+                    version = "8.10.3";
+                    url = "https://downloads.haskell.org/~ghc/${version}/ghc-${version}-src.tar.xz";
+                    sha256 = "0cdrdvs5qnqr93cr9zvrlfjv2xr671kjjghnsw4afa4hahcq7p6c";
+                };
+
+                ghc-patches = ghc-patches "8.10.3";
+            };
             # ghc 8.10.2 with patches needed by plutus
             ghc810220201118 = final.callPackage ../compiler/ghc {
                 extra-passthru = { buildGHC = final.buildPackages.haskell-nix.compiler.ghc810220201118; };
