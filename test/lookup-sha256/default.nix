aa81df00adc43febc51a7507ddb1c07607f9b7cd
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2020-12-23 01:52:28 +1300

    Fix lookup-sha256 test

--- test/lookup-sha256/default.nix
+++ test/lookup-sha256/default.nix
@@ -3,11 +3,14 @@
     inherit compiler-nix-name;
     name         = "pandoc";
     version      = "2.9.2.1";
-    index-state  = "2020-05-25T00:00:00Z";
+    index-state  = "2020-12-17T00:00:00Z";
     # Function that returns a sha256 string by looking up the location
     # and tag in a nested attrset
     lookupSha256 = { location, tag, ... }:
       { "https://github.com/jgm/pandoc-citeproc"."0.17"
           = "0dxx8cp2xndpw3jwiawch2dkrkp15mil7pyx7dvd810pwc22pm2q"; }
         ."${location}"."${tag}";
+    cabalProjectLocal = ''
+      allow-newer: *:base
+    '';
   }).components.exes.pandoc
