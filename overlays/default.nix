d80d589d62fca831fbfffc250fa80ee720f53891
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2021-01-08 12:14:24 +1300

    Use configuration-nix instead of default-modules

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,7 +6,6 @@ let
     #ghcjs = import ./ghcjs-asterius-triple.nix;
     #python = import ./python.nix;
     haskell = import ./haskell.nix args;
-    default-modules = import ./default-modules.nix;
     hackage-quirks = import ./hackage-quirks.nix;
     bootstrap = import ./bootstrap.nix;
     ghc = import ./ghc.nix;
@@ -41,7 +40,6 @@ let
     wine
     haskell
     hackage-quirks
-    default-modules
     bootstrap
     ghc
     ghc-packages
