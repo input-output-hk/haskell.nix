b6cbac3bf8d3aff6e4ba0dbebdf4748d33293c93
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2021-01-28 13:08:18 +0000

    Oops, should start with IFD level 1

--- ci.nix
+++ ci.nix
@@ -1,7 +1,7 @@
 # 'supportedSystems' restricts the set of systems that we will evaluate for. Useful when you're evaluating
 # on a machine with e.g. no way to build the Darwin IFDs you need!
 { supportedSystems ? [ "x86_64-linux" "x86_64-darwin" ]
-, ifdLevel ? 3
+, ifdLevel ? 1
 # Whether or not we are evaluating in restricted mode. This is true in Hydra, but not in Hercules.
 , restrictEval ? false
 , checkMaterialization ? false }:
