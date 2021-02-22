34180fa0037a3ea05f2555c3423d8b9d76038466
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-02-22 17:50:55 +1300

    Add inputs for nixpkgs pins

--- flake.nix
+++ flake.nix
@@ -1,7 +1,15 @@
 {
   description = "Alternative Haskell Infrastructure for Nixpkgs";
 
-  outputs = { self }: let
+  inputs = {
+    # Note: keep this in sync with sources.json!
+    nixpkgs.url = github:NixOS/nixpkgs/f02bf8ffb9a5ec5e8f6f66f1e5544fd2aa1a0693;
+    nixpkgs-2003.url = github:NixOS/nixpkgs/7f73e46625f508a793700f5110b86f1a53341d6e;
+    nixpkgs-2009.url = github:NixOS/nixpkgs/f02bf8ffb9a5ec5e8f6f66f1e5544fd2aa1a0693;
+    nixpkgs-unstable.url = github:NixOS/nixpkgs/410bbd828cdc6156aecd5bc91772ad3a6b1099c7;
+  };
+
+  outputs = { self, nixpkgs, ... }: let
     config = import ./config.nix;
     # We can't import ./nix/sources.nix directly, because that uses nixpkgs to fetch by default,
     # and importing nixpkgs without specifying localSystem doesn't work on flakes.
@@ -29,7 +37,7 @@
           value = f name;
         }) lst);
     in genAttrs [ "x86_64-linux" "x86_64-darwin" ] (system:
-      import sources.nixpkgs
+      import nixpkgs
       (nixpkgsArgs // { localSystem = { inherit system; }; }));
   };
 }
