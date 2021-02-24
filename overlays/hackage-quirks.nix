583acc2219f4183dcb601688f82e21eadd5cce5b
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2021-02-24 22:49:31 +1300

    Fix for postgresql-libpg and musl (fixes #948) (#1056)
    This combined with setting the `use-pkg-config` flag on `postgresql-libpq` should get it compiling.  There is still a problem with `openssl` that requires a `module` to fix the linking of executables.
    
    Add the following to the `cabal.project` file or to `cabalProjectLocal`:
    
    ```
    package postgresql-libpq
      flags: +use-pkg-config
    ```
    
    Include a module like this one to include `openssl` in the linker arguments:
    
    ```nix
    {
      modules = [(
       {pkgs, ...}: final.lib.mkIf pkgs.stdenv.hostPlatform.isMusl {
         # The order of -lssl and -lcrypto is important here
         packages.postgrest.configureFlags = [
           "--ghc-option=-optl=-lssl"
           "--ghc-option=-optl=-lcrypto"
           "--ghc-option=-optl=-L${pkgs.openssl.out}/lib"
         ];
      })];
    }
    ```
    
    This PR also adds a "hackage quirk" for `postgrest`  to that makes these changes for `postgrest` automatically when built it as a `tool` or with `hackage-package`.
    
    Unfortunately we do not have a good way to avoid the need make these changes.

--- overlays/hackage-quirks.nix
+++ overlays/hackage-quirks.nix
@@ -73,6 +73,24 @@ in { haskell-nix = prev.haskell-nix // {
       '';
     };
 
+    # See https://github.com/input-output-hk/haskell.nix/issues/948
+    postgrest = {
+      cabalProject = ''
+        packages: .
+        package postgresql-libpq
+          flags: +use-pkg-config
+      '';
+      modules = [(
+       {pkgs, ...}: final.lib.mkIf pkgs.stdenv.hostPlatform.isMusl {
+         # The order of -lssl and -lcrypto is important here
+         packages.postgrest.configureFlags = [
+           "--ghc-option=-optl=-lssl"
+           "--ghc-option=-optl=-lcrypto"
+           "--ghc-option=-optl=-L${pkgs.openssl.out}/lib"
+         ];
+      })];
+    };
+
   }."${name}" or {};
 
 }; }
