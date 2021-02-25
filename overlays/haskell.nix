7bff66f4431f466cc2bb69a5f55459de36da278d
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-02-25 23:44:27 +1300

    Switch from cabal 3.2 to 3.4

--- overlays/haskell.nix
+++ overlays/haskell.nix
@@ -254,7 +254,7 @@ final: prev: {
         # If you want to update this value it important to check the
         # materializations.  Turn `checkMaterialization` on below and
         # check the CI results before turning it off again.
-        internalHackageIndexState = "2021-01-08T00:00:00Z";
+        internalHackageIndexState = "2021-02-25T00:00:00Z";
 
         checkMaterialization = false; # This is the default. Use an overlay to set it to true and test all the materialized files
 
