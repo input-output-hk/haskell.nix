0d781c04c4ab9045a165bacf428b043995a167f6
Hamish Mackenzie <Hamish.Mackenzie@iohk.io>
2021-03-01 15:42:23 +1300

    Switch from cabal 3.2 to 3.4 (#1058)

--- overlays/hackage-quirks.nix
+++ overlays/hackage-quirks.nix
@@ -22,6 +22,9 @@ in { haskell-nix = prev.haskell-nix // {
         # Version of of cabal-install in hackage is broken for GHC 8.10.1
         (lib.optionalAttrs (version == "3.2.0.0") {
           packages.cabal-install.src = final.haskell-nix.sources.cabal-32 + "/cabal-install";
+        })
+        (lib.optionalAttrs (builtins.compareVersions version "3.0.0.0" >= 0
+            && builtins.compareVersions version "3.5" < 0) {
           # Include patches needed for ghcjs
           packages.Cabal.patches = [
             ./patches/Cabal/Cabal-3.0.0.0-drop-pkg-db-check.diff
