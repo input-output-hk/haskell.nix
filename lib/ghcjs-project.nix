6d89aa81eb47d74cdede0e890cfd0c91da543d9b
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-03-25 17:02:39 +1300

    Update materialization

--- lib/ghcjs-project.nix
+++ lib/ghcjs-project.nix
@@ -128,7 +128,6 @@ let
                 ["src" "ghcjsVersion" "ghcVersion" "happy" "alex" "cabal-install"])) args) // {
         src = configured-src;
         index-state = "2021-03-20T00:00:00Z";
-        checkMaterialization = true;
         compiler-nix-name = if isGhcjs810 then "ghc8104" else if isGhcjs88 then "ghc884" else "ghc865";
         configureArgs = pkgs.lib.optionalString (isGhcjs88 && !isGhcjs810) "--constraint='Cabal >=3.0.2.0 && <3.1'";
         materialized = ../materialized + (if isGhcjs810 then "/ghcjs8104" else if isGhcjs88 then "/ghcjs884" else "/ghcjs865");
