3d280605a2eec82566936993f3a3a795570a04a0
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-03-26 19:30:40 +1300

    Fix disabling of shell-for-setup-deps test

--- test/shell-for-setup-deps/default.nix
+++ test/shell-for-setup-deps/default.nix
@@ -16,6 +16,12 @@ let
   env = project.shellFor {};
 
 in recurseIntoAttrs ({
+  # Making this work for cross compilers will be difficult as setup-deps are
+  # built for the build platform and the shell will be for the host platform.
+  # We probably need a shell that provides both build and host ghc
+  # and corresponding package DBs and a way to use them.
+  # This problem affects musl as well as the build libraries are linked to glibc.
+  meta.disabled = stdenv.buildPlatform != stdenv.hostPlatform;
   ifdInputs = {
     inherit (project) plan-nix;
   };
@@ -38,12 +44,6 @@ in recurseIntoAttrs ({
 
     meta = {
       platforms = platforms.all;
-      # Making this work for cross compilers will be difficult as setup-deps are
-      # built for the build platform and the shell will be for the host platform.
-      # We probably need a shell that provides both build and host ghc
-      # and corresponding package DBs and a way to use them.
-      # This problem affects musl as well as the build libraries are linked to glibc.
-      disabled = stdenv.buildPlatform != stdenv.hostPlatform;
     };
 
     passthru = {
