482bdc0e80390b0643dafd7b6691d607650c7a53
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-06-01 23:30:36 +1200

    Include patch to fix #1127

--- overlays/bootstrap.nix
+++ overlays/bootstrap.nix
@@ -159,6 +159,7 @@ in {
                 ++ fromUntil "8.10.3" "8.10.5" ./patches/ghc/ghc-8.10.3-rts-make-markLiveObject-thread-safe.patch
                 ++ final.lib.optional (versionAtLeast "8.10.4" && final.targetPlatform.isWindows) ./patches/ghc/ghc-8.10-z-drive-fix.patch
                 ++ final.lib.optional (versionAtLeast "8.6.5") ./patches/ghc/ghc-8.10-windows-add-dependent-file.patch
+                ++ until              "8.10.5" ./patches/ghc/AC_PROG_CC_99.patch
                 ;
         in ({
             ghc844 = final.callPackage ../compiler/ghc {
