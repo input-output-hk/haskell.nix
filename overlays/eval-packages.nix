ab5fc53f3a54323f86aa2e4b8d2b72524449d139
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-07-07 22:41:54 +1200

    Add a comment about evalPackages

--- overlays/eval-packages.nix
+++ overlays/eval-packages.nix
@@ -1,7 +1,13 @@
 final: prev: {
   # This overlay makes `evalPackages` is like `buildPackages`, but on
-  # `builtins.currentSystem`.
+  # `builtins.currentSystem` (when not building a nix flake).
+  # We do not pass the `config` or `overlays` (not in `final.haskell-nix.overlays`).
+  # This could cause flake and non flake builds to diverge, although only the
+  # derivations that use `evalPackages` (these should be eval time only derviations
+  # used to build nix inputs for IFD, the generated nix should match and so derivations
+  # that depend on the IFD should match).
   evalPackages =
+    # If we are building a flake there will be no currentSystem attribute
     if builtins ? currentSystem
       then (import final.path {
           inherit (final.haskell-nix) overlays;
