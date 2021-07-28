3dc324a3014b14261a8409e200913b3d963a86c2
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-07-29 00:52:41 +1200

    Use a version of flake-compat that allows us to avoid builtin fetch

--- default.nix
+++ default.nix
@@ -6,12 +6,13 @@ let
       url = "https://github.com/NixOS/nixpkgs/archive/3c6f3f84af60a8ed5b8a79cf3026b7630fcdefb8.tar.gz";
       sha256 = "sha256:0jf9l6j60sa8cms7r4a02kr9j9884pwv1prf79b2ysnxmnhimnch";
     };
+  pkgs = args.pkgs or import nixpkgsSrc {};
   flake-compat =
-    (args.pkgs or import nixpkgsSrc {}).fetchgit {
-      url = "https://github.com/edolstra/flake-compat";
-      rev = "99f1c2157fba4bfe6211a321fd0ee43199025dbf";
-      sha256 = "sha256:0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
+    pkgs.fetchgit {
+      url = "https://github.com/hamishmack/flake-compat";
+      rev = "90c3753102f9e619234b5d23f5db3b63f7bf8544";
+      sha256 = "sha256-MM7nIIdFkA8hwG/vFqHZJ2+PP2CYgJifyPV3ET9TXyg=";
     };
-  self = import flake-compat { src = ./.; };
+  self = import flake-compat { src = ./.; inherit pkgs; };
 in self.defaultNix.internal.compat
 ({ system = args.pkgs.system or builtins.currentSystem; } // args) // self.defaultNix
