4dfbe8074e906d1e6920ae65881a0561bddec5d1
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-07-28 21:51:26 +1200

    Use fetchTarball to get flake-compat so we can include a sha256

--- default.nix
+++ default.nix
@@ -2,7 +2,10 @@
 
 let
   flake-compat =
-    builtins.fetchGit { url = "https://github.com/edolstra/flake-compat"; };
+    builtins.fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+      sha256 = "sha256:0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
+    };
   self = import flake-compat { src = ./.; };
 in self.defaultNix.internal.compat
 ({ system = args.pkgs.system or builtins.currentSystem; } // args) // self.defaultNix
