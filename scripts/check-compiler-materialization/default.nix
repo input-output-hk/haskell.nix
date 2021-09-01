550d945fda0df6c1effd2cba78999f1cac78e960
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-09-01 21:30:41 +1200

    Update materialization with win32 fix

--- scripts/check-compiler-materialization/default.nix
+++ scripts/check-compiler-materialization/default.nix
@@ -20,13 +20,13 @@ in eval.linkFarm "check-${compiler-nix-name}" (builtins.concatMap (system:
   { name = "${system}-nix-tools";     path = pkgs.haskell-nix.nix-tools.${compiler-nix-name}; }
   { name = "${system}-extra";         path = pkgs.ghc-extra-projects.${compiler-nix-name}.plan-nix; }
   { name = "${system}-boot";          path = pkgs.ghc-boot-packages-nix.${compiler-nix-name}; }
-] ++ eval.lib.optionals (system == "x86_64-linux") [
+] ++ eval.lib.optionals (system == "x86_64-linux") ([
   # In some cased you may need comment out one or more of these if the GHC version needed cannot be built.
   { name = "${system}-musl";          path = pkgs.pkgsCross.musl64.ghc-extra-projects.${compiler-nix-name}.plan-nix; }
   { name = "${system}-windows";       path = pkgsForWindows.pkgsCross.mingwW64.ghc-extra-projects.${compiler-nix-name}.plan-nix; }
 ] ++ eval.lib.optionals (__elem compiler-nix-name ["ghc884" "ghc8105" "ghc8106" "ghc8107"]) [
   { name = "${system}-arm";           path = pkgs.pkgsCross.aarch64-multiplatform.ghc-extra-projects.${compiler-nix-name}.plan-nix; }
-] ++ eval.lib.optionals (__elem compiler-nix-name ["ghc865" "ghc884" "ghc8105" "ghc8106" "ghc8107"]) [
+]) ++ eval.lib.optionals (__elem compiler-nix-name ["ghc865" "ghc884" "ghc8105" "ghc8106" "ghc8107"]) [
   { name = "${system}-ghcjs";         path = pkgs.pkgsCross.ghcjs.ghc-extra-projects.${compiler-nix-name}.plan-nix; }
 ]) systems)
 
