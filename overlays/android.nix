12bf435a2686de61cdd52d9fed72ad3cf9381c25
Moritz Angermann <moritz.angermann@gmail.com>
2021-12-25 12:31:38 +0800

    Update android.nix

--- overlays/android.nix
+++ overlays/android.nix
@@ -1,3 +1,4 @@
 final: prev: prev.lib.optionalAttrs prev.stdenv.hostPlatform.isAndroid ({
-  libiconv = prev.libiconv.override { enableStatic = true; };
+  # we really only want the static one.
+  libiconv = prev.libiconv.override { enableStatic = true; enableShared = false; };
 })
