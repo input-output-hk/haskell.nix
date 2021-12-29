ae7c2cf2d071722e1b83b0a3d7e63ab46d72b452
Moritz Angermann <moritz.angermann@gmail.com>
2021-12-29 17:49:08 +0800

    disable more hardening flags on android

--- overlays/android.nix
+++ overlays/android.nix
@@ -1,7 +1,7 @@
 final: prev: prev.lib.optionalAttrs prev.stdenv.hostPlatform.isAndroid ({
   # we really only want the static one.
   libiconv = (prev.libiconv.override { enableStatic = true; enableShared = false; }).overrideAttrs(_: {
-    hardeningDisable = [ "fortify" ];
+    hardeningDisable = [ "fortify" "stackprotector" "format" ];
     # For some reason building libiconv with nixpgks android setup produces
     # LANGINFO_CODESET to be found, which is not compatible with android sdk 23;
     # so we'll patch up iconv to not include that.    
@@ -14,13 +14,13 @@ final: prev: prev.lib.optionalAttrs prev.stdenv.hostPlatform.isAndroid ({
   libffi = prev.libffi.overrideAttrs (_: {
     dontDisableStatic = true;
 
-    hardeningDisable = [ "fortify" ];
+    hardeningDisable = [ "fortify" "stackprotector" "format" ];
     # see libiconv. We want to target a lower minsdk
     postConfigure = ''
       echo "#undef HAVE_MEMFD_CREATE" >> aarch64-unknown-linux-android/fficonfig.h
     '';
   });
   gmp6 = (prev.gmp6.override { withStatic = true; }).overrideAttrs(_: {
-    hardeningDisable = [ "fortify" ];
+    hardeningDisable = [ "fortify" "stackprotector" "format" ];
   });
 })
