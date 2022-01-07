1e4032d741d4568fcc561c75245936b9ffe916bd
Moritz Angermann <moritz.angermann@gmail.com>
2022-01-07 09:21:49 +0800

    Fix android cross

--- overlays/android.nix
+++ overlays/android.nix
@@ -1,6 +1,5 @@
 final: prev: prev.lib.optionalAttrs prev.stdenv.hostPlatform.isAndroid ({
   # we really only want the static one.
-  bionic = prev.bionic.override { enableStatic = true; };
   libiconv = (prev.libiconv.override { enableStatic = true; enableShared = false; }).overrideAttrs(_: {
     hardeningDisable = [ "fortify" "stackprotector" "format" ];
     # For some reason building libiconv with nixpgks android setup produces
@@ -24,4 +23,6 @@ final: prev: prev.lib.optionalAttrs prev.stdenv.hostPlatform.isAndroid ({
   gmp6 = (prev.gmp6.override { withStatic = true; }).overrideAttrs(_: {
     hardeningDisable = [ "fortify" "stackprotector" "format" ];
   });
+}) // prev.lib.optionalAttrs prev.stdenv.targetPlatform.isAndroid ({
+  bionic = prev.bionic.override { enableStatic = true; };
 })
