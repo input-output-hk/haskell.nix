a4e85e4714a8f11dbb36923bc63c5e93fa25032a
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2022-02-23 18:22:00 +1300

    Fix for #1381 (introduced by #1378) (#1382)

--- lib/default.nix
+++ lib/default.nix
@@ -202,7 +202,7 @@ in {
   cleanGit = import ./clean-git.nix {
     inherit lib cleanSourceWith;
     git = gitMinimal;
-    inherit (pkgs.evalPackages.buildPackages) runCommand;
+    inherit (pkgs.evalPackages.buildPackages) runCommandLocal;
   };
 
   # Some times it is handy to temporarily use a relative path between git
