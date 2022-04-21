1b232a16f65ab1595f2f86cdf71a2174262c7c19
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2022-04-21 20:21:07 +1200

    Update materialization

--- materialized/ghcjs/alex/ghc8107/default.nix
+++ materialized/ghcjs/alex/ghc8107/default.nix
@@ -2,16 +2,16 @@
   pkgs = hackage:
     {
       packages = {
+        "array".revision = (((hackage."array")."0.5.4.0").revisions).default;
+        "bytestring".revision = (((hackage."bytestring")."0.10.12.0").revisions).default;
+        "filepath".revision = (((hackage."filepath")."1.4.2.1").revisions).default;
         "ghc-prim".revision = (((hackage."ghc-prim")."0.6.1").revisions).default;
         "base".revision = (((hackage."base")."4.14.3.0").revisions).default;
         "time".revision = (((hackage."time")."1.9.3").revisions).default;
-        "array".revision = (((hackage."array")."0.5.4.0").revisions).default;
         "directory".revision = (((hackage."directory")."1.3.6.0").revisions).default;
         "rts".revision = (((hackage."rts")."1.0.1").revisions).default;
-        "bytestring".revision = (((hackage."bytestring")."0.10.12.0").revisions).default;
         "deepseq".revision = (((hackage."deepseq")."1.4.4.0").revisions).default;
         "unix".revision = (((hackage."unix")."2.7.2.2").revisions).default;
-        "filepath".revision = (((hackage."filepath")."1.4.2.1").revisions).default;
         "integer-gmp".revision = (((hackage."integer-gmp")."1.0.3.0").revisions).default;
         "containers".revision = (((hackage."containers")."0.6.5.1").revisions).default;
         };
@@ -19,16 +19,16 @@
         version = "8.10.7";
         nix-name = "ghc8107";
         packages = {
+          "array" = "0.5.4.0";
+          "bytestring" = "0.10.12.0";
+          "filepath" = "1.4.2.1";
           "ghc-prim" = "0.6.1";
           "base" = "4.14.3.0";
           "time" = "1.9.3";
-          "array" = "0.5.4.0";
           "directory" = "1.3.6.0";
           "rts" = "1.0.1";
-          "bytestring" = "0.10.12.0";
           "deepseq" = "1.4.4.0";
           "unix" = "2.7.2.2";
-          "filepath" = "1.4.2.1";
           "integer-gmp" = "1.0.3.0";
           "containers" = "0.6.5.1";
           };
@@ -48,15 +48,15 @@
         packages = {
           "filepath".components.library.planned = lib.mkOverride 900 true;
           "bytestring".components.library.planned = lib.mkOverride 900 true;
-          "alex".components.exes."alex".planned = lib.mkOverride 900 true;
           "ghc-prim".components.library.planned = lib.mkOverride 900 true;
           "array".components.library.planned = lib.mkOverride 900 true;
           "rts".components.library.planned = lib.mkOverride 900 true;
           "unix".components.library.planned = lib.mkOverride 900 true;
-          "deepseq".components.library.planned = lib.mkOverride 900 true;
           "directory".components.library.planned = lib.mkOverride 900 true;
-          "base".components.library.planned = lib.mkOverride 900 true;
           "time".components.library.planned = lib.mkOverride 900 true;
+          "alex".components.exes."alex".planned = lib.mkOverride 900 true;
+          "deepseq".components.library.planned = lib.mkOverride 900 true;
+          "base".components.library.planned = lib.mkOverride 900 true;
           "integer-gmp".components.library.planned = lib.mkOverride 900 true;
           "containers".components.library.planned = lib.mkOverride 900 true;
           };
--- materialized/ghcjs/happy/ghc884/default.nix
+++ materialized/ghcjs/happy/ghc884/default.nix
@@ -2,12 +2,12 @@
   pkgs = hackage:
     {
       packages = {
-        "ghc-prim".revision = (((hackage."ghc-prim")."0.5.3").revisions).default;
-        "base".revision = (((hackage."base")."4.13.0.0").revisions).default;
         "array".revision = (((hackage."array")."0.5.4.0").revisions).default;
         "mtl".revision = (((hackage."mtl")."2.2.2").revisions).default;
-        "transformers".revision = (((hackage."transformers")."0.5.6.2").revisions).default;
+        "ghc-prim".revision = (((hackage."ghc-prim")."0.5.3").revisions).default;
+        "base".revision = (((hackage."base")."4.13.0.0").revisions).default;
         "rts".revision = (((hackage."rts")."1.0").revisions).default;
+        "transformers".revision = (((hackage."transformers")."0.5.6.2").revisions).default;
         "deepseq".revision = (((hackage."deepseq")."1.4.4.0").revisions).default;
         "integer-gmp".revision = (((hackage."integer-gmp")."1.0.2.0").revisions).default;
         "containers".revision = (((hackage."containers")."0.6.2.1").revisions).default;
@@ -16,12 +16,12 @@
         version = "8.8.4";
         nix-name = "ghc884";
         packages = {
-          "ghc-prim" = "0.5.3";
-          "base" = "4.13.0.0";
           "array" = "0.5.4.0";
           "mtl" = "2.2.2";
-          "transformers" = "0.5.6.2";
+          "ghc-prim" = "0.5.3";
+          "base" = "4.13.0.0";
           "rts" = "1.0";
+          "transformers" = "0.5.6.2";
           "deepseq" = "1.4.4.0";
           "integer-gmp" = "1.0.2.0";
           "containers" = "0.6.2.1";
@@ -42,11 +42,11 @@
         packages = {
           "ghc-prim".components.library.planned = lib.mkOverride 900 true;
           "array".components.library.planned = lib.mkOverride 900 true;
-          "mtl".components.library.planned = lib.mkOverride 900 true;
           "rts".components.library.planned = lib.mkOverride 900 true;
+          "happy".components.exes."happy".planned = lib.mkOverride 900 true;
+          "mtl".components.library.planned = lib.mkOverride 900 true;
           "transformers".components.library.planned = lib.mkOverride 900 true;
           "deepseq".components.library.planned = lib.mkOverride 900 true;
-          "happy".components.exes."happy".planned = lib.mkOverride 900 true;
           "base".components.library.planned = lib.mkOverride 900 true;
           "integer-gmp".components.library.planned = lib.mkOverride 900 true;
           "containers".components.library.planned = lib.mkOverride 900 true;
