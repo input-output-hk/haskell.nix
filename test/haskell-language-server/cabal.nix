8aa6dc63717dca5afe4e30dfcff11434de978326
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2022-05-31 12:33:52 +1200

    Fix for hls in ci

--- test/haskell-language-server/cabal.nix
+++ test/haskell-language-server/cabal.nix
@@ -1,9 +1,12 @@
 { testSrc, evalPackages, buildPackages, compiler-nix-name, recurseIntoAttrs }:
 let
-  inherit (buildPackages.haskell-nix.tool compiler-nix-name "haskell-language-server" "latest") project;
+  inherit (buildPackages.haskell-nix.tool compiler-nix-name "haskell-language-server" { modules = [{ reinstallableLibGhc = true; }]; }) project;
 in recurseIntoAttrs {
   ifdInputs = {
     inherit (project) plan-nix;
   };
   build = project.getComponent "haskell-language-server:exe:haskell-language-server";
+
+  # Haskell Language Server in hackage does not build for GHC 9.2 yet
+  meta.disabled = __elem compiler-nix-name ["ghc921" "ghc922" "ghc923"];
 }
