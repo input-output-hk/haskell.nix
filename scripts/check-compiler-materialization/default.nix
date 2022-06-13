74be835961a9dec92f8214b234e7d271cc7f506d
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2022-06-13 16:45:50 +1200

    Fixes for materialization

--- scripts/check-compiler-materialization/default.nix
+++ scripts/check-compiler-materialization/default.nix
@@ -27,7 +27,8 @@ in builtins.listToAttrs (builtins.concatMap (system: builtins.concatMap (compile
   { name = "${prefix}-extra";         value = pkgs.ghc-extra-projects.${compiler-nix-name}.plan-nix; }
   { name = "${prefix}-boot";          value = pkgs.ghc-boot-packages-nix.${compiler-nix-name}; }
   { name = "${prefix}-hello";         value = pkgs.haskell-nix.tool compiler-nix-name "hello" {}; }
-] ++ eval.lib.optionals (!__elem system ["aarch64-darwin" "aarch64-linux"]) [
+] ++ eval.lib.optionals (!__elem system ["aarch64-darwin" "aarch64-linux"]
+         && !__elem compiler-nix-name ["ghc881" "ghc882" "ghc883"]) [
   { name = "${prefix}-windows";       value = pkgsForWindows.pkgsCross.mingwW64.ghc-extra-projects.${compiler-nix-name}.plan-nix; }
   { name = "${prefix}-hello-windows"; value = pkgsForWindows.pkgsCross.mingwW64.haskell-nix.tool compiler-nix-name "hello" {}; }
 ] ++ eval.lib.optionals (system == "x86_64-linux") [
@@ -38,7 +39,9 @@ in builtins.listToAttrs (builtins.concatMap (system: builtins.concatMap (compile
   { name = "${prefix}-arm";           value = pkgs.pkgsCross.aarch64-multiplatform.ghc-extra-projects.${compiler-nix-name}.plan-nix; }
 ] ++ eval.lib.optionals (system == "x86_64-linux" && __elem compiler-nix-name ["ghc884" "ghc8106" "ghc8107"]) [
   { name = "${prefix}-hello-arm";     value = pkgs.pkgsCross.aarch64-multiplatform.haskell-nix.tool compiler-nix-name "hello" {}; }
-] ++ eval.lib.optionals (!__elem system ["aarch64-darwin" "aarch64-linux"] && __elem compiler-nix-name ["ghc865" "ghc884" "ghc8105" "ghc8106" "ghc8107"]) [
+] ++ eval.lib.optionals (
+        (system == "x86_64-linux"  && __elem compiler-nix-name ["ghc865" "ghc884" "ghc8105" "ghc8106" "ghc8107"])
+     || (system == "x86_64-darwin" && __elem compiler-nix-name ["ghc8107"])) [
   { name = "${prefix}-ghcjs";         value = pkgs.pkgsCross.ghcjs.ghc-extra-projects.${compiler-nix-name}.plan-nix; }
   { name = "${prefix}-hello-ghcjs";   value = pkgs.pkgsCross.ghcjs.haskell-nix.tool compiler-nix-name "hello" {}; }
 ])) compiler-nix-names) systems)
