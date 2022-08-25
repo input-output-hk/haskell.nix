64bd94a69b0c6104253bdfc8b33982048edd2716
Scott Murphy <scottmurphy09@gmail.com>
2022-08-25 08:37:39 -0500

    added default resolver

--- lib/fetch-resolver.nix
+++ lib/fetch-resolver.nix
@@ -2,24 +2,36 @@
 { pkgs }:
 { src
 , stackYaml ? "stack.yaml"
+, defaultResolver ? "resolver: lts-16.31" # default resolver is given to fix null case
 , resolverSha256 ? null
 }:
 let
     # Using origSrcSubDir bypasses any cleanSourceWith so that it will work when
     # access to the store is restricted.  If origSrc was already in the store
     # you can pass the project in as a string.
-    rawStackYaml = builtins.readFile ((src.origSrcSubDir or src) + ("/" + stackYaml));
-
+  rawStackYaml = builtins.readFile ((src.origSrcSubDir or src) + ("/" + stackYaml));
+   
+    defaultResolver = "resolver: lts-16.31";
     # Determine the resolver as it may point to another file we need
     # to look at.
     resolver =
-      let
-        rs = pkgs.lib.lists.concatLists (
-          pkgs.lib.lists.filter (l: l != null)
-            (builtins.map (l: builtins.match "^resolver: *(.*)" l)
-              (pkgs.lib.splitString "\n" rawStackYaml)));
-      in
-        pkgs.lib.lists.head (rs ++ [ null ]);
+      let 
+        yamlList = pkgs.lib.splitString "\n" rawStackYaml;
+        resolverFind = pkgs.lib.foldl' (l: a: if builtins.isNull (builtins.match "^resolver: *(.*)" a)
+                                              then defaultResolver
+                                              else a
+        ) [] yamlList;
+        concatLists = builtins.concatLists resolverFind ;
+      in "resolver: lts-16.31"; # builtins.trace resolverFind resolverFind;
+
+    # resolver =
+    #   let
+    #     rs = pkgs.lib.lists.concatLists (
+    #       pkgs.lib.lists.filter (l: l != null)
+    #         (builtins.trace "*(.*)" (builtins.map (l: builtins.match "^resolver: *(.*)" l))
+    #           (builtins.trace ( rawStackYaml)  (pkgs.lib.splitString "\n" rawStackYaml))));
+    #   in
+    #     builtins.trace ("get resolver") (pkgs.lib.lists.head ( (builtins.trace "rs" rs) ++ [ null ]));
 
     # If we found a resolver and we have a resolverSha256 then we should download it.
     fetchedResolver =
@@ -29,5 +41,14 @@ let
           sha256 = resolverSha256;
         }
         else null;
+
+
+
+
+
+  
+    
+
+
 
 in { inherit resolver fetchedResolver; }
