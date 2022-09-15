a197fd0afb0241649633ee9ada376400c7d62719
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2022-09-15 17:13:05 +1200

    Add cabal-issue-8352-workaround (#1671)
    * Add cabal-issue-8352-workaround
    
    See https://github.com/haskell/cabal/issues/8352
    
    * More script fixes
    
    * More script fixes

--- build.nix
+++ build.nix
@@ -49,7 +49,7 @@ in rec {
       # Update scripts use the internal nix-tools and cabal-install (compiled with a fixed GHC version)
       nix-tools = haskell.internal-nix-tools;
       cabal-install = haskell.internal-cabal-install;
-      inherit (haskell) update-index-state-hashes;
+      inherit (haskell) update-index-state-hashes cabal-issue-8352-workaround;
     };
     update-stackage = haskell.callPackage ./scripts/update-stackage.nix {
       inherit (pkgs) stdenv lib writeScript coreutils glibc git
@@ -57,6 +57,7 @@ in rec {
       # Update scripts use the internal nix-tools and cabal-install (compiled with a fixed GHC version)
       nix-tools = haskell.internal-nix-tools;
       cabal-install = haskell.internal-cabal-install;
+      inherit (haskell) cabal-issue-8352-workaround;
     };
     update-pins = haskell.callPackage ./scripts/update-pins.nix {};
     update-docs = pkgs.buildPackages.callPackage ./scripts/update-docs.nix {
