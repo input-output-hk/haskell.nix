6e458f243fe0814ece0a75f2de5978ff7c37a8b5
Yvan Sraka <yvan@sraka.xyz>
2022-10-05 19:51:07 +0200

    Fix never ending builtkite job

--- docs/tests.sh
+++ docs/tests.sh
@@ -6,32 +6,32 @@ pushd tutorials
 ## Getting started
 pushd getting-started
 nix-build
-nix-shell --run "cabal build hello"
+nix-shell --pure --run "cabal build hello"
 popd
 
 ## Getting started with flakes
 pushd getting-started-flakes
-nix build
-nix develop . -c cabal build hello
+nix build --accept-flake-config
+nix develop . --accept-flake-config -c cabal build hello
 popd
 
 ## Development
 pushd development
 pwd
-nix-shell --pure --run "cabal build hello" # FIXME
-nix-shell --pure shell-hoogle.nix --run "cabal build hello" # FIXME --run "hoogle server --local"
-nix-shell --pure shell-package.nix --run "cabal build hello" # FIXME
-nix-shell --pure shell-stackage.nix --run "cabal build hello" # FIXME
+nix-shell --pure --run ""
+nix-shell --pure shell-hoogle.nix --run ""
+nix-shell --pure shell-package.nix --run ""
+nix-shell --pure shell-stackage.nix --run ""
 popd
 
 ## Bumping Hackage and Stackage snapshots
 pushd hackage-stackage
-nix build
+nix build --accept-flake-config
 popd
 
 ## Handling git repositories in projects
 pushd source-repository-hashes
-nix build
+nix build --accept-flake-config
 popd
 
 # TODO
@@ -47,7 +47,7 @@ pushd template
 
 # IOHK's nix tooling
 pushd iohk-nix
-nix-build
+nix build --accept-flake-config
 popd
 
 popd
