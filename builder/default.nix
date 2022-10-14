57cb1649c92e1d4bdbe443effd495ff2aa75277d
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2022-10-14 17:03:47 +1300

    Use inputMap for soruce of packages in custom repository (#1750)
    * Use inputMap for soruce of packages in custom repository
    
    * Include inputMap for hspkg-builder to use
    
    * Use inputMap for soruce of packages in custom repository
    
    * Fix url replacement.
    
    * Check all the urls
    
    * Add comments

--- builder/default.nix
+++ builder/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, buildPackages, evalPackages, stdenv, lib, haskellLib, ghc, compiler-nix-name, fetchurl, pkgconfig, nonReinstallablePkgs, hsPkgs, compiler }:
+{ pkgs, buildPackages, evalPackages, stdenv, lib, haskellLib, ghc, compiler-nix-name, fetchurl, pkgconfig, nonReinstallablePkgs, hsPkgs, compiler, inputMap }:
 
 let
   # Builds a single component of a package.
@@ -83,7 +83,7 @@ in {
   # Build a Haskell package from its config.
   # TODO: this pkgs is the adjusted pkgs, but pkgs.pkgs is unadjusted
   build-package = haskellLib.weakCallPackage pkgs ./hspkg-builder.nix {
-    inherit haskellLib ghc compiler-nix-name comp-builder setup-builder;
+    inherit haskellLib ghc compiler-nix-name comp-builder setup-builder inputMap;
   };
 
   inherit shellFor makeConfigFiles;
