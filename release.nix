4aaa768542330eda900370c92b3f41b66cc4320f
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2022-10-26 18:19:53 +1300

    Add inputs arg back to release.nix

--- release.nix
+++ release.nix
@@ -1,14 +1,24 @@
 # TODO remove this file when we no longer need to build with the
 # non flake hydra configuration.
 {
-  supportedSystems ? ["x86_64-linux" "x86_64-darwin"],
+  supportedSystems ? ["x86_64-linux" "x86_64-darwin"]
+, include ? (compiler-nix-name: true)
 }: let
   defaultNix = import ./. {};
 
   inherit (defaultNix) pkgs;
   inherit (pkgs) lib;
 
-  jobs = lib.getAttrs supportedSystems defaultNix.ciJobs;
+  filterCiJobs = __mapAttrs (systemName: system:
+    lib.optionalAttrs (__elem systemName supportedSystems) (
+      __mapAttrs (n: jobs:
+        let ghcMatch = __match ".*-(ghc[0-9]*)-.*" n;
+        in
+          lib.optionalAttrs (
+            (n == "latest" && include "ghc8107")
+            || (ghcMatch != null && include (__head ghcMatch))) jobs) system));
+
+  jobs = lib.getAttrs supportedSystems (filterCiJobs defaultNix.ciJobs);
 
   required = defaultNix.pkgs.releaseTools.aggregate {
     name = "github-required";
