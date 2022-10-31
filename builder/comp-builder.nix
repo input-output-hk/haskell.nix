input-output-hk/haskell.nix

commit ec0c59e2de05053c21301bc959a27df92fe93376

Support for packages with only tests (#1732)
* Support for packages with only tests

Fixes #362

* Fix for ghcjs

builder/comp-builder.nix
@@ -454,11 +454,13 @@ let
         target-pkg-and-db = "${ghc.targetPrefix}ghc-pkg -v0 --package-db $out/package.conf.d";
       in ''
       runHook preInstall
-      $SETUP_HS copy ${lib.concatStringsSep " " (
-        setupInstallFlags
-        ++ lib.optional configureAllComponents
-              (haskellLib.componentTarget componentId)
-      )}
+      ${ # `Setup copy` does not install tests and benchmarks.
+        lib.optionalString (!haskellLib.isTest componentId && !haskellLib.isBenchmark componentId) ''
+          $SETUP_HS copy ${lib.concatStringsSep " " (
+            setupInstallFlags
+            ++ lib.optional configureAllComponents
+                  (haskellLib.componentTarget componentId)
+          )}''}
       ${lib.optionalString (haskellLib.isLibrary componentId) ''
         $SETUP_HS register --gen-pkg-config=${name}.conf
         ${ghc.targetPrefix}ghc-pkg -v0 init $out/package.conf.d
@@ -512,7 +514,7 @@ let
         if [ -f ${testExecutable} ]; then
           mkdir -p $(dirname $out/bin/${exeName})
           ${if stdenv.hostPlatform.isGhcjs then ''
-            cat <(echo \#!${lib.getBin buildPackages.nodejs-12_x}/bin/node) ${testExecutable} >| $out/bin/${exeName}
+            cat <(echo \#!${lib.getBin buildPackages.nodejs-18_x}/bin/node) ${testExecutable} >| $out/bin/${exeName}
             chmod +x $out/bin/${exeName}
           '' else ''
              cp -r ${testExecutable} $(dirname $out/bin/${exeName})

test/default.nix
@@ -215,6 +215,7 @@ let
     exe-lib-dlls = callTest ./exe-lib-dlls { inherit util; };
     ca-derivations = callTest ./ca-derivations { inherit CADerivationsEnabled; };
     ca-derivations-include = callTest ./ca-derivations-include { inherit CADerivationsEnabled; };
+    test-only = callTest ./test-only { inherit util; };
 
     unit = unitTests;
   };

test/test-only/Main.hs
@@ -1,0 +1,2 @@
+module Main where
+main = return ()

test/test-only/cabal.project
@@ -1,0 +1,1 @@
+packages: .

test/test-only/default.nix
@@ -1,0 +1,17 @@
+{ stdenv, lib, util, project', haskellLib, recurseIntoAttrs, testSrc, compiler-nix-name, evalPackages }:
+
+with lib;
+
+let
+  project = project' {
+    inherit compiler-nix-name evalPackages;
+    src = testSrc "test-only";
+  };
+
+in recurseIntoAttrs {
+  ifdInputs = {
+    inherit (project) plan-nix;
+  };
+
+  run = project.hsPkgs.test-only.checks.my-test;
+}

test/test-only/test-only.cabal
@@ -1,0 +1,8 @@
+cabal-version: 1.24
+name: test-only
+version: 1
+build-type: Simple
+test-suite my-test
+  type: exitcode-stdio-1.0
+  main-is: Main.hs
+  build-depends: base

commit e3cd6e05c7189542f61d1099360b1c96fc6316da

Fix datadir for tests and benchmarks
Broken by #1732

builder/comp-builder.nix
@@ -461,12 +461,28 @@ let
       in ''
       runHook preInstall
       ${ # `Setup copy` does not install tests and benchmarks.
-        lib.optionalString (!haskellLib.isTest componentId && !haskellLib.isBenchmark componentId) ''
-          $SETUP_HS copy ${lib.concatStringsSep " " (
-            setupInstallFlags
-            ++ lib.optional configureAllComponents
-                  (haskellLib.componentTarget componentId)
-          )}''}
+        if !haskellLib.isTest componentId && !haskellLib.isBenchmark componentId
+          then ''
+            $SETUP_HS copy ${lib.concatStringsSep " " (
+              setupInstallFlags
+              ++ lib.optional configureAllComponents
+                    (haskellLib.componentTarget componentId)
+            )}''
+          else
+            # However if there are exes or libraries it does copy the datadir.
+            # So run it, but expect it might complain there was nothing to do.
+            ''
+            SETUP_OUTPUT=$(mktemp)
+            if $SETUP_HS copy ${lib.concatStringsSep " " (
+              setupInstallFlags
+              ++ lib.optional configureAllComponents
+                    (haskellLib.componentTarget componentId)
+              )} 2>&1 | tee $SETUP_OUTPUT; then
+              echo Setup copy success
+            else
+              cat $SETUP_OUTPUT | grep 'Error: Setup: No executables and no library found\. Nothing to do\.'
+            fi
+            ''}
       ${lib.optionalString (haskellLib.isLibrary componentId) ''
         $SETUP_HS register --gen-pkg-config=${name}.conf
         ${ghc.targetPrefix}ghc-pkg -v0 init $out/package.conf.d