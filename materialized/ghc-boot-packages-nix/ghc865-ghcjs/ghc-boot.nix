7106a3b6f771cedc0ea5d61db279613841e8fffe
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2022-12-08 02:25:52 +1300

    update materialization

--- materialized/ghc-boot-packages-nix/ghc865-ghcjs/ghc-boot.nix
+++ materialized/ghc-boot-packages-nix/ghc865-ghcjs/ghc-boot.nix
@@ -11,7 +11,7 @@
     flags = {};
     package = {
       specVersion = "1.22";
-      identifier = { name = "ghc-boot"; version = "8.8.2"; };
+      identifier = { name = "ghc-boot"; version = "8.6.5"; };
       license = "BSD-3-Clause";
       copyright = "";
       maintainer = "ghc-devs@haskell.org";
@@ -21,14 +21,6 @@
       synopsis = "Shared functionality between GHC and its boot libraries";
       description = "This library is shared between GHC, ghc-pkg, and other boot\nlibraries.\n\nA note about \"GHC.PackageDb\": it only deals with the subset of\nthe package database that the compiler cares about: modules\npaths etc and not package metadata like description, authors\netc. It is thus not a library interface to ghc-pkg and is *not*\nsuitable for modifying GHC package databases.\n\nThe package database format and this library are constructed in\nsuch a way that while ghc-pkg depends on Cabal, the GHC library\nand program do not have to depend on Cabal.";
       buildType = "Simple";
-      isLocal = true;
-      detailLevel = "FullDetails";
-      licenseFiles = [ "LICENSE" ];
-      dataDir = ".";
-      dataFiles = [];
-      extraSrcFiles = [ "changelog.md" ];
-      extraTmpFiles = [];
-      extraDocFiles = [];
       };
     components = {
       "library" = {
@@ -41,13 +33,6 @@
           (hsPkgs."ghc-boot-th" or (errorHandler.buildDepError "ghc-boot-th"))
           ];
         buildable = true;
-        modules = [
-          "GHC/LanguageExtensions"
-          "GHC/PackageDb"
-          "GHC/Serialized"
-          "GHC/ForeignSrcLang"
-          "GHC/HandleEncoding"
-          ];
         };
       };
-    } // rec { src = (pkgs.lib).mkDefault ../libraries/ghc-boot; }
+    } // rec { src = (pkgs.lib).mkDefault ./.; }
--- materialized/ghc-extra-projects/ghcjs/ghc884/default.nix
+++ materialized/ghc-extra-projects/ghcjs/ghc884/default.nix
@@ -2,19 +2,18 @@
   pkgs = hackage:
     {
       packages = {
+        "ghc-boot".revision = (((hackage."ghc-boot")."8.8.2").revisions).default;
         "pretty".revision = (((hackage."pretty")."1.1.3.6").revisions).default;
         "array".revision = (((hackage."array")."0.5.4.0").revisions).default;
         "network".revision = (((hackage."network")."2.6.3.6").revisions).default;
         "bytestring".revision = (((hackage."bytestring")."0.10.10.0").revisions).default;
         "filepath".revision = (((hackage."filepath")."1.4.2.1").revisions).default;
         "ghcjs-prim".revision = (((hackage."ghcjs-prim")."0.1.1.0").revisions).default;
-        "terminfo".revision = (((hackage."terminfo")."0.4.1.5").revisions).default;
         "ghc-heap".revision = (((hackage."ghc-heap")."8.8.2").revisions).default;
         "ghc-prim".revision = (((hackage."ghc-prim")."0.5.3").revisions).default;
         "ghc-boot-th".revision = (((hackage."ghc-boot-th")."8.8.2").revisions).default;
         "base".revision = (((hackage."base")."4.13.0.0").revisions).default;
         "time".revision = (((hackage."time")."1.9.3").revisions).default;
-        "process".revision = (((hackage."process")."1.6.7.0").revisions).default;
         "directory".revision = (((hackage."directory")."1.3.4.0").revisions).default;
         "rts".revision = (((hackage."rts")."1.0").revisions).default;
         "transformers".revision = (((hackage."transformers")."0.5.6.2").revisions).default;
@@ -22,13 +21,14 @@
         "deepseq".revision = (((hackage."deepseq")."1.4.4.0").revisions).default;
         "unix".revision = (((hackage."unix")."2.7.2.2").revisions).default;
         "integer-gmp".revision = (((hackage."integer-gmp")."1.0.2.0").revisions).default;
-        "binary".revision = (((hackage."binary")."0.8.9.1").revisions).default;
-        "containers".revision = (((hackage."containers")."0.5.11.0").revisions).default;
+        "binary".revision = (((hackage."binary")."0.8.7.0").revisions).default;
+        "containers".revision = (((hackage."containers")."0.6.2.1").revisions).default;
         };
       compiler = {
         version = "8.8.4";
         nix-name = "ghc884";
         packages = {
+          "ghc-boot" = "8.8.2";
           "pretty" = "1.1.3.6";
           "array" = "0.5.4.0";
           "bytestring" = "0.10.10.0";
@@ -39,7 +39,6 @@
           "ghc-boot-th" = "8.8.2";
           "base" = "4.13.0.0";
           "time" = "1.9.3";
-          "process" = "1.6.7.0";
           "directory" = "1.3.4.0";
           "rts" = "1.0";
           "transformers" = "0.5.6.2";
@@ -47,6 +46,8 @@
           "deepseq" = "1.4.4.0";
           "unix" = "2.7.2.2";
           "integer-gmp" = "1.0.2.0";
+          "binary" = "0.8.7.0";
+          "containers" = "0.6.2.1";
           };
         };
       };
@@ -54,10 +55,8 @@
     {
       packages = {
         iserv-proxy = ./.plan.nix/iserv-proxy.nix;
-        ghc = ./.plan.nix/ghc.nix;
         Win32 = ./.plan.nix/Win32.nix;
         ghci = ./.plan.nix/ghci.nix;
-        ghc-boot = ./.plan.nix/ghc-boot.nix;
         iserv = ./.plan.nix/iserv.nix;
         libiserv = ./.plan.nix/libiserv.nix;
         hpc = ./.plan.nix/hpc.nix;
@@ -68,20 +67,8 @@
       {
         packages = {
           "iserv-proxy" = { flags = {}; };
-          "ghc" = {
-            flags = {
-              "stage2" = lib.mkOverride 900 false;
-              "integer-simple" = lib.mkOverride 900 false;
-              "terminfo" = lib.mkOverride 900 true;
-              "ghci" = lib.mkOverride 900 true;
-              "stage3" = lib.mkOverride 900 false;
-              "integer-gmp" = lib.mkOverride 900 false;
-              "stage1" = lib.mkOverride 900 false;
-              };
-            };
           "Win32" = { flags = {}; };
           "ghci" = { flags = { "ghci" = lib.mkOverride 900 true; }; };
-          "ghc-boot" = { flags = {}; };
           "iserv" = { flags = {}; };
           "libiserv" = { flags = { "network" = lib.mkOverride 900 true; }; };
           "hpc" = { flags = {}; };
@@ -103,9 +90,7 @@
           "iserv".components.exes."iserv".planned = lib.mkOverride 900 true;
           "time".components.library.planned = lib.mkOverride 900 true;
           "network".components.library.planned = lib.mkOverride 900 true;
-          "ghc".components.library.planned = lib.mkOverride 900 true;
           "ghcjs-prim".components.library.planned = lib.mkOverride 900 true;
-          "process".components.library.planned = lib.mkOverride 900 true;
           "template-haskell".components.library.planned = lib.mkOverride 900 true;
           "ghci".components.library.planned = lib.mkOverride 900 true;
           "ghc-boot".components.library.planned = lib.mkOverride 900 true;
@@ -118,7 +103,6 @@
           "integer-gmp".components.library.planned = lib.mkOverride 900 true;
           "containers".components.library.planned = lib.mkOverride 900 true;
           "iserv-proxy".components.exes."iserv-proxy".planned = lib.mkOverride 900 true;
-          "terminfo".components.library.planned = lib.mkOverride 900 true;
           };
         })
     ];
