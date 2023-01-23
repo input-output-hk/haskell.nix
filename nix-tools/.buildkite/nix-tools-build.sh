f305c8be5e3259d9e708917e3cc680feeda7560b
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-01-23 13:48:23 +1300

    Use nix-build

--- nix-tools/.buildkite/nix-tools-build.sh
+++ nix-tools/.buildkite/nix-tools-build.sh
@@ -60,8 +60,8 @@ nix build -f .buildkite/nix2 nix-tools.components.exes --no-link
 echo
 echo "--- Test index file truncation"
 
-nix build --check -f test/truncate-index.nix --no-link \
+nix-build --check test/truncate-index.nix --no-link \
     --arg nix-tools-path ./.buildkite/nix2  \
     --argstr index-state "$index_state" \
     --argstr hash "$expected_hash" \
-    indexTruncated
+    -A indexTruncated
