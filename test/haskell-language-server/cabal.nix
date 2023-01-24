07d19825a5d6766ccf2abb6fd894eb5c15604dde
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-01-25 02:24:37 +1300

    Use hls 1.9 on GHC 9.0 and 9.2

--- test/haskell-language-server/cabal.nix
+++ test/haskell-language-server/cabal.nix
@@ -2,7 +2,7 @@
 let
   inherit (haskell-nix.tool compiler-nix-name "haskell-language-server" {
     version =
-      if __compareVersions haskell-nix.compiler.${compiler-nix-name}.version "9.4" < 0
+      if __compareVersions haskell-nix.compiler.${compiler-nix-name}.version "9.0" < 0
         then "1.8.0.0"
         else "latest";
     inherit evalPackages; }) project;
