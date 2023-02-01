4a625b700066b1b3751937ba179394135fa51b90
Moritz Angermann <moritz.angermann@gmail.com>
2023-02-01 09:21:41 +0000

    Support for armv7a-bionic (android)
    This adds support to haskell.nix for building for armv7a-android.
    It includes
    - various minor fixes
    - a lot of bionic (libc) patches for ghc
    - no gold on 32bit android
    - android overlay: mostly static
    - disable KTLS on openssl for android

--- overlays/android.nix
+++ overlays/android.nix
@@ -11,18 +11,31 @@ final: prev: prev.lib.optionalAttrs prev.stdenv.hostPlatform.isAndroid ({
     '';
     # my current thinking is that this is due to the android toolchain using r23, api30.
   });
-  libffi = prev.libffi.overrideAttrs (_: {
+  libffi = prev.libffi.overrideAttrs ( old: {
     dontDisableStatic = true;
+    configureFlags = old.configureFlags ++ [ "--disable-shared" ];
 
     hardeningDisable = [ "fortify" "stackprotector" "format" ];
+  } // prev.lib.optionalAttrs (prev.stdenv.hostPlatform.isAarch32 || prev.stdenv.hostPlatform.isAarch64) {
     # see libiconv. We want to target a lower minsdk
     postConfigure = ''
-      echo "#undef HAVE_MEMFD_CREATE" >> aarch64-unknown-linux-android/fficonfig.h
+      echo "#undef HAVE_MEMFD_CREATE" >> ${prev.stdenv.hostPlatform.config}/fficonfig.h
     '';
   });
-  gmp6 = (prev.gmp6.override { withStatic = true; }).overrideAttrs(_: {
+  gmp6 = (prev.gmp6.override { withStatic = true; }).overrideAttrs(old: {
     hardeningDisable = [ "fortify" "stackprotector" "format" ];
+    configureFlags = old.configureFlags ++ [ "--disable-shared" ];
   });
+  zlib = prev.zlib.override { shared = false; static = true; };
+  # kernel tls (ktls) doesn't work with the android kernel. And will complain
+  # about lots of implicitly declared functions and undeclared identifiers,
+  # because the android (linux) kernel doesn't expose those.
+  openssl = prev.openssl.override { static = true; enableKTLS = false; };
+  
 }) // prev.lib.optionalAttrs prev.stdenv.targetPlatform.isAndroid ({
-  bionic = prev.bionic.override { enableStatic = true; };
+  # we still need the shared libraries to link against on the platform.  GHC
+  # has been neutered to not even try loading shared libs and will use dynamic ones.
+  # We also link iserv against the static libs, so that we have a fully static
+  # android (bionic/linux) iserv we can execute on glibc/linux.
+  bionic = prev.bionic.override { enableStatic = true; enableShared = true; };
 })
