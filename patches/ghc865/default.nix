8c110164d3e5af8f7a7850a2a5505da8db73e385
Andrea Bedini <andrea.bedini@tweag.io>
2023-02-28 17:10:49 +0800

    Update patches

--- patches/ghc865/default.nix
+++ patches/ghc865/default.nix
@@ -1,7 +1,6 @@
 {
   packages.binary.patches = [ ({ version }: if version == "0.8.6.0" then ./binary-0.8.6.0.patch else null) ];
   packages.containers.patches = [ ({ version }: if version == "0.6.0.1" then ./containers-0.6.0.1.patch else null) ];
-  packages.parsec.patches = [ ({ version }: if version == "3.1.13.0" then ./parsec-3.1.13.0.patch else null) ];
   packages.singletons.patches = [ ({ version }: if version == "2.5.1" then ./singletons-2.5.1.patch else null) ];
   packages.unix.patches = [ ({ version }: if version == "2.7.2.2" then ./unix-2.7.2.2.patch else null) ];
 }
