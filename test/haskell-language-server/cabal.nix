b4dddbdc0b34130d12a331c99ea6eb9c8c79996b
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-03-10 12:36:44 +1300

    Fix hls tests

--- test/haskell-language-server/cabal.nix
+++ test/haskell-language-server/cabal.nix
@@ -13,5 +13,5 @@ in recurseIntoAttrs {
   build = project.getComponent "haskell-language-server:exe:haskell-language-server";
 
   # hls does not need to be cross compiled.
-  meta.disabled = stdenv.hostPlatform != stdenv.buildPlatform || __elem compiler-nix-name ["ghc941" "ghc942" "ghc943" "ghc944"];
+  meta.disabled = stdenv.hostPlatform != stdenv.buildPlatform || __elem compiler-nix-name ["ghc941" "ghc942" "ghc943" "ghc944" "ghc96020230302"];
 }
