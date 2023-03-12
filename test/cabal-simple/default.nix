71c53a3645e937295477ee21c943e6701035d796
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-03-12 19:53:11 +1300

    Fix compiler-nix-name

--- test/cabal-simple/default.nix
+++ test/cabal-simple/default.nix
@@ -19,7 +19,7 @@ let
     cabalProject = ''
       packages: .
       allow-newer: aeson:*
-    '' + lib.optionalString (__elem cabal-nix-name ["ghc96020230302"]) ''
+    '' + lib.optionalString (__elem compiler-nix-name ["ghc96020230302"]) ''
       allow-newer: aeson:*, *:base, *:ghc-prim, *:template-haskell
     '';
   };
