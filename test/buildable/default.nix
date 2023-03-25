e9716146aa0fdb8ba9d4cce677f3eaf9a7b23e19
Moritz Angermann <moritz.angermann@gmail.com>
2023-03-25 15:01:34 +0800

    Update default.nix

--- test/buildable/default.nix
+++ test/buildable/default.nix
@@ -26,7 +26,7 @@ in recurseIntoAttrs {
     '';
 
     meta = rec {
-      platforms = platforms.all;
+      platforms = lib.platforms.all;
       broken = stdenv.hostPlatform.isGhcjs;
       disabled = broken;
     };
