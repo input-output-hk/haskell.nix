fd6b589375d8cb8ed9d693800be19c73c650769f
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-05-18 23:46:37 +1200

    Backport fix for m32_allocator_init to GHC 9.2
    See https://gitlab.haskell.org/ghc/ghc/-/merge_requests/10453

--- overlays/bootstrap.nix
+++ overlays/bootstrap.nix
@@ -154,7 +154,8 @@ in {
                 ++ final.lib.optional (versionAtLeast "9.2.1"  && versionLessThan "9.3" && final.stdenv.targetPlatform.isAarch64) ./patches/ghc/ghc-9.2-3434.patch
 
                 ++ fromUntil "8.10.1" "9.4"    ./patches/ghc/ghc-acrt-iob-func.patch
-                ++ fromUntil "8.10.1" "9.4"    ./patches/ghc/ghc-mprotect-nonzero-len.patch
+                ++ fromUntil "8.10.1" "9.2"    ./patches/ghc/ghc-mprotect-nonzero-len.patch
+                ++ fromUntil "9.2"    "9.4"    ./patches/ghc/ghc-9.2-fix-m32_allocator_init-10453.patch
 
                 ++ fromUntil "8.10.1" "8.10.3" ./patches/ghc/ghc-8.10-ubxt.patch
                 ++ fromUntil "8.10.3" "8.10.5" ./patches/ghc/ghc-8.10.3-ubxt.patch
