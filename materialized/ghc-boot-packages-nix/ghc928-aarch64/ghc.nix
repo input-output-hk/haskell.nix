fc07b4447b8f30b160d447ee4d1dcd28ea419ad6
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-05-27 17:46:30 +1200

    Materialized files

---
+++ materialized/ghc-boot-packages-nix/ghc928-aarch64/ghc.nix
@@ -0,0 +1,60 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {
+      internal-interpreter = false;
+      stage1 = false;
+      stage2 = false;
+      stage3 = false;
+      terminfo = true;
+      dynamic-system-linker = true;
+      };
+    package = {
+      specVersion = "1.22";
+      identifier = { name = "ghc"; version = "9.2.8"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "glasgow-haskell-users@haskell.org";
+      author = "The GHC Team";
+      homepage = "http://www.haskell.org/ghc/";
+      url = "";
+      synopsis = "The GHC API";
+      description = "GHC's functionality can be useful for more things than just\ncompiling Haskell programs. Important use cases are programs\nthat analyse (and perhaps transform) Haskell code. Others\ninclude loading Haskell code dynamically in a GHCi-like manner.\nFor this reason, a lot of GHC's functionality is made available\nthrough this package.\n\nSee <https://gitlab.haskell.org/ghc/ghc/-/wikis/commentary/compiler>\nfor more information.";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."deepseq" or (errorHandler.buildDepError "deepseq"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          (hsPkgs."process" or (errorHandler.buildDepError "process"))
+          (hsPkgs."bytestring" or (errorHandler.buildDepError "bytestring"))
+          (hsPkgs."binary" or (errorHandler.buildDepError "binary"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."array" or (errorHandler.buildDepError "array"))
+          (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+          (hsPkgs."template-haskell" or (errorHandler.buildDepError "template-haskell"))
+          (hsPkgs."hpc" or (errorHandler.buildDepError "hpc"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."exceptions" or (errorHandler.buildDepError "exceptions"))
+          (hsPkgs."ghc-boot" or (errorHandler.buildDepError "ghc-boot"))
+          (hsPkgs."ghc-heap" or (errorHandler.buildDepError "ghc-heap"))
+          (hsPkgs."ghci" or (errorHandler.buildDepError "ghci"))
+          ] ++ (if system.isWindows
+          then [ (hsPkgs."Win32" or (errorHandler.buildDepError "Win32")) ]
+          else [
+            (hsPkgs."unix" or (errorHandler.buildDepError "unix"))
+            ] ++ (pkgs.lib).optional (flags.terminfo) (hsPkgs."terminfo" or (errorHandler.buildDepError "terminfo")));
+        buildable = true;
+        };
+      };
+    } // rec { src = (pkgs.lib).mkDefault ./.; }
