b7d83ad684ee7bd5c0c90d4a24c35914f39493b5
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-05-31 01:47:20 +1200

    Avoid rebuilding GHC at all

--- overlays/bootstrap.nix
+++ overlays/bootstrap.nix
@@ -210,7 +210,6 @@ in {
                 ++ final.lib.optional (versionAtLeast "8.10"   && versionLessThan "9.0" && final.stdenv.targetPlatform.isAarch64) ./patches/ghc/ghc-8.10-aarch64-handle-none-rela.patch
                 ++ final.lib.optional (versionAtLeast "9.0"                             && final.stdenv.targetPlatform.isAarch64) ./patches/ghc/ghc-9.0-better-symbol-addr-debug.patch
                 ++ final.lib.optional (versionAtLeast "9.0"                             && final.stdenv.targetPlatform.isAarch64) ./patches/ghc/ghc-9.0-aarch64-handle-none-rela.patch
-                ++ final.lib.optional (versionAtLeast "8.10.6")                                                                   ./patches/ghc/ghc-8.10.7-iserv-proxy-load-dlls.patch
                 ;
         in ({
             ghc865 = final.callPackage ../compiler/ghc (traceWarnOld "8.6" {
