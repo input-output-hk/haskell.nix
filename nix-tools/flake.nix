a06585a546c36222fc076bffd18534f9985b42aa
Andrea Bedini <andrea.bedini@tweag.io>
2023-08-09 18:00:16 +0800

    WIP: move everything nix-tools related to the subflake

--- nix-tools/flake.nix
+++ nix-tools/flake.nix
@@ -14,88 +14,86 @@
 
       inherit (nixpkgs) lib;
 
-      # A simple thing but hard to do without screwing up lazyness.
-      # We don't want packages.x to trigger evaluation of packages.y
-      forEachSystem = f:
-        let
-          perSystem = lib.genAttrs systems f;
-        in
-        lib.genAttrs
-          [ "apps" "checks" "devShells" "hydraJobs" "packages" ]
-          (attrName: lib.genAttrs systems (system: perSystem.${system}.${attrName} or { }))
-      ;
+      # keep it simple (from https://ayats.org/blog/no-flake-utils/)
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system:
+          # Maybe this could be
+          #     haskellNix.legacyPackages.${system}.extend self.overlays.default
+          # in terms of evaluation it is the same but we don't have to repeat
+          # the haskell.nix configuration.
+          f (import nixpkgs {
+            inherit system;
+            inherit (haskellNix) config;
+            overlays = [ haskellNix.overlay self.overlays.default ];
+          }));
+
+      # This is a project overlay that adds a mkTarball function that
+      # makes a tarball out of a package's exe components.
+      # Maybe, rather than a function, this could be a derivation under each
+      # package in hsPkgs but it find that a bit more awkard to write.
+      mkTarballOverlay = final: prev: {
+        mkTarball = pname:
+          let
+            inherit (final) pkgs;
+            package = final.hsPkgs.${pname};
+            pkgId = "${package.identifier.name}-${package.identifier.version}";
+            exes = builtins.attrValues package.components.exes;
+          in
+          pkgs.runCommand pkgId
+            { preferLocalBuild = true; }
+            ''
+              mkdir -p ${pkgId}
+              cp --verbose --target-directory ${pkgId} \
+                ${pkgs.lib.concatMapStringsSep "  \\\n  " (p: "${p}/bin/*") exes}
+
+              mkdir -p $out
+              tar cvzf $out/${pkgId}.tar.gz ${pkgId}
+
+              mkdir -p $out/nix-support
+              echo "file binary-dist $out/${pkgId}.tar.gz" >> $out/nix-support/hydra-build-products
+              # Propagate the release name of the source tarball.  This is
+              # to get nice package names in channels.
+              echo "${pkgId}" >> $out/nix-support/hydra-release-name
+            '';
+      };
     in
     {
+      # this is not per-system!
       overlays.default = import ./overlay.nix;
 
       # This break the loop. Our overlay evaluates with the flake provided
       # haskell-nix but haskell-nix won't re-evaluate with our nix-tools.
-      legacyPackages = lib.genAttrs systems (system:
-        let pkgs = haskellNix.legacyPackages.${system};
-        in self.overlays.default pkgs pkgs);
-    }
-    // forEachSystem (system:
-      let
-        pkgs = self.legacyPackages.${system};
-        project = pkgs.nix-tools.project.extend (final: prev: {
-          mkTarball = pname:
-            let
-              inherit (final) pkgs;
-              package = final.hsPkgs.${pname};
-              pkgId = "${package.identifier.name}-${package.identifier.version}";
-              exes = builtins.attrValues package.components.exes;
-            in
-            pkgs.runCommand pkgId
-              { preferLocalBuild = true; }
-              ''
-                mkdir -p ${pkgId}
-                cp --verbose --target-directory ${pkgId} \
-                  ${pkgs.lib.concatMapStringsSep "  \\\n  " (p: "${p}/bin/*") exes}
+      legacyPackages = forAllSystems (pkgs:
+        self.overlays.default pkgs pkgs);
 
-                mkdir -p $out
-                tar cvzf $out/${pkgId}.tar.gz ${pkgId}
+      project = forAllSystems (pkgs:
+        pkgs.nix-tools.project.extend mkTarballOverlay);
 
-                mkdir -p $out/nix-support
-                echo "file binary-dist $out/${pkgId}.tar.gz" >> $out/nix-support/hydra-build-products
-                # Propagate the release name of the source tarball.  This is
-                # to get nice package names in channels.
-                echo "${pkgId}" >> $out/nix-support/hydra-release-name
-              '';
+      packages = forAllSystems (pkgs:
+        lib.mapAttrs'
+          (n: v: { name = v.exeName; value = v; })
+          pkgs.nix-tools.project.flake'.packages);
+
+      checks = forAllSystems (pkgs:
+        pkgs.nix-tools.project.flake'.checks
+        // {
+          truncate-index = import ./tests/truncate-index.nix { inherit pkgs; };
         });
-      in
-      {
-        inherit (project.flake') "devShells";
 
-        packages =
-          lib.mapAttrs'
-            (n: v: { name = v.exeName; value = v; })
-            project.flake'.packages;
+      devShells = forAllSystems (pkgs:
+        { default = pkgs.nix-tools.project.shell; });
 
-        checks = project.flake'.checks // {
-          truncate-index =
-            let
-              hash = "0z2jc4fibfxz88pfgjq3wk5j3v7sn34xkwb8h60hbwfwhhy63vx6";
-              index-state = "2020-01-10T00:00:00Z";
-            in
-            pkgs.runCommand "nix-tools-test-truncate-index"
-              {
-                outputHashAlgo = "sha256";
-                outputHash = hash;
-                buildInputs = [ pkgs.wget ];
-              } ''
-              wget http://hackage.haskell.org/01-index.tar.gz
-              ${project.hsPkgs.nix-tools.components.exes.truncate-index}/bin/truncate-index -o $out -i 01-index.tar.gz -s ${index-state}
-            '';
-        };
-
-        hydraJobs = project.flake'.hydraJobs
-        // lib.optionalAttrs (system == "x86_64-linux") {
-          binary-tarball = project.projectCross.musl64.mkTarball "nix-tools";
+      hydraJobs = forAllSystems (pkgs:
+        # project's hydraJobs
+        pkgs.nix-tools.project.flake'.hydraJobs
+        # tarballs with static builds.
+        // lib.optionalAttrs (pkgs.buildPlatform.system == "x86_64-linux") {
+          binary-tarball = pkgs.nix-tools.project.projectCross.musl64.mkTarball "nix-tools";
         }
-        // lib.optionalAttrs (system == "aarch64-linux") {
-          binary-tarball = project.projectCross.aarch64-multiplatform-musl.mkTarball "nix-tools";
-        };
-      });
+        // lib.optionalAttrs (pkgs.buildPlatform.system == "aarch64-linux") {
+          binary-tarball = pkgs.nix-tools.project.projectCross.aarch64-multiplatform-musl.mkTarball "nix-tools";
+        });
+    };
 
   nixConfig = {
     extra-substituters = [
