5c0e21d715d2fd4ac9582ca4841cede2d39df1fd
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-08-14 16:33:21 +1200

    Test fixes

--- test/th-dlls/default.nix
+++ test/th-dlls/default.nix
@@ -23,7 +23,7 @@ in recurseIntoAttrs {
     # just depends on the `text` package (this may be related to the C++ dependency).
     (stdenv.hostPlatform.isWindows && __compareVersions buildPackages.haskell-nix.compiler.${compiler-nix-name}.version "9.4.0" >= 0) ||
     # Similar problem on macOS
-    (stdenv.hostPlatform.isDarwin && __compareVersions buildPackages.haskell-nix.compiler.${compiler-nix-name}.version "9.8.0" >= 0) ||
+    (stdenv.hostPlatform.isDarwin && __compareVersions buildPackages.haskell-nix.compiler.${compiler-nix-name}.version "9.4.0" >= 0) ||
     # On aarch64 this test also breaks form musl builds (including cross compiles on x86_64-linux)
     (stdenv.hostPlatform.isAarch64 && stdenv.hostPlatform.isMusl);
 
