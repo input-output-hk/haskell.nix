19e9a45ada72f6c6e43f79c0ff3440b38df815ef
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-08-14 12:46:47 +1200

    Test fix

--- test/call-cabal-project-to-nix/default.nix
+++ test/call-cabal-project-to-nix/default.nix
@@ -9,6 +9,21 @@ let
     inherit compiler-nix-name evalPackages;
     # reuse the cabal-simple test project
     src = testSrc "cabal-simple";
+    cabalProjectLocal = optionalString (__compareVersions pkgs.buildPackages.haskell-nix.compiler.${config.compiler-nix-name}.version "9.8.0" >= 0) ''
+      allow-newer: *:*
+
+      repository head.hackage.ghc.haskell.org
+        url: https://ghc.gitlab.haskell.org/head.hackage/
+        secure: True
+        key-threshold: 3
+        root-keys:
+          f76d08be13e9a61a377a85e2fb63f4c5435d40f8feb3e12eb05905edb8cdea89
+          26021a13b401500c8eb2761ca95c61f2d625bfef951b939a8124ed12ecf07329
+          7541f32a4ccca4f97aea3b22f5e593ba2c0267546016b992dfadcd2fe944e55d
+        --sha256: sha256-WrhyfhCN6IiP5/yCyQibeHHfUDoAUioAH2ysMLoRqdg=
+
+      active-repositories: hackage.haskell.org, head.hackage.ghc.haskell.org:override
+    '';
   };
   pkgSet = mkCabalProjectPkgSet {
     plan-pkgs = importAndFilterProject {
