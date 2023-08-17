5490442f89e52ab6a97436be0804d5a9570d798e
Andrea Bedini <andrea.bedini@tweag.io>
2023-08-17 17:19:21 +0800

    Add tarballs with static binaries

--- nix-tools/flake.nix
+++ nix-tools/flake.nix
@@ -17,57 +17,38 @@
       # keep it simple (from https://ayats.org/blog/no-flake-utils/)
       forAllSystems = f:
         nixpkgs.lib.genAttrs systems (system:
-          # Maybe this could be
-          #     haskellNix.legacyPackages.${system}.extend self.overlays.default
-          # in terms of evaluation it is the same but we don't have to repeat
-          # the haskell.nix configuration.
-          f (import nixpkgs {
-            inherit system;
-            inherit (haskellNix) config;
-            overlays = [ haskellNix.overlay self.overlays.default ];
-          }));
+          f (haskellNix.legacyPackages.${system}.extend self.overlays.default));
 
-      # This is a project overlay that adds a mkTarball function that
-      # makes a tarball out of a package's exe components.
-      # Maybe, rather than a function, this could be a derivation under each
-      # package in hsPkgs but it find that a bit more awkard to write.
-      mkTarballOverlay = final: prev: {
-        mkTarball = pname:
-          let
-            inherit (final) pkgs;
-            package = final.hsPkgs.${pname};
-            pkgId = "${package.identifier.name}-${package.identifier.version}";
-            exes = builtins.attrValues package.components.exes;
-          in
-          pkgs.runCommand pkgId
-            { preferLocalBuild = true; }
-            ''
-              mkdir -p ${pkgId}
-              cp --verbose --target-directory ${pkgId} \
-                ${pkgs.lib.concatMapStringsSep "  \\\n  " (p: "${p}/bin/*") exes}
+      mkTarball = pkgs: cross:
+        let
+          # Use haskell.nix compilers here
+          prj = pkgs.nix-tools.project.appendModule
+            ({ lib, ... }:
+              { compilerSelection = lib.mkForce (p: p.haskell-nix.compiler); });
+          nix-tools = prj.projectCross.${cross}.hsPkgs.nix-tools;
+          pkgId = "${nix-tools.identifier.name}-${nix-tools.identifier.version}";
+          exes = builtins.attrValues nix-tools.components.exes;
+        in
+        pkgs.runCommand pkgId
+          { preferLocalBuild = true; }
+          ''
+            mkdir -p ${pkgId}
+            cp --verbose --target-directory ${pkgId} ${pkgs.lib.concatMapStringsSep " " (p: "${p}/bin/*") exes}
 
-              mkdir -p $out
-              tar cvzf $out/${pkgId}.tar.gz ${pkgId}
+            mkdir -p $out
+            tar cvzf $out/${pkgId}.tar.gz ${pkgId}
 
-              mkdir -p $out/nix-support
-              echo "file binary-dist $out/${pkgId}.tar.gz" >> $out/nix-support/hydra-build-products
-              # Propagate the release name of the source tarball.  This is
-              # to get nice package names in channels.
-              echo "${pkgId}" >> $out/nix-support/hydra-release-name
-            '';
-      };
+            mkdir -p $out/nix-support
+            echo "file binary-dist $out/${pkgId}.tar.gz" >> $out/nix-support/hydra-build-products
+          '';
     in
     {
       # this is not per-system!
       overlays.default = import ./overlay.nix;
 
-      # This break the loop. Our overlay evaluates with the flake provided
-      # haskell-nix but haskell-nix won't re-evaluate with our nix-tools.
-      legacyPackages = forAllSystems (pkgs:
-        self.overlays.default pkgs pkgs);
+      legacyPackages = forAllSystems (pkgs: pkgs);
 
-      project = forAllSystems (pkgs:
-        pkgs.nix-tools.project.extend mkTarballOverlay);
+      project = forAllSystems (pkgs: pkgs.nix-tools.project);
 
       packages = forAllSystems (pkgs:
         lib.mapAttrs'
@@ -82,20 +63,15 @@
       devShells = forAllSystems (pkgs:
         { default = pkgs.nix-tools.project.shell; });
 
-      hydraJobs = forAllSystems (pkgs:
-        # project's hydraJobs
-        pkgs.nix-tools.project.flake'.hydraJobs
-        # tarballs with static builds.
-
-        #
-        # FIXME: temporarily disable
-        #
-        # // lib.optionalAttrs (pkgs.buildPlatform.system == "x86_64-linux") {
-        #   binary-tarball = pkgs.nix-tools.project.projectCross.musl64.mkTarball "nix-tools";
-        # }
-        # // lib.optionalAttrs (pkgs.buildPlatform.system == "aarch64-linux") {
-        #   binary-tarball = pkgs.nix-tools.project.projectCross.aarch64-multiplatform-musl.mkTarball "nix-tools";
-        # }
+      hydraJobs = forAllSystems
+        (pkgs:
+          # project's hydraJobs
+          pkgs.nix-tools.project.flake'.hydraJobs
+          # tarballs with static builds.
+          // lib.optionalAttrs (pkgs.buildPlatform.system == "x86_64-linux")
+            { binary-tarball = mkTarball pkgs "musl64"; }
+          // lib.optionalAttrs (pkgs.buildPlatform.system == "aarch64-linux")
+            { binary-tarball = mkTarball pkgs "aarch64-multiplatform-musl"; }
         );
     };
 
