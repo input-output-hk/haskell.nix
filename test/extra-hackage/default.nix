5c5d21d855892ad3ac237984ff13438efff277dd
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-08-22 22:51:26 +1200

    Bump head.hackage

--- test/extra-hackage/default.nix
+++ test/extra-hackage/default.nix
@@ -29,7 +29,7 @@ let
           f76d08be13e9a61a377a85e2fb63f4c5435d40f8feb3e12eb05905edb8cdea89
           26021a13b401500c8eb2761ca95c61f2d625bfef951b939a8124ed12ecf07329
           7541f32a4ccca4f97aea3b22f5e593ba2c0267546016b992dfadcd2fe944e55d
-        --sha256: sha256-WrhyfhCN6IiP5/yCyQibeHHfUDoAUioAH2ysMLoRqdg=
+        --sha256: sha256-yMzVCP7DLb1Ztif1KCGk4RfREoROjtb6QBBtrSFy4OQ=
 
       active-repositories: hackage.haskell.org, head.hackage.ghc.haskell.org:override, extra-hackage-demo:override
     '';
