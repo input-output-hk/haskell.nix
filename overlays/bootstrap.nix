cb09f7848692514e6c11cf91e44daca579657ffa
Moritz Angermann <moritz.angermann@gmail.com>
2023-11-12 08:26:38 +0000

    Add [PATCH] AArch64: Fix broken conditional jumps for offsets >= 1MB
    This is added for aarch64 for now only as that's the only affeted platform.

--- overlays/bootstrap.nix
+++ overlays/bootstrap.nix
@@ -260,6 +260,8 @@ in {
                 ++ final.lib.optional (versionAtLeast "9.4"                             && final.stdenv.targetPlatform.isWindows) ./patches/ghc/ghc-9.6-fix-code-symbol-jumps.patch
                 # this one is to allow linking extra symbols from iserv.
                 # ++ fromUntil "9.6.1" "9.10"                                                                                       ./patches/ghc/iserv-syms.patch
+                # Fix the bad fixups: https://gitlab.haskell.org/ghc/ghc/-/commit/2adc050857a9c1b992040fbfd55fbe65b2851b19
+                ++ final.lib.optional (versionAtLeast "9.6"    && versionLessThan "9.9" && final.stdenv.targetPlatform.isAarch64) ./patches/ghc/2adc050857a9c1b992040fbfd55fbe65b2851b19.patch
                 ;
         in ({
             ghc865 = final.callPackage ../compiler/ghc (traceWarnOld "8.6" {
