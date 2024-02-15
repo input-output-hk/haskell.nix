9a36a272d0aa6eedf5acfe3b9b7004f4178a916d
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-02-15 19:43:21 +1300

    Try +hash-unit-ids

--- compiler/ghc/default.nix
+++ compiler/ghc/default.nix
@@ -301,6 +301,7 @@ let
           + lib.optionalString enableDWARF "+debug_info"
           + lib.optionalString ((enableNativeBignum && hadrianHasNativeBignumFlavour) || targetPlatform.isGhcjs) "+native_bignum"
           + lib.optionalString targetPlatform.isGhcjs "+no_profiled_libs"
+          + lib.optionalString (builtins.compareVersions ghc-version "9.8.1" >= 0) "+hash-unit-ids"
       } --docs=no-sphinx -j --verbose"
       # This is needed to prevent $GCC from emitting out of line atomics.
       # Those would then result in __aarch64_ldadd1_sync and others being referenced, which
