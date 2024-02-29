4d038a193242846d3110490b58791c61bed4be9a
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-02-29 14:39:31 +1300

    Update hackage.head sha256

--- overlays/bootstrap.nix
+++ overlays/bootstrap.nix
@@ -1351,7 +1351,7 @@ in {
                f76d08be13e9a61a377a85e2fb63f4c5435d40f8feb3e12eb05905edb8cdea89
                26021a13b401500c8eb2761ca95c61f2d625bfef951b939a8124ed12ecf07329
                7541f32a4ccca4f97aea3b22f5e593ba2c0267546016b992dfadcd2fe944e55d
-            --sha256: sha256-7gj1rdsbRtU+HWGjDuLqb/YZ6NNjtUeCgAVsB9RUNx8=
+            --sha256: sha256-ez/5hpSnuVaiHYGsxsB5T/rwbMpzeAR2XLiWscurcYQ=
 
           active-repositories: hackage.haskell.org, head.hackage.ghc.haskell.org:override
         '';
