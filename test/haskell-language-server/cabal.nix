c7129d254f9cd187ba0566118fa64a50c1101b79
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-04-15 19:44:27 +1200

    Update tests to haskell-language-server 2.7 (#2184)
    * Update tests to haskell-language-server 2.7
    
    * Update tests to haskell-language-server 2.7
    
    * Bump hackage-overlay-ghcjs
    
    * Skip hls for ghc 9.10 for now

--- test/haskell-language-server/cabal.nix
+++ test/haskell-language-server/cabal.nix
@@ -3,24 +3,7 @@ let
   project = haskell-nix.cabalProject' {
     inherit compiler-nix-name evalPackages;
     name = "haskell-language-server";
-    src = haskell-nix.sources."hls-2.6";
-    # Even though this is in the cabal.project it is inside a condional
-    # and so haskell.nix cannot parse it properly.  Luckily adding it
-    # again seems to work fine.
-    cabalProjectLocal = ''
-      repository head.hackage.ghc.haskell.org
-        url: https://ghc.gitlab.haskell.org/head.hackage/
-        secure: True
-        key-threshold: 3
-        root-keys:
-           f76d08be13e9a61a377a85e2fb63f4c5435d40f8feb3e12eb05905edb8cdea89
-           26021a13b401500c8eb2761ca95c61f2d625bfef951b939a8124ed12ecf07329
-           7541f32a4ccca4f97aea3b22f5e593ba2c0267546016b992dfadcd2fe944e55d
-        --sha256: sha256-Bkn2Etb0JVmb7tM7jxuIoYLFnSp7acqraEYVq0I5oUM=
-
-      if impl(ghc < 9.7)
-        active-repositories: hackage.haskell.org
-    '';
+    src = haskell-nix.sources."hls-2.7";
     configureArgs = "--disable-benchmarks --disable-tests";
   };
 in recurseIntoAttrs {
@@ -33,5 +16,5 @@ in recurseIntoAttrs {
   meta.disabled =
     stdenv.hostPlatform != stdenv.buildPlatform
     || __compareVersions buildPackages.haskell-nix.compiler.${compiler-nix-name}.version "9.0.1" < 0
-    || __compareVersions buildPackages.haskell-nix.compiler.${compiler-nix-name}.version "9.8.0" >= 0;
+    || __compareVersions buildPackages.haskell-nix.compiler.${compiler-nix-name}.version "9.9.0" >= 0;
 }
