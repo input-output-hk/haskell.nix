92ae227b2c95762580033b75c8038e1905efae4a
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-05-04 01:52:06 +1200

    Add materialization

--- materialized/ghc-boot-packages-nix/ghc963llvm/genprimopcode.nix
+++ materialized/ghc-boot-packages-nix/ghc963llvm/genprimopcode.nix
@@ -21,20 +21,20 @@
       synopsis = "Generates various files implementing GHC's primitive operations.";
       description = "This utility reads a textual description of GHC's primitive operations\n(@primops.txt.pp@) and produces a number of outputs. These include,\n\n* the @GHC.Prim@ module included in the @ghc-prim@ package.\n* the @GHC.PrimopWrappers@ module included in the @ghc-prim@ package.\n* an LaTeX document describing the primitive operations.";
       buildType = "Simple";
-      };
+    };
     components = {
       exes = {
         "genprimopcode" = {
           depends = [
             (hsPkgs."base" or (errorHandler.buildDepError "base"))
             (hsPkgs."array" or (errorHandler.buildDepError "array"))
-            ];
-          build-tools = (pkgs.lib).optionals (flags.build-tool-depends) [
+          ];
+          build-tools = pkgs.lib.optionals (flags.build-tool-depends) [
             (hsPkgs.buildPackages.alex.components.exes.alex or (pkgs.buildPackages.alex or (errorHandler.buildToolDepError "alex:alex")))
             (hsPkgs.buildPackages.happy.components.exes.happy or (pkgs.buildPackages.happy or (errorHandler.buildToolDepError "happy:happy")))
-            ];
+          ];
           buildable = true;
-          };
         };
       };
-    } // rec { src = (pkgs.lib).mkDefault ./.; }
+    };
+  } // rec { src = pkgs.lib.mkDefault ./.; }
