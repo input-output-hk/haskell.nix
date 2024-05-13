6bdb46f13c2408d025c16593f82153efd2f4f2a2
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-05-13 20:15:49 +1200

    Add HLS 2.8

--- build.nix
+++ build.nix
@@ -62,7 +62,7 @@ in rec {
         inherit evalPackages;
         src = pkgs.haskell-nix.sources."hls-2.2";
       };
-    } // pkgs.lib.optionalAttrs (ghcFromTo "9.0" "9.11") {
+    } // pkgs.lib.optionalAttrs (ghcFromTo "9.0" "9.10") {
       "hls-28" = tool compiler-nix-name "haskell-language-server" {
         inherit evalPackages;
         src = pkgs.haskell-nix.sources."hls-2.8";
