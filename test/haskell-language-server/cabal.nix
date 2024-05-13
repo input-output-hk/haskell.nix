a97861f4f2e03a84b3f04eaadac96b40d2d50680
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-05-13 20:44:24 +1200

    Add HLS 2.8

--- test/haskell-language-server/cabal.nix
+++ test/haskell-language-server/cabal.nix
@@ -15,6 +15,6 @@ in recurseIntoAttrs {
   # hls does not need to be cross compiled.
   meta.disabled =
     stdenv.hostPlatform != stdenv.buildPlatform
-    || __compareVersions buildPackages.haskell-nix.compiler.${compiler-nix-name}.version "9.0.1" < 0
+    || __compareVersions buildPackages.haskell-nix.compiler.${compiler-nix-name}.version "9.8.1" < 0
     || __compareVersions buildPackages.haskell-nix.compiler.${compiler-nix-name}.version "9.9.0" >= 0;
 }
