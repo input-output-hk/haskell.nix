1019b1be4675018b11091ef7dfe57a08a1703eb7
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-05-13 13:37:12 +1200

    Ignore cabal from GHC source for 9.10

--- overlays/ghc-packages.nix
+++ overlays/ghc-packages.nix
@@ -73,7 +73,7 @@ let
       iserv        = "utils/iserv";
     } // final.lib.optionalAttrs ((!final.stdenv.hostPlatform.isGhcjs || builtins.compareVersions ghcVersion "9.6" < 0) && builtins.compareVersions ghcVersion "9.8" < 0) {
       libiserv     = "libraries/libiserv";
-    } // final.lib.optionalAttrs (builtins.compareVersions ghcVersion "9.9" > 0) {
+    } // final.lib.optionalAttrs (builtins.compareVersions ghcVersion "9.11" > 0) {
       Cabal        = "libraries/Cabal/Cabal";
       Cabal-syntax = "libraries/Cabal/Cabal-syntax";
       cabal-install = "libraries/Cabal/cabal-install";
