a210027aac10cd892b89c3f55ce1d18ac891dda9
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-05-22 23:48:11 +1200

    Include fixes for ghc 9.10 as well

--- overlays/bootstrap.nix
+++ overlays/bootstrap.nix
@@ -279,7 +279,7 @@ in {
                 ++ final.lib.optional (versionAtLeast "9.6"    && versionLessThan "9.11" && final.stdenv.targetPlatform.isMusl && final.stdenv.targetPlatform.isAarch64) ./patches/ghc/ghc-9.6-linker-pool-allocator.patch
                 ++ final.lib.optional (versionAtLeast "9.6"    && versionLessThan "9.11" && final.stdenv.targetPlatform.isMusl && final.stdenv.targetPlatform.isAarch64) ./patches/ghc/ghc-9.6-linker-pool-allocator-2.patch
 
-                ++ final.lib.optional (versionAtLeast "9.6"    && versionLessThan "9.11" && final.stdenv.targetPlatform.isMusl) ./patches/ghc/ghc-9.6-0001-Refactor-IServ.hs.patch
+                ++ final.lib.optional (versionAtLeast "9.6"    && versionLessThan "9.8" && final.stdenv.targetPlatform.isMusl) ./patches/ghc/ghc-9.6-0001-Refactor-IServ.hs.patch
                 ++ final.lib.optional (versionAtLeast "9.6"    && versionLessThan "9.11" && final.stdenv.targetPlatform.isMusl) ./patches/ghc/ghc-9.6-0002-Drop-spurious-8-byte-offset-from-elf_plt.patch
                 ++ final.lib.optional (versionAtLeast "9.6"    && versionLessThan "9.11" && final.stdenv.targetPlatform.isMusl && final.stdenv.targetPlatform.isAarch64) ./patches/ghc/ghc-9.6-0003-Better-pool-alignment.-We-still-hardcode-section-ali.patch
                 ++ final.lib.optional (versionAtLeast "9.6"    && versionLessThan "9.11" && final.stdenv.targetPlatform.isMusl && final.stdenv.targetPlatform.isAarch64) ./patches/ghc/ghc-9.6-0007-fixup-Better-pool-alignment.-We-still-hardcode-secti.patch
