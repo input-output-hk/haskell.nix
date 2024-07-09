e3b308496f947c63614fa0fe503eda7e0a12f18e
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-07-09 13:09:55 +1200

    Disable darwin for now

--- flake.nix
+++ flake.nix
@@ -108,9 +108,9 @@
       # systems supported by haskell.nix
       systems = [
         "x86_64-linux"
-        "x86_64-darwin"
+        # "x86_64-darwin"
         "aarch64-linux"
-        "aarch64-darwin"
+        # "aarch64-darwin"
       ];
 
       nixpkgsArgs = {
