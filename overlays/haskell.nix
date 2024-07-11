cbf3ebe3ce1781505e6190fc7d1582352a5ebbba
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-07-11 12:54:35 +1200

    Use stable-haskell/iserv-proxy?ref=iserv-syms for ghc <9.4

--- overlays/haskell.nix
+++ overlays/haskell.nix
@@ -1035,14 +1035,6 @@ final: prev: {
         withInputs = final.recurseIntoAttrs;
 
         iserv-proxy-exes = __mapAttrs (compiler-nix-name: _ghc:
-          if __compareVersions final.buildPackages.haskell-nix.compiler.${compiler-nix-name}.version "9.4" <0
-            then {
-              inherit (final.buildPackages.ghc-extra-packages.${compiler-nix-name}.iserv-proxy.components.exes) iserv-proxy;
-              # remote-iserv however needs to come from the regular packages as it has to
-              # run on the target host.
-              iserv-proxy-interpreter = final.ghc-extra-packages.${compiler-nix-name}.remote-iserv.components.exes.remote-iserv;
-            }
-          else
             let
               exes = pkgs: (pkgs.haskell-nix.cabalProject' ({pkgs, ...}: {
                 name = "iserv-proxy";
