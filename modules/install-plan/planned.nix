1257535a574dc1dde73b65baa101d3ecb2e8cdde
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-09-09 21:01:47 +1200

    Remove unused binding

--- modules/install-plan/planned.nix
+++ modules/install-plan/planned.nix
@@ -1,20 +1,15 @@
 # Mark everthing in the install-plan as "planned"
 {getComponents}:
 {config, lib, ...}: {
-  packages = lib.listToAttrs (map (p:
-    let components =
-      if p ? component-name
-        then { ${p.component-name} = { inherit (p) depends exe-depends; }; }
-        else p.components or {};
-    in {
-        name = p.id;
-        value.components = lib.mapAttrs (type: x:
-            if type == "library" || type == "setup"
-              then { planned = lib.mkOverride 900 true; }
-              else
-                lib.mapAttrs (_: _: {
-                  planned = lib.mkOverride 900 true;
-                }) x
-          ) (getComponents null {} p);
-      }) config.plan-json.install-plan);
+  packages = lib.listToAttrs (map (p: {
+    name = p.id;
+    value.components = lib.mapAttrs (type: x:
+        if type == "library" || type == "setup"
+          then { planned = lib.mkOverride 900 true; }
+          else
+            lib.mapAttrs (_: _: {
+              planned = lib.mkOverride 900 true;
+            }) x
+      ) (getComponents null {} p);
+  }) config.plan-json.install-plan);
 }
