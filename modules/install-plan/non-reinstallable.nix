8956daa3713d001c04060465c9ddafd0515453ac
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-09-24 20:05:50 +1200

    Fix for GHC HEAD JS

--- modules/install-plan/non-reinstallable.nix
+++ modules/install-plan/non-reinstallable.nix
@@ -6,7 +6,7 @@
       "ghc-internal"]
     ++ lib.optionals (pkgs.stdenv.hostPlatform.isGhcjs) ([
       # ghci and its dependencies
-      "ghci" "binary" "bytestring" "containers" "template-haskell" "array" "deepseq" "filepath" "ghc-boot" "ghc-boot-th" "ghc-heap" "transformers" "unix" "directory" "time" "ghc-platform" "os-string"]
+      "ghci" "binary" "bytestring" "containers" "template-haskell" "array" "deepseq" "file-io" "filepath" "ghc-boot" "ghc-boot-th" "ghc-heap" "transformers" "unix" "directory" "time" "ghc-platform" "os-string"]
     ++ lib.optionals (builtins.compareVersions config.compiler.version "8.11" < 0) [
       "ghcjs-prim" "ghcjs-th"]);
 }
