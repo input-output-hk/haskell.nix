d86768a7b6c227f84891ef3d901ae24dd38c9e8a
Andrea Bedini <andrea.bedini@tweag.io>
2024-10-15 11:04:53 +0800

    Improve formatting

--- flake.nix
+++ flake.nix
@@ -272,34 +272,16 @@
                   pkgs.haskell-nix.compiler.${compiler-nix-name}
                 ];
               };
-            shells = lib.genAttrs
-              (
-                # Exclude old versions of GHC to speed up `nix flake check`
-                lib.attrNames (
-                  lib.removeAttrs pkgs.haskell-nix.compiler
-                    [
-                      "ghc844"
-                      "ghc861"
-                      "ghc862"
-                      "ghc863"
-                      "ghc864"
-                      "ghc881"
-                      "ghc882"
-                      "ghc883"
-                      "ghc8101"
-                      "ghc8102"
-                      "ghc8103"
-                      "ghc8104"
-                      "ghc8105"
-                      "ghc8106"
-                      "ghc810420210212"
-                      "ghc901"
-                      "ghc921"
-                      "ghc922"
-                      "ghc923"
-                    ]
-                )
-              )
+            shells = lib.genAttrs (
+              # Exclude old versions of GHC to speed up `nix flake check`
+              lib.attrNames (
+                lib.removeAttrs pkgs.haskell-nix.compiler
+                  ([ "ghc844" ] ++
+                    [ "ghc861" "ghc862" "ghc863" "ghc864" ] ++
+                    [ "ghc881" "ghc882" "ghc883" ] ++
+                    [ "ghc8101" "ghc8102" "ghc8103" "ghc8104" "ghc810420210212" "ghc8105" "ghc8106" ] ++
+                    [ "ghc901" ] ++ [ "ghc921" "ghc922" "ghc923" ])
+              ))
               mkHaskellNixShell;
           in
           shells // { default = shells.${compiler}; });
