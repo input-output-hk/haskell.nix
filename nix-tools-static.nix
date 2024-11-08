e6faa83e79573cff1ac132fca767fc608020161f
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-11-08 15:21:37 +1300

    Revert "update nix-tools-static.nix"
    This reverts commit 0bb2cb5f8981501f84bdc16681f07a725aebc7f9.

--- nix-tools-static.nix
+++ nix-tools-static.nix
@@ -1,22 +1,22 @@
-pkgs: let baseurl = "https://github.com/input-output-hk/haskell.nix/releases/download/nix-tools-0.2.8/"; in {
+pkgs: let baseurl = "https://github.com/input-output-hk/haskell.nix/releases/download/nix-tools-0.2.6/"; in {
   aarch64-darwin = pkgs.fetchurl { 
      name = "aarch64-darwin-nix-tools-static";
      url = "${baseurl}aarch64-darwin-nix-tools-static.zip";
-     sha256 = "sha256-XzVWGDDJUd4SAKREkdN1k3EId3Gs2ji3J2LA0hLalOk=";
+     sha256 = "sha256-9WpTIWlpUvG3pI+tcbAMh6sMH0QO/coZrxDYWD43iq0=";
   };
   x86_64-darwin = pkgs.fetchurl { 
      name = "x86_64-darwin-nix-tools-static";
      url = "${baseurl}x86_64-darwin-nix-tools-static.zip";
-     sha256 = "sha256-sz9x8U/N0OfkyTltxJbbVaZvW0jemDAfcov3X2xtFmY=";
+     sha256 = "sha256-UUr9bo2OpLPsvHRSeO2B6DKVDVTsHepRlTqN6UZoZ2M=";
   };
   aarch64-linux = pkgs.fetchurl { 
      name = "aarch64-linux-nix-tools-static";
      url = "${baseurl}aarch64-linux-nix-tools-static.zip";
-     sha256 = "sha256-gBpNfqtvbQvZzOKhk68+6jw+VUl5ORi3QzPgwyXiKVA=";
+     sha256 = "sha256-96s6RXN8st0JK0eYSOkTJvnlTxYVdE81+ZUGJEsC46A=";
   };
   x86_64-linux = pkgs.fetchurl { 
      name = "x86_64-linux-nix-tools-static";
      url = "${baseurl}x86_64-linux-nix-tools-static.zip";
-     sha256 = "sha256-aZn6D/Cc7anLJvXiKnJpKlizh5jHSYhEwVen/atE97o=";
+     sha256 = "sha256-LMFVUKNycjVFBb3ChZsPbRNgab50zOHl7nMBrDdeTrQ=";
   };
 }
