bc9ba011d50392aea92cca6bbf19aa971a0e970c
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-11-26 13:30:33 +1300

    Try Android SDK version 26

--- overlays/android.nix
+++ overlays/android.nix
@@ -1,14 +1,21 @@
 _final: prev: {
+  # Use android SDK version 26
   pkgsCross = prev.pkgsCross // {
     aarch64-android = import prev.path {
       inherit system;
       inherit (prev) overlays;
-      crossSystem = prev.lib.systems.examples.aarch64-android // { sdkVer = "26"; };
+      crossSystem = prev.lib.systems.examples.aarch64-android // {
+        sdkVer = "26"; # Old name for old nixpkgs
+        androidSdkVersion = "26";
+      };
     };
     armv7a-android-prebuilt = import prev.path {
       inherit system;
       inherit (prev) overlays;
-      crossSystem = prev.lib.systems.examples.armv7a-android-prebuilt // { sdkVer = "26"; };
+      crossSystem = prev.lib.systems.examples.armv7a-android-prebuilt // {
+        sdkVer = "26"; # Old name for old nixpkgs
+        androidSdkVersion = "26";
+      };
     };
   };
 } // prev.lib.optionalAttrs prev.stdenv.hostPlatform.isAndroid ({
