989066b742b4423b0a2483f6b1c5b9a59619e50d
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-11-26 16:37:07 +1300

    Use small address space

--- overlays/android.nix
+++ overlays/android.nix
@@ -4,14 +4,12 @@ _final: prev: {
     aarch64-android = import prev.path {
       inherit (prev) system overlays;
       crossSystem = prev.lib.systems.examples.aarch64-android // {
-        sdkVer = "26"; # Old name for old nixpkgs
         androidSdkVersion = "26";
       };
     };
     armv7a-android-prebuilt = import prev.path {
       inherit (prev) system overlays;
       crossSystem = prev.lib.systems.examples.armv7a-android-prebuilt // {
-        sdkVer = "26"; # Old name for old nixpkgs
         androidSdkVersion = "26";
       };
     };
