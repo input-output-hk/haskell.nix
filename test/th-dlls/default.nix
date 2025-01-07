0d79e71ce2459163000fd29b5893363636611764
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2025-01-07 14:32:00 +1300

    Skip failing tests

--- test/th-dlls/default.nix
+++ test/th-dlls/default.nix
@@ -37,13 +37,14 @@ in recurseIntoAttrs {
   build-ei = packages-ei.th-dlls.components.library;
   just-template-haskell-ei = packages-ei.th-dlls.components.exes.just-template-haskell;
 } // optionalAttrs
-    (!(builtins.elem compiler-nix-name ["ghc984" "ghc9121" "ghc912120241215x"]  && stdenv.buildPlatform.isx86_64 && stdenv.hostPlatform.isAarch64)) {
+    (!(builtins.elem compiler-nix-name ["ghc984" "ghc9121" "ghc912120241215"]  && stdenv.buildPlatform.isx86_64 && stdenv.hostPlatform.isAarch64)) {
   # On for aarch64 cross compile on GHC this test is fails sometimes for non profiled builds
   # (and always for the profiled builds).
   # This may be related to the memory allocation changes made in 9.8.4 that
   # replace the pool allocator patches we used in earlier versions.
 
   # Interestingly GHC 9.10.1 and HEAD are wotking while 9.8.4 and 9.12 seem break.
+  # Perhaps there is a fix in GHC HEAD?
   build-profiled = packages.th-dlls.components.library.profiled;
   build-profiled-ei = packages-ei.th-dlls.components.library.profiled;
 }
