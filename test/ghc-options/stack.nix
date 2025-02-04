cf3a4426201375fec69e84d19c90e15a1e04a1a6
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2025-02-04 16:38:10 +1300

    Fix broke tests

--- test/ghc-options/stack.nix
+++ test/ghc-options/stack.nix
@@ -9,15 +9,6 @@ let
   };
   packages = project.hsPkgs;
 
-  # Get the names of all packages. This is a test to see
-  # whether there is a broken "$locals" package present.
-  hasIdentifier = p: p != null && p ? identifier;
-  packageNames = mapAttrsToList (name: p:
-    # TODO work out why these are `ghcide` and `hnix` are here in the first place
-    # it might be because we have patches in `modules/configuration.nix`
-    lib.optionalString (!__elem name ["ghcide" "hnix"]) p.identifier.name)
-      (filterAttrs (_name: hasIdentifier) packages);
-
 in recurseIntoAttrs {
   # This test is somehow broken for ghcjs
   meta.disabled = stdenv.hostPlatform.isGhcjs || compiler-nix-name != "ghc984";
@@ -32,7 +23,7 @@ in recurseIntoAttrs {
       printf "checking whether executable runs... " >& 2
       cat ${haskellLib.check packages.test-ghc-options.components.exes.test-ghc-options-exe}/test-stdout
 
-      echo '${concatStringsSep " " packageNames}' > $out
+      touch $out
     '';
 
     meta = rec {
