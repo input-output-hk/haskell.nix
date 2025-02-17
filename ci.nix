e86f8bb6650ba9f4bd919d340ad21fea608c36da
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2025-02-17 15:38:33 +1300

    Fix darwin builds for nixpkgs 24.11
    Fixes
    
    error: use of undeclared identifier 'IP_RECVTOS'
    
    for whatever reason nixpkgs 24.11 defines x86_64-darwin
    to be sdk-10.12.2, and aarch64-darwin to be sdk-11.
    nixpkgs 25.05 will drop sdk-10.12, and unify aarch64 and x86 at last.

--- ci.nix
+++ ci.nix
@@ -58,16 +58,17 @@
       # from here (so that is no longer cached) also remove ./materialized/ghcXXX.
       # Update supported-ghc-versions.md to reflect any changes made here.
       nixpkgs.lib.optionalAttrs (nixpkgsName == "R2411") {
-        # TODO perhaps these
-        # ghc96 = false;
-        # ghc98 = false;
+        ghc96 = false;
+        ghc98 = false;
+        ghc910 = false;
+        ghc912 = true;
       } // nixpkgs.lib.optionalAttrs (nixpkgsName == "unstable") {
         ghc96 = true;
         ghc98 = true;
         ghc98llvm = false;
         ghc910 = true;
         ghc910llvm = true;
-        ghc9121 = true;
+        ghc912 = true;
         ghc912X = true;
         ghc913 = true;
       })));
