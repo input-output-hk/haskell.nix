4c2b9c3f2437d83b7985b718daa0566c475cc531
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2025-03-17 00:17:07 +1300

    Skip broken test

--- test/th-dlls/default.nix
+++ test/th-dlls/default.nix
@@ -39,7 +39,7 @@ in recurseIntoAttrs {
   build-ei = packages-ei.th-dlls.components.library;
   just-template-haskell-ei = packages-ei.th-dlls.components.exes.just-template-haskell;
 } // optionalAttrs
-    (!(builtins.elem compiler-nix-name ["ghc984" "ghc9121" "ghc912120241215" "ghc91320241230"]  && stdenv.buildPlatform.isx86_64 && stdenv.hostPlatform.isAarch64)) {
+    (!(builtins.elem compiler-nix-name ["ghc984" "ghc9121" "ghc912120241215" "ghc91320241230" "ghc912120250219"]  && stdenv.buildPlatform.isx86_64 && stdenv.hostPlatform.isAarch64)) {
   # On for aarch64 cross compile on GHC this test is fails sometimes for non profiled builds
   # (and always for the profiled builds).
   # This may be related to the memory allocation changes made in 9.8.4 that
