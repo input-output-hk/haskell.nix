c60ed7f646e3ea2c018e6980aa3f2f05fbaa9cd3
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2025-03-24 12:35:10 +1300

    Drop Hadrian Materialization
    Maintaining these plans is a bit difficult.  For instance with #2338 the use of GHC 9.6.6 instead of 9.6.4 for building hadrian requires different plans for 9.8.2 and 9.8.4.
    
    Now that we are using a prebuilt static version of nix-tools, there should be less overhead in calculating these plans.

--- compiler/ghc/default.nix
+++ compiler/ghc/default.nix
@@ -262,25 +262,6 @@ let
     buildPackages.haskell-nix.tool compiler-nix-name "hadrian" {
       compilerSelection = p: p.haskell.compiler;
       index-state = buildPackages.haskell-nix.internalHackageIndexState;
-      # Verions of hadrian that comes with 9.6 depends on `time`
-      materialized =
-        if builtins.compareVersions ghc-version "9.4" < 0
-          then ../../materialized/${compiler-nix-name}/hadrian-ghc92
-        else if builtins.compareVersions ghc-version "9.4.8" < 0
-          then ../../materialized/${compiler-nix-name}/hadrian-ghc947
-        else if builtins.compareVersions ghc-version "9.6" < 0
-          then ../../materialized/${compiler-nix-name}/hadrian-ghc94
-        else if builtins.compareVersions ghc-version "9.6.5" < 0
-          then ../../materialized/${compiler-nix-name}/hadrian-ghc964
-        else if builtins.compareVersions ghc-version "9.8" < 0
-          then ../../materialized/${compiler-nix-name}/hadrian-ghc96
-        else if builtins.compareVersions ghc-version "9.8.2" < 0
-          then ../../materialized/${compiler-nix-name}/hadrian-ghc981
-        else if builtins.compareVersions ghc-version "9.9" < 0
-          then ../../materialized/${compiler-nix-name}/hadrian-ghc98
-        else if builtins.compareVersions ghc-version "9.11" < 0
-          then ../../materialized/${compiler-nix-name}/hadrian-ghc910
-        else null;
       modules = [{
         reinstallableLibGhc = false;
         # Apply the patches in a way that does not require using something
