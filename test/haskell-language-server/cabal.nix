53ceccefa39ea4601d44c1aa679f0d6ce5afc79f
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2025-05-09 10:31:00 +1200

    Disable HLS build for GHC 9.10.2

--- test/haskell-language-server/cabal.nix
+++ test/haskell-language-server/cabal.nix
@@ -16,5 +16,5 @@ in recurseIntoAttrs {
   meta.disabled =
     stdenv.hostPlatform != stdenv.buildPlatform
     || __compareVersions buildPackages.haskell-nix.compiler.${compiler-nix-name}.version "9.0.1" < 0
-    || __compareVersions buildPackages.haskell-nix.compiler.${compiler-nix-name}.version "9.11.0" >= 0;
+    || __compareVersions buildPackages.haskell-nix.compiler.${compiler-nix-name}.version "9.10.2" >= 0;
 }
