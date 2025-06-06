6ffde3ebfb4032e043f85c567bfee22d0cbbe2c8
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2025-06-06 19:54:58 +1200

    Skip broken android cross compile

--- ci.nix
+++ ci.nix
@@ -105,7 +105,9 @@
     # Out llvm versions of GHC seem to break for musl32
     inherit (lib.systems.examples) musl32;
   } // lib.optionalAttrs (system == "x86_64-linux" && !builtins.elem compiler-nix-name ["ghc902" "ghc928" "ghc948"]) {
-    inherit (lib.systems.examples) aarch64-android-prebuilt armv7a-android-prebuilt;
+    inherit (lib.systems.examples) aarch64-android-prebuilt;
+  } // lib.optionalAttrs (system == "x86_64-linux" && !builtins.elem compiler-nix-name ["ghc902" "ghc928" "ghc948" "ghc91320250523"]) {
+    inherit (lib.systems.examples) armv7a-android-prebuilt;
   } // lib.optionalAttrs (system == "x86_64-linux" && nixpkgsName == "unstable" && !builtins.elem compiler-nix-name ["ghc8107" "ghc902"]) {
     # TODO fix this for the compilers we build with hadrian (ghc >=9.4)
     inherit (lib.systems.examples) aarch64-multiplatform-musl;
