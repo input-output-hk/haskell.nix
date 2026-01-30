4849a53d07ae39d5d41b560bb6b25f0e9fb86cb4
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2026-01-30 17:30:53 +1300

    Disable aarch64-linux for now

--- flake.nix
+++ flake.nix
@@ -105,7 +105,6 @@
       systems = [
         "x86_64-linux"
       ] ++ (if runningHydraEvalTest then [ ] else [
-        "aarch64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
       ]);
