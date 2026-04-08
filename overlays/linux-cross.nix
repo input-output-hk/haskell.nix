bf27e699a8170d255b9b2bbb69c327b9f65b7e4d
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2026-04-08 22:42:53 +1200

    Use stdio pipes for iserv-proxy, enable Windows TH tests
    Replace TCP socket communication between iserv-proxy and the
    interpreter with stdin/stdout pipes (--pipe/--stdio modes). TCP
    networking inside nix-linux-builder VMs is unreliable, causing iserv
    to hang during Template Haskell evaluation.
    
    The proxy now spawns the interpreter as a subprocess and communicates
    via its stdin/stdout, bypassing TCP entirely. The old TCP mode is
    preserved for backward compatibility.
    
    Also:
    - Add -optl-ldl to android ghcOptions (GHC RTS references dlopen even
      with -dynamic-system-linker disabled)
    - Enable TH tests (annotations, th-dlls, th-dlls-minimal,
      js-template-haskell) for ucrt64 Windows cross-compilation
    - Disable mingwW64 (msvcrt) due to Rosetta wine crash
    - Android TH tests remain disabled pending linker pool patch

--- overlays/linux-cross.nix
+++ overlays/linux-cross.nix
@@ -14,23 +14,6 @@
 , ...
 }:
 let
-  # For 32bit android, we need to pass -no-pie, as we otherwise
-  # get -pie injected into the linker flags. We don't want that.
-  # If we target 32bit android, we need remote-iserv to be runnable
-  # in a 32bit linux (via qemu-arm user mode emulation).  If we have
-  # -pie enabled, it will produce a static-pie executable, which
-  # seems a lot like what we want but will crash on launch.  It appears
-  # the __stack_chk_guard lookups go through some lookup table, and
-  # while the relocations for the lookup table are correct, the __stack_chk_guard
-  # address isn't properly relocated.  This could also be because libc isn't
-  # supposed to be staticlly linked really.  However because we are lacking
-  # the loader for arm on linux, we can't used dynamically linked executables
-  # until one in /system/bin/linker is provided.
-  #
-  # We also need to run armv7a-android in unshare --user --pid --fork, to
-  # ensure that we get a low pid < 65535 for android (If we run outside)
-  # of nix build envs.
-
   qemuIservWrapperScript = enableProfiling:
     let
       interpreter =
@@ -45,14 +28,8 @@ let
     PROXY_ARGS=''${PROXY_ARGS:-}
     # Unset configure flags as configure should have run already
     unset configureFlags
-    PORT=$((5000 + $RANDOM % 5000))
-    (>&2 echo "---> Starting ${interpreter.exeName} on port $PORT")
-    ${qemu}/bin/qemu-${qemuSuffix} ${interpreter}/bin/${interpreter.exeName} tmp $PORT $ISERV_ARGS &
-    (>&2 echo "---| ${interpreter.exeName} should have started on $PORT")
-    RISERV_PID="$!"
-    ${iserv-proxy}/bin/iserv-proxy $@ 127.0.0.1 "$PORT" $PROXY_ARGS
-    (>&2 echo "---> killing ${interpreter.exeName}...")
-    kill $RISERV_PID
+    (>&2 echo "---> Starting iserv-proxy with piped ${interpreter.exeName} (qemu: ${qemu}/bin/qemu-${qemuSuffix})")
+    ${iserv-proxy}/bin/iserv-proxy $@ --pipe ${qemu}/bin/qemu-${qemuSuffix} ${interpreter}/bin/${interpreter.exeName} tmp --stdio $ISERV_ARGS
     '';
   qemuIservWrapper = symlinkJoin { name = "iserv-wrapper"; paths = [ (qemuIservWrapperScript false) (qemuIservWrapperScript true) ]; };
   configureFlags = lib.optional (hostPlatform.isAarch32 || hostPlatform.isAndroid) "--disable-split-sections";
@@ -61,7 +38,10 @@ let
       "-pgmi" "${qemuIservWrapper}/bin/iserv-wrapper"
       "-L${gmp}/lib"
          # Required to work-around https://gitlab.haskell.org/ghc/ghc/issues/15275
-    ] ++ lib.optionals hostPlatform.isAarch64 ["-fPIC"];
+    ] ++ lib.optionals hostPlatform.isAarch64 ["-fPIC"]
+      # The GHC RTS references dlopen/dlclose/dlsym/dlerror even with
+      # -dynamic-system-linker disabled. Link -ldl for android.
+      ++ lib.optionals hostPlatform.isAndroid ["-optl-ldl"];
 
   # Wrapper for qemu testing
   qemuTestWrapper = writeShellScriptBin "test-wrapper" ''
